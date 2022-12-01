b7d823899cd58ed4b7d32ac1d7c9fe4afb954552
tjrwls0826 <tjrwls0826@naver.com>
2022-12-01 16:30:12 +0900

    Update 짝수와 홀수.py
    자체 테스트 케이스 통과 채점 대기

--- Programmers/Lv1/짝수와 홀수.py
+++ Programmers/Lv1/짝수와 홀수.py
@@ -1,1 +1,13 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12937
+def solution(num):
+    answer = ''
+    if num//2 == 1 or num//2 == -1:
+        answer = 'Odd'
+    else:
+        answer = 'Even'
+    print(answer)
+    return answer
+
+solution(3)
+solution(4)
+solution(-1)
