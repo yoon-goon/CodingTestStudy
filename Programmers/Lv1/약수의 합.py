7749aef8b7092e1d2387fee8a7792000839c3aca
tjrwls0826 <tjrwls0826@naver.com>
2022-12-01 16:49:22 +0900

    Create 약수의 합.py
    1차 에러 발생 수정 후 채점 완료

---
+++ Programmers/Lv1/약수의 합.py
@@ -0,0 +1,15 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12928
+def solution(n):
+    answer = 0
+
+
+    for i in range(n):
+        if n % (i+1) == 0 :
+            answer += (i+1)
+    return print(answer)
+#i+1을 이용하지 않을 경우 n을 0부터 나머지계산을 시작해 에러 발생
+
+
+solution(12)
+
+
