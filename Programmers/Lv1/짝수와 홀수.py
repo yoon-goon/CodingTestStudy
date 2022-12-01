0a413cced30e76047ecff0168b80d99211bd6ec2
tjrwls0826 <tjrwls0826@naver.com>
2022-12-01 16:38:42 +0900

    Update 짝수와 홀수.py
    다시 작성 후 채점 통과

--- Programmers/Lv1/짝수와 홀수.py
+++ Programmers/Lv1/짝수와 홀수.py
@@ -1,4 +1,5 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/12937
+# https://school.programmers.co.kr/learn/courses/30/lessons/12937
+'''
 def solution(num):
     answer = ''
     if num//2 == 1 or num//2 == -1:
@@ -6,7 +7,19 @@ def solution(num):
     else:
         answer = 'Even'
     print(answer)
+    return answer#
+    '''  # 예시 입출력 테스트는 통과했으나 채점에서 문제발생
+
+
+def solution(num):
+    answer = ''
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    print(answer)
     return answer
+
 
 solution(3)
 solution(4)
