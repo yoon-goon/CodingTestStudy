85016ccd17b7c8ec4a84bd51b7b8f5f1414920c0
tjrwls0826 <tjrwls0826@naver.com>
2022-12-04 14:54:46 +0900

    Update 자리수 더하기.py
    다른풀이 추가

--- Programmers/Lv1/자리수 더하기.py
+++ Programmers/Lv1/자리수 더하기.py
@@ -1,12 +1,29 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/12931
+# https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
 def solution(n):
     answer = 0
     while n > 0:
-        answer += n%10  #정답값에 입력값을 10으로 나머지계산해 첫번째 자리수를 구함
-        n//=10          #입력값을 10으로 몫을 구해 자리수를 변경 ex) 294 -> 29 -> 2
-
+        answer += n % 10  # 정답값에 입력값을 10으로 나머지계산해 첫번째 자리수를 구함
+        n //= 10  # 입력값을 10으로 몫을 구해 자리수를 변경 ex) 294 -> 29 -> 2
 
     return print(answer)
 
+
 solution(123)
 solution(987)
+
+
+
+# 다른풀이
+'''
+def solution(n):
+    answer = 0
+    X = [int(i) for i in str(n)]
+    answer = sum(X)
+
+    return print(answer)
+
+
+solution(123)
+solution(987)
+'''
