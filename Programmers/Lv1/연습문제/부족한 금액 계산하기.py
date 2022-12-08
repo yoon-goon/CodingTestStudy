6b34789d843fbab77cdee1a7673e79061b3faf42
tjrwls0826 <tjrwls0826@naver.com>
2022-12-08 21:23:37 +0900

    해설 추가
    해설 추가

--- Programmers/Lv1/연습문제/부족한 금액 계산하기.py
+++ Programmers/Lv1/연습문제/부족한 금액 계산하기.py
@@ -1,13 +1,14 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/82612
+# https://school.programmers.co.kr/learn/courses/30/lessons/82612
 
 def solution(price, money, count):
     total = 0
     for i in range(count):
-        total += price * (i + 1)
-    if money > total:
+        total += price * (i + 1)  # i가 0부터 시작하기 때문에 +1
+    if money > total:  # 문제 조건에 돈이 부족하지 않은 경우 0을 출력하도록 되어있어 if문으로 구현
         return 0
     answer = money - total
 
     return abs(answer)
+
 
 print(solution(3, 20, 4))
