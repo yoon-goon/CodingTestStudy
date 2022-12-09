cd21ba0f8305db1776735ff6768cc888f1003da4
tjrwls0826 <tjrwls0826@naver.com>
2022-12-09 16:12:01 +0900

    Update 문자열 내 p와 y의 개수.py

--- Programmers/Lv1/연습문제/문자열 내 p와 y의 개수.py
+++ Programmers/Lv1/연습문제/문자열 내 p와 y의 개수.py
@@ -4,7 +4,7 @@ def solution(s):
     pcount = 0
     ycount = 0
     for i in range(len(s)):
-        if s[i] == 'p' or s[i] == 'P':
+        if s[i] == 'p' or s[i] == 'P':# s[0] 부터 s[len(s)] 즉 끝까지 돌면서 p혹은 P가 있을경우 카운트 +1
             pcount += 1
         if s[i] == 'y' or s[i] == 'Y':
             ycount += 1
