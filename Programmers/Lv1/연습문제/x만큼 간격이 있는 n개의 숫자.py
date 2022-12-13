102115e47fb1e3782c943e1bdb9b68773b82588c
tjrwls0826 <tjrwls0826@naver.com>
2022-12-13 21:53:07 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- Programmers/Lv1/연습문제/x만큼 간격이 있는 n개의 숫자.py
+++ Programmers/Lv1/연습문제/x만큼 간격이 있는 n개의 숫자.py
@@ -3,7 +3,7 @@
 def solution(x, n):
     answer = []
     for i in range(n):
-        answer.append(x*(i+1))
+        answer.append(x*(i+1)) # i가 0부터 시작하기 때문에 +1
     return answer
 
 print((solution(2,5)))
