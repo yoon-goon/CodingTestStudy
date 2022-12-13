34d6335a8617876f0f9f66f776a43c0c8d3ba894
tjrwls0826 <tjrwls0826@naver.com>
2022-12-13 21:52:35 +0900

    Create x만큼 간격이 있는 n개의 숫자.py

---
+++ Programmers/Lv1/연습문제/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+def solution(x, n):
+    answer = []
+    for i in range(n):
+        answer.append(x*(i+1))
+    return answer
+
+print((solution(2,5)))
+print((solution(4,3)))
+print((solution(-4,2)))
+print((solution(0,2)))
+print((solution(4,0)))
