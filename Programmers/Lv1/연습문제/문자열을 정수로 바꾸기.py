968ce8fd3d35083fd2c88c41aa72056b2e65c18d
tjrwls0826 <tjrwls0826@naver.com>
2022-12-15 22:27:12 +0900

    Create 문자열을 정수로 바꾸기.py

---
+++ Programmers/Lv1/연습문제/문자열을 정수로 바꾸기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+def solution(s):
+    answer = int(s) # 매운 쉬운 풀이
+    return answer
+
+print(solution("1234"))
+print(solution("-1234"))
