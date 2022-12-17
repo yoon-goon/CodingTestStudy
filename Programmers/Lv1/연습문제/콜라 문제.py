87d921b8e5784ced0f8b799a65e51695398cbf7f
tjrwls0826 <tjrwls0826@naver.com>
2022-12-17 21:19:54 +0900

    콜라 문제 해석 주석 추가

--- Programmers/Lv1/연습문제/콜라 문제.py
+++ Programmers/Lv1/연습문제/콜라 문제.py
@@ -3,9 +3,9 @@
 def solution(a, b, n):
     count = 0
     while n >= a:
-        rest = int(n / a) * b
-        n = rest + (n % a)
-        count += rest
+        rest = int(n / a) * b # a개당 b개를 줌으로 총 갯수 n 에서 a 를 나누고(나머지는 int로 배제)
+        n = rest + (n % a) # 갯수 n은 위에서 받은 rest 에 나누고 남아있던 갯수를 더하면 됨
+        count += rest # 이 과정을 반복하면서 받은 병의 갯수를 합산
     return count
 
 
