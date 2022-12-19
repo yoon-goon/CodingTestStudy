943b1efb5460b2120a2bef3420f4987bc62427f8
tjrwls0826 <tjrwls0826@naver.com>
2022-12-19 22:50:28 +0900

    Update 두 정수 사이의 합.py
    역순 조건 추가

--- Programmers/Lv1/연습문제/두 정수 사이의 합.py
+++ Programmers/Lv1/연습문제/두 정수 사이의 합.py
@@ -3,10 +3,16 @@
 
 def solution(a, b):
     answer = 0
-
+    if a <= b:
+        x = a
+        y = b
+    else:
+        x = b
+        y = a
     for i in range(x, y + 1):
         answer += i
     return answer
 
 print(solution(3,5))
 print(solution(3,3))
+print(solution(5,3))
