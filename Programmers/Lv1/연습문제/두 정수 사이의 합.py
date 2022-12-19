8ba65ee2a99f653f54775be5b61dae7c52330c11
tjrwls0826 <tjrwls0826@naver.com>
2022-12-19 22:50:11 +0900

    Create 두 정수 사이의 합.py

---
+++ Programmers/Lv1/연습문제/두 정수 사이의 합.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12912
+
+
+def solution(a, b):
+    answer = 0
+
+    for i in range(x, y + 1):
+        answer += i
+    return answer
+
+print(solution(3,5))
+print(solution(3,3))
