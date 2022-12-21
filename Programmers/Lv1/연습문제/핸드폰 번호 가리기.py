dee3e7e9283f6f7cc1b32e3000619f3f37faa315
tjrwls0826 <tjrwls0826@naver.com>
2022-12-21 15:48:34 +0900

    Update 핸드폰 번호 가리기.py

--- Programmers/Lv1/연습문제/핸드폰 번호 가리기.py
+++ Programmers/Lv1/연습문제/핸드폰 번호 가리기.py
@@ -1,7 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12948
 
 def solution(phone_number):
-    backF = phone_number[-4:] 
-    return "*" * (len(phone_number) - 4) + backF
+    backF = phone_number[-4:] # 뒷 4자리만 저장
+    return "*" * (len(phone_number) - 4) + backF # 번호 길이에서 4 뺀뒤 뒷 4자리를 더함
 
 print(solution("01033334444"))
