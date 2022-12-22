19bf76030db6cc47358b8016ac25ca59ca160e98
tjrwls0826 <tjrwls0826@naver.com>
2022-12-22 21:24:27 +0900

    update 가장 가까운 같은 글자

--- Programmers/Lv1/연습문제/가장 가까운 같은 글자.py
+++ Programmers/Lv1/연습문제/가장 가까운 같은 글자.py
@@ -1,4 +1,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
+
+
+def solution(s):
+    answer = []
+    dic1 = dict()
+    for i in range(len(s)):
+        if s[i] in dic1:
+            answer.append(i - dic1[s[i]])
+        else:
+            answer.append(-1)  # 이전에 없던 글자면 -1
+        dic1[s[i]] = i
+    return answer
+
+
+print(solution("banana"))
+print(solution("foobar"))
+
 ''' 완전 오답, 문제 이해를 잘못함
 def solution(s):
     answer = []
@@ -12,12 +29,3 @@ def solution(s):
 
     return answer
 '''
-
-
-def solution(s):
-    answer = []
-    return answer
-
-
-print(solution("banana"))
-print(solution("foobar"))
