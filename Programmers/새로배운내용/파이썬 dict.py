40e89bca48bc83766d1eabaedea42b85bf742d08
tjrwls0826 <tjrwls0826@naver.com>
2022-12-22 20:58:32 +0900

    Create 파이썬 dict.py

---
+++ Programmers/새로배운내용/파이썬 dict.py
@@ -0,0 +1,16 @@
+# Source: https://blog.naver.com/indra494/222886972407
+
+# 딕셔너리는 key, value 방식
+# 빠른 탐색과 사용에 용이
+
+dic01 = {}
+dic01['name'] = '김철수'
+dic01['id'] = 'kim'
+dic01['age'] = 20
+print('dic01:', dic01)
+
+dic02 = dict()
+dic02['name'] = '유재민'
+dic02['id'] = 'jaemin'
+dic02['age'] = 13
+print('dic02:', dic02)
