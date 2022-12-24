74140075e44d32d7ceb1e4fc6af6c724fca44fed
tjrwls0826 <tjrwls0826@naver.com>
2022-12-24 23:14:07 +0900

    제일 작은 수 제거하기 에러 수정
    에러 수정

--- Programmers/Lv1/연습문제/제일 작은 수 제거하기.py
+++ Programmers/Lv1/연습문제/제일 작은 수 제거하기.py
@@ -1,6 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12935
 
-
+'''
 def solution(arr):
     x = min(arr)
     for i in range(len(arr)):
@@ -11,8 +11,18 @@ def solution(arr):
         return [-1]
 
     return arr
+'''
 
 
-print(solution([4, 3, 2, 1,1]))
+def solution(arr):
+    x = min(arr)
+    while x in arr:
+        arr.remove(x)
+    if arr == []:
+        return [-1]
+    return arr
+
+
+print(solution([4, 3, 2, 1, 1]))
 print(solution([10]))
 print(solution([2, 3, 5, 8]))
