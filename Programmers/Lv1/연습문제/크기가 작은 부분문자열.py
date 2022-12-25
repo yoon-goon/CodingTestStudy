4cf8c6cfad1b7068848009f97a6703e590ca57ee
tjrwls0826 <tjrwls0826@naver.com>
2022-12-25 13:35:28 +0900

    Update 크기가 작은 부분문자열.py

--- Programmers/Lv1/연습문제/크기가 작은 부분문자열.py
+++ Programmers/Lv1/연습문제/크기가 작은 부분문자열.py
@@ -3,15 +3,13 @@
 def solution(t, p):
     count = 0
     newarr = []
-    for i in range(len(t)-2):
-        newarr.append(int(t[i:i+3]))
-        if newarr[i] <= int(p):
+    for i in range(len(t) - (len(p)-1)):
+        newarr.append(t[i:i + len(p)])
+        if newarr[i] <= p:
             count += 1
     return count
 
+
 print(solution("3141592","271"))
 print(solution("500220839878","7"))
-
-def test():
-    if "500" > "007":
-        return 1
+print(solution("10203", "15"))
