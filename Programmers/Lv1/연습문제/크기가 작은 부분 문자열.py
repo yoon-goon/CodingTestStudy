b79fe3a2248cb641b6229eb4809af7ec55f49df3
tjrwls0826 <tjrwls0826@naver.com>
2022-12-25 13:36:57 +0900

    Update 크기가 작은 부분 문자열.py

--- Programmers/Lv1/연습문제/크기가 작은 부분 문자열.py
+++ Programmers/Lv1/연습문제/크기가 작은 부분 문자열.py
@@ -3,7 +3,7 @@
 def solution(t, p):
     count = 0
     newarr = []
-    for i in range(len(t) - (len(p)-1)):
+    for i in range(len(t) - (len(p)-1)): # p의 길이에 따라 반복해야할 횟수가 달라지기에
         newarr.append(t[i:i + len(p)])
         if newarr[i] <= p:
             count += 1
