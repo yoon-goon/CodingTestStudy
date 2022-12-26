e4e4e73f4d75fdb0f95da13b3bcbef691a2432ee
tjrwls0826 <tjrwls0826@naver.com>
2022-12-26 23:27:16 +0900

    푸드 파이트 대회 채점완료

--- Programmers/Lv1/연습문제/푸드 파이트 대회.py
+++ Programmers/Lv1/연습문제/푸드 파이트 대회.py
@@ -2,11 +2,10 @@
 
 def solution(food):
     answer = ''
-    newanswer = ''
     for i in range(len(food)):
         x = int(food[i]/2)
         for y in range(x):
-            answer = answer+str(food.index(food[i])) # food[i]의 값이 아닌 몇번째인지가 중요함으로 index 사용
+            answer = answer+str(i) # food[i]의 값이 아닌 몇번째인지가 중요함으로 index 사용 # index도 사용할 필요 없이 i만 사용하면 됨
     realanswer = answer + str(0)
     ''' #풀이 과정중에 사용했으나 마지막에 슬라이싱을 통해 뒤집는 방식을 사용해 의미없어짐
     for i in range(len(food)):
@@ -17,6 +16,6 @@ def solution(food):
     newanswer = answer[::-1]
     return realanswer + newanswer
 
-#print(solution([1, 3, 4, 6]))
-#print(solution([1, 7, 1, 2]))
+print(solution([1, 3, 4, 6]))
+print(solution([1, 7, 1, 2]))
 print(solution([1,3,2,3]))
