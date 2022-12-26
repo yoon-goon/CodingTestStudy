4561f0e730e65ba5f2e2851df811a8578e73939d
tjrwls0826 <tjrwls0826@naver.com>
2022-12-26 23:05:40 +0900

    Update 푸드 파이트 대회.py

--- Programmers/Lv1/연습문제/푸드 파이트 대회.py
+++ Programmers/Lv1/연습문제/푸드 파이트 대회.py
@@ -7,12 +7,14 @@ def solution(food):
         x = int(food[i]/2)
         for i in range(x):
             answer = answer+str(x)
+            ''' #풀이 과정중에 사용했으나 마지막에 슬라이싱을 통해 뒤집는 방식을 사용해 의미없어짐
     answer = answer + str(0)
     for i in range(len(food)):
         x = int(food[i]/2)
         for i in range(x):
             newanswer = newanswer+str(x)
-    newanswer = str(reversed(newanswer))
-    return answer + newansweri
+            '''
+    newanswer = answer[::-1]
+    return answer + newanswer
 
 print(solution([1, 3, 4, 6]))
