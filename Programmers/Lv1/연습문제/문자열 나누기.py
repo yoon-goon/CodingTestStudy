466ab66964996499256c582a519ac713e3c6fb18
tjrwls0826 <tjrwls0826@naver.com>
2022-12-31 22:03:47 +0900

    Update 문자열 나누기.py

--- Programmers/Lv1/연습문제/문자열 나누기.py
+++ Programmers/Lv1/연습문제/문자열 나누기.py
@@ -7,17 +7,19 @@ def solution(s):
     count2 = 0
     count3 = 0
     for i in range(len(s)):
+        if count1 == count2:
+            answer = answer + " "
+            letter = s[i]
+            count3 += 1
         if s[i] == letter:
             count1 += 1
             answer = answer + s[i]
         else:
             count2 += 1
             answer = answer + s[i]
-        if count1 == count2:
-            answer = answer + " "
-            letter = s[i]
-            count3 += 1
+
     return answer + str(count3)
+
 
 print(solution("banana"))
 print(solution("abracadabra"))
