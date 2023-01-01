af66a9d402e499d18c258f3d0e273080926a3888
tjrwls0826 <tjrwls0826@naver.com>
2023-01-01 22:49:09 +0900

    Update 음양 더하기.py

--- Programmers/Lv1/연습문제/음양 더하기.py
+++ Programmers/Lv1/연습문제/음양 더하기.py
@@ -1,9 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/76501
-
+'''
 true = 1
 false = -1
+'''
 
 
+# 파이참에서는 돌아가나 채점 불가
 def solution(absolutes, signs):
     answer = 0
     for i in range(len(absolutes)):
@@ -13,4 +15,15 @@ def solution(absolutes, signs):
     return sum(absolutes)
 
 
-print(solution([4, 7, 12], [true, false, true]))
+# 파이참에서는 안돌아가지만 채점 가능
+def solution2(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
+
+
+print(solution2([4, 7, 12], [true, false, true]))
