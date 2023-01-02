29dd17d8dffe2c55cb290aa31731cc69d2666253
tjrwls0826 <tjrwls0826@naver.com>
2023-01-02 20:30:46 +0900

    Update 기사단원의 무기.py

--- Programmers/Lv1/연습문제/기사단원의 무기.py
+++ Programmers/Lv1/연습문제/기사단원의 무기.py
@@ -23,14 +23,14 @@ print(solution(15, 3, 2))
 
 def solution2(number, limit, power):
     answer = 0
-    for i in range(1, number + 1): # i 는 기사의 번호
+    for i in range(1, number + 1):  # i 는 기사의 번호
         count = 0
-        for y in range(1, int(i ** (1 / 2)) + 1): # y는 1~ 기사번호제곱근 까지 for
-            if i % y == 0: # 약수일경우 일단 1 카운트
+        for y in range(1, int(i ** (1 / 2)) + 1):  # y는 1~ 기사번호제곱근 까지 for
+            if i % y == 0:  # 약수일경우 일단 1 카운트
                 count += 1
-                if y ** 2 != i: # 제곱이 되는 약수라면 중복됨으로 한번만 새야하고 아니라면 2번
+                if y ** 2 != i:  # 제곱이 되는 약수라면 중복됨으로 한번만 새야하고 아니라면 2번
                     count += 1
-            if count > limit:
+            if count > limit:  # 계산된 무기의 파워가 리밋을 넘을경우 값은 power
                 count = power
         answer += count
     return answer
@@ -39,4 +39,4 @@ def solution2(number, limit, power):
 print(solution2(5, 3, 2))
 print(solution2(10, 3, 2))
 print(solution2(15, 3, 2))
-print(solution2(80000, 3, 2))
+print(solution2(80000, 3, 2))  # 제곱근을 이용했기 때문에 큰수도 오랜시간이 걸리지 않음
