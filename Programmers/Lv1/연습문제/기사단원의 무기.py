9b63740b19db42383410c90de61f35f6c7f63f33
tjrwls0826 <tjrwls0826@naver.com>
2023-01-02 20:17:01 +0900

    Update 기사단원의 무기.py

--- Programmers/Lv1/연습문제/기사단원의 무기.py
+++ Programmers/Lv1/연습문제/기사단원의 무기.py
@@ -3,9 +3,9 @@
 def solution(number, limit, power):
     answer = []
     count = 0
-    for i in range(1,number+1):
-        for y in range(1,i+1):
-            if i%y==0:
+    for i in range(1, number + 1):
+        for y in range(1, i + 1):
+            if i % y == 0:
                 count += 1
             if count > limit:
                 count = power
@@ -14,6 +14,10 @@ def solution(number, limit, power):
     return sum(answer)
 
 
-print(solution(5,3,2))
-print(solution(10,3,2))
-print(solution(1,0,2))
+print(solution(5, 3, 2))
+print(solution(10, 3, 2))
+print(solution(15, 3, 2))
+#print(solution(80000, 3, 2)) # 값이 커질경우 계산에 너무 오랜 시간이 소요됨 수정 필요
+
+def solution2(number,limit,power):
+    count = 0
