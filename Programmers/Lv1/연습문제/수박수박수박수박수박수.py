c3c55c86754e35173e6860fd74d2a04f1db1e1c5
tjrwls0826 <tjrwls0826@naver.com>
2023-01-04 18:11:14 +0900

    Update 수박수박수박수박수박수.py

--- Programmers/Lv1/연습문제/수박수박수박수박수박수.py
+++ Programmers/Lv1/연습문제/수박수박수박수박수박수.py
@@ -2,11 +2,18 @@
 
 def solution(n):
     answer = ''
-    for i in range(1,n+1):
-        if i%2 != 0:
+    for i in range(1, n + 1):
+        if i % 2 != 0:
             answer = answer + ("수")
         else:
             answer = answer + "박"
     return answer
 
+
+def solution2(n):
+    return "수박" * (n // 2) + "수" * (n % 2)
+
+
 print(solution(3))
+print(solution2(3))
+print(solution2(4))
