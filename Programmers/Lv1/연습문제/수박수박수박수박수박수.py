5b2670bdf595a74d5bc92144c2bd467a143ab306
tjrwls0826 <tjrwls0826@naver.com>
2023-01-04 18:08:34 +0900

    Update 수박수박수박수박수박수.py

--- Programmers/Lv1/연습문제/수박수박수박수박수박수.py
+++ Programmers/Lv1/연습문제/수박수박수박수박수박수.py
@@ -2,10 +2,11 @@
 
 def solution(n):
     answer = ''
-    for i in range(len(n)):
+    for i in range(1,n+1):
         if i%2 != 0:
-            answer.append("수")
+            answer = answer + ("수")
         else:
-            answer.append("박")
+            answer = answer + "박"
     return answer
 
+print(solution(3))
