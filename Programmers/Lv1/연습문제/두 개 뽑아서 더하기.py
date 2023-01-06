ea1c72f321b7f42ed578ccb891b79a653c1808c4
tjrwls0826 <tjrwls0826@naver.com>
2023-01-06 20:58:41 +0900

    Update 두 개 뽑아서 더하기.py

--- Programmers/Lv1/연습문제/두 개 뽑아서 더하기.py
+++ Programmers/Lv1/연습문제/두 개 뽑아서 더하기.py
@@ -1,10 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 
-def solution(numbers): #수정 필요
+def solution(numbers):
     answer = []
     for i in range(len(numbers)):
         for y in numbers[i+1:]:
-            answer.append(numbers[i] + y)
+            if not numbers[i]+y in answer:
+                answer.append(numbers[i] + y)
+
     return sorted(answer)
 
 
