45790211ae07b7e03d6cf68c5374365dab673877
tjrwls0826 <tjrwls0826@naver.com>
2023-01-06 21:23:59 +0900

    Update 직사각형 별찍기.py

--- Programmers/Lv1/연습문제/직사각형 별찍기.py
+++ Programmers/Lv1/연습문제/직사각형 별찍기.py
@@ -7,3 +7,18 @@ for i in range(b):
         line += "*"
     print(line)
     line = ""
+
+''' 이런 풀이도 존재
+a, b = map(int, input().strip().split(' '))
+answer = ('*'*a +'\n')*b
+print(answer)
+'''
+'''
+a, b = map(int, input().strip().split(' '))
+
+for i in range(b):
+    for j in range(a):
+        print('*', end='') # end를 이용해 print 출력후 한줄 띄워지는걸 방지 가능(''로 대체되는 예시)
+    print('')
+    
+'''
