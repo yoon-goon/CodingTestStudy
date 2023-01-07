7f4525d02a1d30929ea959f18978e895e7aec8e0
tjrwls0826 <tjrwls0826@naver.com>
2023-01-07 20:32:07 +0900

    Update 문자열 다루기 기본.py

--- Programmers/Lv1/연습문제/문자열 다루기 기본.py
+++ Programmers/Lv1/연습문제/문자열 다루기 기본.py
@@ -1,7 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12918
 
 def solution(s):
-    if 4 <= len(s) <= 6:
+    if len(s) == 4 or len(s) == 6:
         try:
             int(s)
             return True
