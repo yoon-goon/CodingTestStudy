c077ffe239e527b4acb141a1876ad877fcebb9f7
tjrwls0826 <tjrwls0826@naver.com>
2023-01-09 14:25:35 +0900

    Update 행렬의 덧셈.py

--- Programmers/Lv1/연습문제/행렬의 덧셈.py
+++ Programmers/Lv1/연습문제/행렬의 덧셈.py
@@ -1,11 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12950
 
 def solution(arr1, arr2):
-    answer = []
     for i in range(len(arr1)):
-        for y in range(len(arr2)):
-            answer.append(arr1[i][y] + arr2[i][y])
+        for y in range(len(arr1[0])):
+            arr1[i][y] += arr2[i][y]
 
-    return answer
+    return arr1
 
 print(solution([[1,2],[2,3]],[[3,4],[5,6]]))
