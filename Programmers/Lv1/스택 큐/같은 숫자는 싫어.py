408def678d81ac617788490f206b90a9221da866
tjrwls0826 <tjrwls0826@naver.com>
2023-01-10 23:02:23 +0900

    Create 같은 숫자는 싫어.py

---
+++ Programmers/Lv1/스택 큐/같은 숫자는 싫어.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12906
+
+def solution(arr):
+    answer = []
+    for i in range(1,len(arr)):
+        if arr[i] == arr[i-1]:
+            
+    return answer
