3fa071bd09860433a8bd804d00d05de1c43e7c45
tjrwls0826 <tjrwls0826@naver.com>
2023-01-10 23:19:44 +0900

    Update 같은 숫자는 싫어.py

--- Programmers/Lv1/스택 큐/같은 숫자는 싫어.py
+++ Programmers/Lv1/스택 큐/같은 숫자는 싫어.py
@@ -1,5 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12906
 
+'''
 def solution(arr):
     answer = arr
     i = 0
@@ -8,5 +9,16 @@ def solution(arr):
             answer.pop(i)
         i += 1
     return answer
+'''
 
-print(solution([1,1,3,3,0,1,1]))
+
+def solution(arr):
+    answer = []
+    i = 0
+    while i <= len(arr) - 1:
+        if arr[i] != arr[i + 1]:
+            answer.append(arr[i])
+        i += 1
+
+
+print(solution([1, 1, 3, 3, 0, 1, 1]))
