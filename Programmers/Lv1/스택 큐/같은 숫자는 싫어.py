0dd90a566cd1e382b178fd562febed5b6568b09e
tjrwls0826 <tjrwls0826@naver.com>
2023-01-10 23:30:53 +0900

    Update 같은 숫자는 싫어.py

--- Programmers/Lv1/스택 큐/같은 숫자는 싫어.py
+++ Programmers/Lv1/스택 큐/같은 숫자는 싫어.py
@@ -12,13 +12,16 @@ def solution(arr):
 '''
 
 
-def solution(arr):
+def solution(arr): # 코드 실행은 됬지만 채점이 진행되지 않음 개선필요
     answer = []
-    i = 0
-    while i <= len(arr) - 1:
-        if arr[i] != arr[i + 1]:
+    arr.append(10)
+    i = 1
+    while i < len(arr)-1:
+        if arr[i] != arr[i+1]:
             answer.append(arr[i])
         i += 1
+    return answer
 
 
 print(solution([1, 1, 3, 3, 0, 1, 1]))
+print(solution([4,4,4,3,3]))
