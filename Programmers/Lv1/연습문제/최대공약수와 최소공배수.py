6c1cbec3449498aea7b12ed90a1e4d7dc2e0f4e6
tjrwls0826 <tjrwls0826@naver.com>
2023-01-12 00:51:22 +0900

    Update 최대공약수와 최소공배수.py

--- Programmers/Lv1/연습문제/최대공약수와 최소공배수.py
+++ Programmers/Lv1/연습문제/최대공약수와 최소공배수.py
@@ -2,23 +2,24 @@
 
 def solution(n, m):
     answer = []
-    arr1=[]
-    arr2=[]
-    for i in range(1,n+1):
-        if n%i == 0:
+    arr1 = []
+    arr2 = []
+    for i in range(1, n + 1):  # 공약수를 구하는 과정
+        if n % i == 0:
             arr1.append(i)
-    for y in range(1,m+1):
-        if m%y==0:
+    for y in range(1, m + 1):
+        if m % y == 0:
             arr2.append(y)
     for x in arr1:
         if x in arr2:
             answer.append(x)
     answer.remove(1)
 
-    for i in range(1,1000001):
-        if i%n==0 and i%m ==0:
+    for i in range(1, 1000001):  # 공배수를 구하는 과정
+        if i % n == 0 and i % m == 0:
             answer.append(i)
             return answer
     return answer
 
-print(solution(3,12))
+
+print(solution(3, 12))
