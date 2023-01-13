2d1267647a1d976ba1341e24d38e8060f35b6cc8
tjrwls0826 <tjrwls0826@naver.com>
2023-01-13 22:03:16 +0900

    Update 3진법 뒤집기.py

--- Programmers/Lv1/월간 코드 챌린지 시즌1/3진법 뒤집기.py
+++ Programmers/Lv1/월간 코드 챌린지 시즌1/3진법 뒤집기.py
@@ -1,9 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68935
 
 def solution(n):
-    answer = 0
+    answer = ''
+    count = 1
+    while n >= 3:
+        n = n - 3 ** count
+        count += 1
+        if n % 3 == 0:
+            answer = answer + "0"
+        else:
+            answer = answer + "1"
     return answer
-
 
 
 print(solution(45))
