1efd7de8927816ba929aa76619aaf3b77af307f7
tjrwls0826 <tjrwls0826@naver.com>
2023-01-14 21:45:16 +0900

    Update 3진법 뒤집기.py

--- Programmers/Lv1/월간 코드 챌린지 시즌1/3진법 뒤집기.py
+++ Programmers/Lv1/월간 코드 챌린지 시즌1/3진법 뒤집기.py
@@ -6,9 +6,8 @@ def solution(n):
         n, b = divmod(n, 3)  # divmod로 몫과 나머지를 동시에 구함
         answer = answer + str(b)
     answer = (answer + str(n))  # [::-1] 원래 구하기 위해선 앞뒤반전을 하는 과정을 거쳐야하지만 과정에서 한번 뒤집으라고 했기에 생략
-    answer = int(answer, 3)
 
-    return answer
+    return int(answer, 3)
 
 
 print(solution(45))
