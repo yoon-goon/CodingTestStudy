8fa2f2ef0593d9eeb3db3942935dd0099cf7d6bf
tjrwls0826 <tjrwls0826@naver.com>
2023-01-14 21:53:51 +0900

    Update 예산.py

--- Programmers/Lv1/Summer,Winter Coding(~2018)/예산.py
+++ Programmers/Lv1/Summer,Winter Coding(~2018)/예산.py
@@ -2,5 +2,13 @@
 
 
 def solution(d, budget):
-    answer = 0
-    return answer
+    count = 0
+    d = sorted(d)
+    for i in range(len(d)):
+        if budget >= d[i]:
+            budget = budget - d[i]
+            count += 1
+    return count
+
+
+print(solution([1,3,2,5,4],9))
