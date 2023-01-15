153eb934cde4a26c406918348fc97c1de6272f36
tjrwls0826 <tjrwls0826@naver.com>
2023-01-15 23:27:14 +0900

    Update K번째수.py

--- Programmers/Lv1/정렬/K번째수.py
+++ Programmers/Lv1/정렬/K번째수.py
@@ -2,4 +2,12 @@
 
 def solution(array, commands):
     answer = []
-    return answer
+    temp = []
+    for i in range(len(commands)):
+        temp.append( array[commands[i][0]:commands[i][1]+1])
+
+        #print(temp[commands[i][2]])
+    return temp
+
+
+print(solution([1,5,2,6,3,7,4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
