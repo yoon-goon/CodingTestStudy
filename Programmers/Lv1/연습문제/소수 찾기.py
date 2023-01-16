7bbacaf0929374a7c12ed77343c215865c4670a5
tjrwls0826 <tjrwls0826@naver.com>
2023-01-16 23:55:35 +0900

    Update 소수 찾기.py

--- Programmers/Lv1/연습문제/소수 찾기.py
+++ Programmers/Lv1/연습문제/소수 찾기.py
@@ -2,16 +2,18 @@
 
 def solution(n):
     answer = []
+    if n >= 2:
+        answer.append(2)
 
     for i in range(1, n + 1):
         for y in range(2, i):
-            if i == 2:
-                answer.append(2)
             if i % y == 0:
-                break;
-            answer.append(i)
+                break
+
+        answer.append(i)
 
     return answer
 
 
 print(solution(10))
+print(solution(5))
