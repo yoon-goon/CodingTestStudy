fbb3dbde00bbf711772ae22af2f8eee58b7c709d
tjrwls0826 <tjrwls0826@naver.com>
2023-01-17 00:35:28 +0900

    Update 소수 찾기.py

--- Programmers/Lv1/연습문제/소수 찾기.py
+++ Programmers/Lv1/연습문제/소수 찾기.py
@@ -1,12 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
-def solution(n):
+def solution(n):  # 시간 초과
     answer = []
     count = 0
     for i in range(1, n + 1):
-        for y in range(i, 1, -1):
+        for y in range(1, i, 1):
             if i % y == 0:
                 count += 1
+            if count >= 2:
+                break
         if count == 1:
             answer.append(i)
         count = 0
@@ -14,6 +16,25 @@ def solution(n):
     return len(answer)
 
 
-print(solution(10))
-print(solution(7))
-print(solution(5))
+def solution2(n):
+    answer = []
+
+    for i in range(1, n + 1):
+        if i == 1: continue
+        count = 0
+
+        for y in range(2, int(i ** (1 / 2)) + 1):
+            if i % y == 0:
+                count = 1
+                break
+
+        if count == 0:
+            answer.append(i)
+        count = 0
+
+    return len(answer)
+
+
+print(solution2(10))
+# print(solution2(7))
+# print(solution2(5))
