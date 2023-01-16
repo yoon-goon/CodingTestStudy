cb8236adeda362c320fb80ca2e8fc19e4596ec08
tjrwls0826 <tjrwls0826@naver.com>
2023-01-17 00:13:01 +0900

    Update 소수 찾기.py
    실행은 되나 시간복잡도가 문제

--- Programmers/Lv1/연습문제/소수 찾기.py
+++ Programmers/Lv1/연습문제/소수 찾기.py
@@ -2,19 +2,18 @@
 
 def solution(n):
     answer = []
-    if n >= 2:
-        answer.append(2)
-
+    count = 0
     for i in range(1, n + 1):
-        for y in range(i-1, 1,-1):
+        for y in range(i, 1, -1):
             if i % y == 0:
-                break;
+                count += 1
+        if count == 1:
+            answer.append(i)
+        count = 0
 
-
-
-
-    return answer
+    return len(answer)
 
 
 print(solution(10))
+print(solution(7))
 print(solution(5))
