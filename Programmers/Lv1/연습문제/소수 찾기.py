37fe6d3e4c8e0abc4908761a78c023770cadf2fc
tjrwls0826 <tjrwls0826@naver.com>
2023-01-17 00:42:41 +0900

    Update 소수 찾기.py

--- Programmers/Lv1/연습문제/소수 찾기.py
+++ Programmers/Lv1/연습문제/소수 찾기.py
@@ -8,7 +8,7 @@ def solution(n):  # 시간 초과
             if i % y == 0:
                 count += 1
             if count >= 2:
-                break
+                break  # break는 반복문을 끝냄
         if count == 1:
             answer.append(i)
         count = 0
@@ -19,8 +19,9 @@ def solution(n):  # 시간 초과
 def solution2(n):
     answer = []
 
-    for i in range(1, n + 1):
-        if i == 1: continue
+    for i in range(2, n + 1):
+        #if i == 1: continue  # continue 는 아래부분을 실행하지 않고 넘김 /
+        # i를 2부터 시작해도 되고 if i == 1: continue로 생략을 해도 됨
         count = 0
 
         for y in range(2, int(i ** (1 / 2)) + 1):
