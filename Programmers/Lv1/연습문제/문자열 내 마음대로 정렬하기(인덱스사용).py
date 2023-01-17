d22010e2ca4cb4001ad8a117ee709e6285985077
tjrwls0826 <tjrwls0826@naver.com>
2023-01-18 00:22:48 +0900

    Update 문자열 내 마음대로 정렬하기(인덱스사용).py

--- Programmers/Lv1/연습문제/문자열 내 마음대로 정렬하기(인덱스사용).py
+++ Programmers/Lv1/연습문제/문자열 내 마음대로 정렬하기(인덱스사용).py
@@ -1,6 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12915
 
 def solution(strings, n):
+    arr = []
     answer = []
+    for i in strings:
+        arr.append(i[n]+i)
+    arr.sort()
+    for i in arr:
+        answer.append(i[1:])
+
     return answer
 
+
+
+
+print(solution(["sun", "bed", "car"], 1))
