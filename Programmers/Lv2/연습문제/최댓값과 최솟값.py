bd5f189b2db4d182f4ba0080c9054f604ace93f7
tjrwls0826 <tjrwls0826@naver.com>
2023-01-21 04:11:20 +0900

    Update 최댓값과 최솟값.py

--- Programmers/Lv2/연습문제/최댓값과 최솟값.py
+++ Programmers/Lv2/연습문제/최댓값과 최솟값.py
@@ -3,14 +3,20 @@
 def solution(s):
     numlist = []
     answer = s.split(" ")
-    print(answer)
     for i in answer:
         numlist.append(int(i))
 
+    return str(min(numlist)) + " " + str(max(numlist))
 
-    return str(min(numlist))+" "+str(max(numlist))
+def solution2(s):
+    answer = s.split(" ")
+    answer = list(map(int,answer))
+    return answer
 
 
 print(solution("1 2 3 4"))
 print(solution("-1 -2 -3 -4"))
 print(solution("-1 -1"))
+print(solution2("1 2 3 4"))
+print(solution2("-1 -2 -3 -4"))
+print(solution2("-1 -1"))
