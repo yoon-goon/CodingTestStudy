4488e2c15b95f61d4b7c193fbecb572f320e9320
tjrwls0826 <tjrwls0826@naver.com>
2023-01-23 04:59:59 +0900

    Update JadenCase 문자열 만들기.py

--- Programmers/Lv2/연습문제/JadenCase 문자열 만들기.py
+++ Programmers/Lv2/연습문제/JadenCase 문자열 만들기.py
@@ -2,4 +2,11 @@
 
 def solution(s):
     answer = ''
-    return answer
+    tmp = s.split(' ')
+    for i in range(len(tmp)):
+        tmp[i] = tmp[i].capitalize()
+    return ' '.join(tmp)
+
+
+print(solution("3people unFollowed me"))
+print(solution("for the last week"))
