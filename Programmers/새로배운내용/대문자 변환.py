493a2e077316a530c8135d5c208d56c8c8c24b58
tjrwls0826 <tjrwls0826@naver.com>
2023-01-23 05:00:01 +0900

    Create 대문자 변환.py

---
+++ Programmers/새로배운내용/대문자 변환.py
@@ -0,0 +1,15 @@
+A = 'abcd'
+print(A.upper())
+print(A.capitalize())
+print(A.title())
+
+c = 'a2b3C4'
+print(c.upper())
+print(c.capitalize())
+print(c.title())
+
+d = 'abc-def gh'
+print(d.upper())
+print(d.capitalize())
+print(d.title())
+
