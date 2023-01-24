5febaae2399a458c05e708a20740597002ae58ea
tjrwls0826 <tjrwls0826@naver.com>
2023-01-25 01:19:45 +0900

    Update 최솟값 만들기.py

--- Programmers/Lv2/연습문제/최솟값 만들기.py
+++ Programmers/Lv2/연습문제/최솟값 만들기.py
@@ -1,7 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12941
 
-def solution(A,B):
+def solution(A, B):
     answer = 0
-
+    A.sort()
+    B.sort(reverse=True)
+    for i in range(len(A)):
+        answer += (A[i] * B[i])
 
     return answer
+
+
+print(solution([1, 4, 2], [5, 4, 4]))
