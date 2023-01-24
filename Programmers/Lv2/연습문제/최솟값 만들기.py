a0168e84f7d204a83d65718cbfe39f227884c9fc
tjrwls0826 <tjrwls0826@naver.com>
2023-01-25 01:21:16 +0900

    Update 최솟값 만들기.py

--- Programmers/Lv2/연습문제/최솟값 만들기.py
+++ Programmers/Lv2/연습문제/최솟값 만들기.py
@@ -3,7 +3,7 @@
 def solution(A, B):
     answer = 0
     A.sort()
-    B.sort(reverse=True)
+    B.sort(reverse=True) # 한개는 오름차순, 내림차순으로 한뒤 각 인덱스끼리 곱하면 최솟값
     for i in range(len(A)):
         answer += (A[i] * B[i])
 
@@ -11,3 +11,4 @@ def solution(A, B):
 
 
 print(solution([1, 4, 2], [5, 4, 4]))
+print(solution([1, 2], [3, 4]))
