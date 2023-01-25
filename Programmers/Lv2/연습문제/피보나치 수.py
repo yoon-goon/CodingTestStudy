50fa2d5247edd6026d74d68e9bd561cbcc8512f7
tjrwls0826 <tjrwls0826@naver.com>
2023-01-26 00:04:37 +0900

    Update 피보나치 수.py

--- Programmers/Lv2/연습문제/피보나치 수.py
+++ Programmers/Lv2/연습문제/피보나치 수.py
@@ -4,7 +4,7 @@
 def solution(n):
     current, nextnum = 0, 1
     for i in range(n):
-        current, nextnum = nextnum, current + nextnum
+        current, nextnum = nextnum, current + nextnum # 2 계산이 동시에 이뤄저야 하므로 ,로 같은줄에 작성
     return current
 
 
