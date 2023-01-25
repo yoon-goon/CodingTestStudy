4b126c2210219d5b618a02074a0c1d561619ccdd
tjrwls0826 <tjrwls0826@naver.com>
2023-01-26 00:02:04 +0900

    Update 피보나치 수.py

--- Programmers/Lv2/연습문제/피보나치 수.py
+++ Programmers/Lv2/연습문제/피보나치 수.py
@@ -1,5 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12945
 
+
 def solution(n):
-    answer = 0
-    return answer
+    current, next = 0, 1
+    for i in range(n):
+        current, next = next, current + next
+    return current
+
+
+print(solution(3))
+print(solution(5))
