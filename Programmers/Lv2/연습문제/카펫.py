90ba45632d4460cd79902177bad58594759eecc9
tjrwls0826 <tjrwls0826@naver.com>
2023-01-27 04:30:28 +0900

    Update 카펫.py

--- Programmers/Lv2/연습문제/카펫.py
+++ Programmers/Lv2/연습문제/카펫.py
@@ -4,10 +4,14 @@ def solution(brown, yellow):
     answer = []
     all = brown + yellow
     width = 2
-    length = 0
+    length = 1
     while width > length:
-        length += 1
-        width = int(all / (length))
+        if all % length == 0:
+            length += 1
+            width = int(all / (length))
+        else:
+            length += 1
+            continue
 
     return [max(width,length), min(width,length)]
 
