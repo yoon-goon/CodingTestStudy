80d0efd61f951f4dcc1e50524fb3fccf373d8648
tjrwls0826 <tjrwls0826@naver.com>
2023-01-27 02:37:10 +0900

    Update 피보나치 수.py

--- Programmers/Lv2/연습문제/피보나치 수.py
+++ Programmers/Lv2/연습문제/피보나치 수.py
@@ -12,12 +12,12 @@ def better_solution(n):
     current, nextnum = 0, 1
     for i in range(n):
         current, nextnum = nextnum, current + nextnum  # 2 계산이 동시에 이뤄저야 하므로 ,로 같은줄에 작성
-    return current % 1234567 # 문제의 내용에도 써있었고 %1234567을 통해 런타임 에러를 해결할 수있음
+    return current % 1234567  # 문제의 내용에도 써있었고 %1234567을 통해 런타임 에러를 해결할 수있음
+
+
 '''
 https://mozzioi.tistory.com/147
-
 '''
-
 
 print(better_solution(3))
 print(better_solution(5))
