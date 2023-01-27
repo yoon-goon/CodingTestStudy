c7e875aa8be996965cefa4cd2cf1655b0942753d
tjrwls0826 <tjrwls0826@naver.com>
2023-01-28 03:49:27 +0900

    Update 카펫 미해결.py

--- Programmers/Lv2/연습문제/카펫 미해결.py
+++ Programmers/Lv2/연습문제/카펫 미해결.py
@@ -16,12 +16,13 @@ def solution(brown, yellow): #잘못된 알고리즘, 새로 작성해야함
 
     return [max(width,length), min(width,length)]
 '''
+
+
 def solution(brown, yellow):
     answer = []
-    _all = brown+yellow
-
+    _all = brown + yellow
 
 
 print(solution(10, 2))
-print(solution(8,1))
-print(solution(24,24))
+print(solution(8, 1))
+print(solution(24, 24))
