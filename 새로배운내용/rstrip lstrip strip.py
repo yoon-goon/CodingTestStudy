3a595d62965db7578772a340d0fab0bc1ed05a13
tjrwls0826 <tjrwls0826@naver.com>
2023-01-29 07:32:24 +0900

    Create rstrip lstrip strip.py

---
+++ 새로배운내용/rstrip lstrip strip.py
@@ -0,0 +1,24 @@
+# https://wikidocs.net/13#rstrip
+
+'''
+왼쪽 공백 지우기(lstrip)
+>>> a = " hi "
+>>> a.lstrip()
+'hi '
+
+문자열 중 가장 왼쪽에 있는 한 칸 이상의 연속된 공백들을 모두 지운다. lstrip에서 l은 left를 의미한다.
+
+오른쪽 공백 지우기(rstrip)
+>>> a= " hi "
+>>> a.rstrip()
+' hi'
+
+문자열 중 가장 오른쪽에 있는 한 칸 이상의 연속된 공백을 모두 지운다. rstrip에서 r는 right를 의미한다.
+
+양쪽 공백 지우기(strip)
+>>> a = " hi "
+>>> a.strip()
+'hi'
+'''
+#문자열 양쪽에 있는 한 칸 이상의 연속된 공백을 모두 지운다.
+
