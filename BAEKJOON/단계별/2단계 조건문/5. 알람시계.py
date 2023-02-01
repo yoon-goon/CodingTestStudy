b9fb3b70c75275e40572a78de5d6fdd7db0d86f6
tjrwls0826 <tjrwls0826@naver.com>
2023-02-02 05:58:00 +0900

    Update 5. 알람시계.py

--- BAEKJOON/단계별/2단계 조건문/5. 알람시계.py
+++ BAEKJOON/단계별/2단계 조건문/5. 알람시계.py
@@ -3,5 +3,12 @@
 hour, _min = map(int, input().split())
 
 if _min - 45 < 0:
-    hour -= 1
+    if hour -1 < 0:
+        hour = 23
+    else:
+        hour -= 1
     _min = 60 + (_min - 45)
+else:
+    _min -= 45
+
+print(hour, _min)
