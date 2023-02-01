9877fbf7dc32400fb444169fc7923df4c58c5f0a
tjrwls0826 <tjrwls0826@naver.com>
2023-02-02 06:03:01 +0900

    Update 6. 오븐 시계.py

--- BAEKJOON/단계별/2단계 조건문/6. 오븐 시계.py
+++ BAEKJOON/단계별/2단계 조건문/6. 오븐 시계.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/2525
+
+cur_hour, cur_min = map(int, input().split())
+time = int(input())
+
+cur_min += time
+while cur_min >= 60:
+    cur_hour += 1
+    cur_min -= 60
+if cur_hour >= 24:
+    cur_hour -= 24
+
+print(cur_hour, cur_min)
