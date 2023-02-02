b3b07df90c0a0a9d261daf13b22ac762fbbe57ea
tjrwls0826 <tjrwls0826@naver.com>
2023-02-03 03:32:43 +0900

    Update 7. 주사위 세개.py

--- BAEKJOON/단계별/2단계 조건문/7. 주사위 세개.py
+++ BAEKJOON/단계별/2단계 조건문/7. 주사위 세개.py
@@ -4,7 +4,7 @@ a, b, c = map(int, input().split())
 
 if a == b == c:
     print(10000 + a * 1000)
-elif a != b != c:
+elif a != b and a != c and b != c:
     print(max(a, b, c) * 100)
 else:
     if a == b:
