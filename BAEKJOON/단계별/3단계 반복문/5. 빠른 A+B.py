8c0cb7f19b407a76d217e17a4fd588ae3baa65da
tjrwls0826 <tjrwls0826@naver.com>
2023-02-05 12:09:12 +0900

    Update 5. 빠른 A+B.py

--- BAEKJOON/단계별/3단계 반복문/5. 빠른 A+B.py
+++ BAEKJOON/단계별/3단계 반복문/5. 빠른 A+B.py
@@ -1,7 +1,8 @@
 # https://www.acmicpc.net/problem/15552
 import sys
 
+time = int(sys.stdin.readline())
 
-a = sys.stdin.readline()
-
-print(a)
+for i in range(time):  # 반복문으로 여러줄 입력받는 상황에서는 반드시 sys.stdin.readline()을 사용해야 시간초과가 발생하지않음
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
