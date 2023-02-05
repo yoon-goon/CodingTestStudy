1c0fd654cfa5054fd0b608b349624a3ce36a96fe
tjrwls0826 <tjrwls0826@naver.com>
2023-02-05 12:18:18 +0900

    Update 6. A+B - 7.py

--- BAEKJOON/단계별/3단계 반복문/6. A+B - 7.py
+++ BAEKJOON/단계별/3단계 반복문/6. A+B - 7.py
@@ -5,3 +5,4 @@ time = int(input())
 for i in range(1, time + 1):
     a, b = map(int, sys.stdin.readline().split())
     print("Case #" + str(i) + ": " + str(a + b))
+    print(f"Case #{i}: {a+b}") # 이런 문법으로 작성가능
