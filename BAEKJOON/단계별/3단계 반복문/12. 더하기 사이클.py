ba17f4e7e7689987ef0302d93ac080ea700cd141
tjrwls0826 <tjrwls0826@naver.com>
2023-02-06 12:04:53 +0900

    Update 12. 더하기 사이클.py

--- BAEKJOON/단계별/3단계 반복문/12. 더하기 사이클.py
+++ BAEKJOON/단계별/3단계 반복문/12. 더하기 사이클.py
@@ -1,12 +1,23 @@
 # https://www.acmicpc.net/problem/1110
 
-_num = int(input())
+original = int(input())
+_num = original
 
+count =0
 while 1:
     if _num < 10:
-        _num = ("0"+str(_num))
+        _num = ("0" + str(_num))
 
-    _num=str(_num)
-    _num = int(int(_num[0])+int(_num[1]))
+    _num = str(_num)
+    later = _num[1]
+    _num = str(int(_num[0]) + int(_num[1]))
+    if int(_num) >= 10:
+        _num = _num[1]
 
+    _num = later+str(_num)
+
+    _num = int(_num)
+    count += 1
+    if _num == original:
+        break
     print(_num)
