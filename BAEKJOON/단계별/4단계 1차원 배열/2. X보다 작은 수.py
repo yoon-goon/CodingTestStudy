b33a470611e46759525d3cb606a712193bc4d2cd
tjrwls0826 <tjrwls0826@naver.com>
2023-02-08 01:56:01 +0900

    Update 2. X보다 작은 수.py

--- BAEKJOON/단계별/4단계 1차원 배열/2. X보다 작은 수.py
+++ BAEKJOON/단계별/4단계 1차원 배열/2. X보다 작은 수.py
@@ -4,9 +4,10 @@ import sys
 
 total, _num = map(int,input().split())
 list = sys.stdin.readline().split()
-answer = []
+answer = ""
 
 for i in range(total):
-    if list[i] < _num:
-        
+    if int(list[i]) < _num:
+        answer += f"{list[i]} "
+print(answer)
 
