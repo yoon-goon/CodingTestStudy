35c8c33ca834e98c66bbfd68cef5cd7bd93e0192
tjrwls0826 <tjrwls0826@naver.com>
2023-02-07 18:31:00 +0900

    Update 1. 개수 세기.py

--- BAEKJOON/단계별/4단계 1차원 배열/1. 개수 세기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/1. 개수 세기.py
@@ -4,9 +4,14 @@ import sys
 total = int(input())
 numlist = sys.stdin.readline().split()
 find = input()
-count = 0
+cnt = 0
 
 for i in range(total):
     if numlist[i] == find:
-        count += 1
-print(count)
+        cnt += 1
+print(cnt)
+
+print(numlist.count(find)) # count 를 사용해 훨씬 간단하게 해결할 수 있음
+'''
+배열.count(세고자 하는 원소) 
+'''
