a1061421a7e5a62b257633598143f0a57aa1589f
tjrwls0826 <tjrwls0826@naver.com>
2023-02-07 18:28:05 +0900

    Update 1. 개수 세기.py

--- BAEKJOON/단계별/4단계 1차원 배열/1. 개수 세기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/1. 개수 세기.py
@@ -1,5 +1,12 @@
 # https://www.acmicpc.net/problem/10807
-
+import sys
 
 total = int(input())
-numlist = []
+numlist = sys.stdin.readline().split()
+find = input()
+count = 0
+
+for i in range(total):
+    if numlist[i] == find:
+        count += 1
+print(count)
