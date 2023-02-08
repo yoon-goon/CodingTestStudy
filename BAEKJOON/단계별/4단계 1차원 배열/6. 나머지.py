18ae10aadf8282a88d298e83de7f4dc45bf8689f
tjrwls0826 <tjrwls0826@naver.com>
2023-02-09 08:20:02 +0900

    Update 6. 나머지.py

--- BAEKJOON/단계별/4단계 1차원 배열/6. 나머지.py
+++ BAEKJOON/단계별/4단계 1차원 배열/6. 나머지.py
@@ -1,6 +1,7 @@
 # https://www.acmicpc.net/problem/3052
 import sys
 
+'''
 numlist = []
 answer = []
 for i in range(10):
@@ -10,3 +11,11 @@ for y in numlist:
         answer.append(y)
 
 print(len(answer))
+'''
+
+'''
+set 집합 자료형을 사용하면 더 쉽게 풀이 할 수 있다.
+집합 자료형의 특징
+- 중복을 허용하지 않음
+- 순서가 없음
+'''
