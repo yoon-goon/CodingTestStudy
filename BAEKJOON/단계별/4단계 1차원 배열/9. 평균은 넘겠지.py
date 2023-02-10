47ee3b33520fdcfdacfdf9a20d5e0cff3c6fcdb5
tjrwls0826 <tjrwls0826@naver.com>
2023-02-11 07:04:18 +0900

    Update 9. 평균은 넘겠지.py

--- BAEKJOON/단계별/4단계 1차원 배열/9. 평균은 넘겠지.py
+++ BAEKJOON/단계별/4단계 1차원 배열/9. 평균은 넘겠지.py
@@ -3,11 +3,14 @@ import sys
 
 cases = int(input())
 
-
 for i in range(cases):
-    A = list(map(int,sys.stdin.readline().split()))
-    avg = sum(A[1:])/A[0]
-    for y in range(1,len(A)):
-        print(A[y])
+    A = list(map(int, sys.stdin.readline().split()))
+    avg = sum(A[1:]) / A[0]
+    cnt = 0
+    for y in range(1, len(A)):
+        if A[y] > avg:
+            cnt += 1
+
+    print(f"{cnt / (len(A) - 1) * 100:.3f}%")
 
     print(avg)
