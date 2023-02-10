226d7dee773217648d7943696abb0fbec56b5041
tjrwls0826 <tjrwls0826@naver.com>
2023-02-10 19:45:16 +0900

    Update 8. OX퀴즈.py

--- BAEKJOON/단계별/4단계 1차원 배열/8. OX퀴즈.py
+++ BAEKJOON/단계별/4단계 1차원 배열/8. OX퀴즈.py
@@ -13,3 +13,15 @@ for i in range(time):
                 b -= 1
                 answer += 1
     print(answer)
+
+'''
+while을 사용하지 않고 ,split('X')로 X 마다 요소를 나누어 계산하는 방법도 있음
+
+for _ in range(int(sys.stdin.readline())):
+    point = 0
+    for x in str(sys.stdin.readline())[:-1].split('X'):
+        point += len(x)*(len(x)+1)//2
+    print(point)
+
+
+'''
