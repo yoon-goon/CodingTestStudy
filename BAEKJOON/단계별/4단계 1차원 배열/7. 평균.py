ed035027e4888dfb6257bcb86aee4e15c5aa59e8
tjrwls0826 <tjrwls0826@naver.com>
2023-02-10 19:18:47 +0900

    Update 7. 평균.py

--- BAEKJOON/단계별/4단계 1차원 배열/7. 평균.py
+++ BAEKJOON/단계별/4단계 1차원 배열/7. 평균.py
@@ -1,11 +1,21 @@
 # https://www.acmicpc.net/problem/1546
 import sys
 
+
+'''
 _num = int(input())
 numlist = list(map(int,sys.stdin.readline().split()))
 answer = 0
 
 for i in range(_num):
-    answer += int(numlist[i]) / int(max(numlist))*100
+    answer += numlist[i] / max(numlist)*100
 
 print(answer / _num)
+
+'''
+
+# 간략화 풀이
+
+_num = int(input())
+numlist = list(map(int,sys.stdin.readline().split()))
+print(sum(numlist)/max(numlist)*100/_num)
