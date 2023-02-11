e9111c8961fcf70844adeccaee704066ca734117
tjrwls0826 <tjrwls0826@naver.com>
2023-02-12 01:46:38 +0900

    Update 2. 셀프 넘버.py

--- BAEKJOON/단계별/5단계 함수/2. 셀프 넘버.py
+++ BAEKJOON/단계별/5단계 함수/2. 셀프 넘버.py
@@ -1,18 +1,18 @@
 # https://www.acmicpc.net/problem/4673
 
-alist = list(range(1,10001))
+alist = set(range(1, 10001))
 ans = set()
 for i in range(1, 10):
     ans.add(i + i)
-for y in range(11, 100):
+for y in range(10, 100):
     yx = str(y)
     ans.add(int(yx) + int(yx[0]) + int(yx[1]))
-for x in range(101, 1000):
+for x in range(100, 1000):
     xx = str(x)
     ans.add(int(xx) + int(xx[0]) + int(xx[1]) + int(xx[2]))
-for z in range(1001, 10000):
+for z in range(1000, 10000):
     zx = str(z)
     ans.add(int(zx) + int(zx[0]) + int(zx[1]) + int(zx[2]) + int(zx[3]))
 
-
-print(sorted(set(alist) - set(ans)))
+for i in (sorted(set(alist) - set(ans))):
+    print(i)
