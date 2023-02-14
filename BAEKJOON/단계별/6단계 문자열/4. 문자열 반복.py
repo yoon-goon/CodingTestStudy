7129a2e359262e88146fddca2606234887e5e061
tjrwls0826 <tjrwls0826@naver.com>
2023-02-15 00:22:50 +0900

    Update 4. 문자열 반복.py

--- BAEKJOON/단계별/6단계 문자열/4. 문자열 반복.py
+++ BAEKJOON/단계별/6단계 문자열/4. 문자열 반복.py
@@ -6,4 +6,4 @@ for i in range(test_num):
     times, words = sys.stdin.readline().split(' ')
     for y in words:
         for x in range(int(times)):
-            print(y, end='')
+            print(y, end=''.rstrip())
