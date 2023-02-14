82f699ba4a425a6ea7885be25a43258b0f7dcf08
tjrwls0826 <tjrwls0826@naver.com>
2023-02-15 00:20:52 +0900

    Create 4. 문자열 반복.py

---
+++ BAEKJOON/단계별/6단계 문자열/4. 문자열 반복.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/2675
+import sys
+
+test_num = int(input())
+for i in range(test_num):
+    times, words = sys.stdin.readline().split(' ')
+    for y in words:
+        for x in range(int(times)):
+            print(y, end='')
