b72091ba544c4eb3fb9a237e45bc6ae56c2b3aa7
tjrwls0826 <tjrwls0826@naver.com>
2023-02-14 17:20:19 +0900

    Update 3. 알파벳 찾기.py

--- BAEKJOON/단계별/6단계 문자열/3. 알파벳 찾기.py
+++ BAEKJOON/단계별/6단계 문자열/3. 알파벳 찾기.py
@@ -3,3 +3,9 @@
 S = str(input())
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'w', 'x', 'y', 'z']
+
+for i in (alphabet):
+    if i in S:
+        print(S.index(i),end=" ")
+    else:
+        print("-1",end=" ")
