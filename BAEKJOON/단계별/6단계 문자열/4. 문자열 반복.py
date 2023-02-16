ded14efd8d46f1359b468fd61d7777488b4ed45d
tjrwls0826 <tjrwls0826@naver.com>
2023-02-16 19:09:06 +0900

    Update 4. 문자열 반복.py

--- BAEKJOON/단계별/6단계 문자열/4. 문자열 반복.py
+++ BAEKJOON/단계별/6단계 문자열/4. 문자열 반복.py
@@ -4,14 +4,11 @@ import sys
 
 test_num = int(input())
 for i in range(test_num):
-    times, words = map(str,sys.stdin.readline().split())
+    times, words = map(str, sys.stdin.readline().split())
     for y in words:
         for x in range(int(times)):
             print(y, end='')
     print('')
-
-
-
 
 '''
 for i in range(2):
