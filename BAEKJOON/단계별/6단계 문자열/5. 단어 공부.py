25d43cc2d94d9b1ce22bf779bd633accec4037e2
tjrwls0826 <tjrwls0826@naver.com>
2023-02-16 21:34:42 +0900

    Update 5. 단어 공부.py

--- BAEKJOON/단계별/6단계 문자열/5. 단어 공부.py
+++ BAEKJOON/단계별/6단계 문자열/5. 단어 공부.py
@@ -1,10 +1,28 @@
 # https://www.acmicpc.net/problem/1157
 
+'''
+
+
 ans = list(str(input().upper()))
 set_ans = set(ans)
 cnt = []
 list_ans = list(set_ans)
 for i in set_ans:
+    cnt.append(ans.count(i))
+
+if cnt.count(max(cnt)) > 1:
+    print('?')
+else:
+    print(list_ans[cnt.index(max(cnt))])
+
+'''
+
+# 불필요한 코드 줄이기
+
+ans = list(str(input().upper()))
+list_ans = list(set(ans))
+cnt = []
+for i in list_ans:
     cnt.append(ans.count(i))
 
 if cnt.count(max(cnt)) > 1:
