42622f069f699f6d8abcd108e926349892a64886
tjrwls0826 <tjrwls0826@naver.com>
2023-02-17 22:35:02 +0900

    Update 5. 단어 공부.py

--- BAEKJOON/단계별/6단계 문자열/5. 단어 공부.py
+++ BAEKJOON/단계별/6단계 문자열/5. 단어 공부.py
@@ -20,12 +20,12 @@ else:
 # 불필요한 코드 줄이기
 
 ans = list(str(input().upper()))
-list_ans = list(set(ans))
+list_ans = list(set(ans)) # 중복 제거
 cnt = []
 for i in list_ans:
-    cnt.append(ans.count(i))
+    cnt.append(ans.count(i)) # ans 에서 list_ans 의 요소를 카운트해 cnt 배열에 append
 
-if cnt.count(max(cnt)) > 1:
+if cnt.count(max(cnt)) > 1: # cnt 배열에 최대값이 2개 이상일 경우 ? 출력
     print('?')
 else:
-    print(list_ans[cnt.index(max(cnt))])
+    print(list_ans[cnt.index(max(cnt))]) # 아닐경우 최대값의 인덱스를 통해 list_ans에서의 값 출력
