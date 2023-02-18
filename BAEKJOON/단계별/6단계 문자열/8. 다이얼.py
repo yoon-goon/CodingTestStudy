388933b7d28075a87a39ab3a56d180eb97a95c15
tjrwls0826 <tjrwls0826@naver.com>
2023-02-18 20:35:20 +0900

    Update 8. 다이얼.py

--- BAEKJOON/단계별/6단계 문자열/8. 다이얼.py
+++ BAEKJOON/단계별/6단계 문자열/8. 다이얼.py
@@ -2,12 +2,21 @@
 
 
 inp = input()
-alist = [" ", "A", "D", "G", "J", "M", "P", "T", "W"]
-blist = [" ", "B", "E", "H", "K", "N", "Q", "U", "X"]
-clist = [" ", "C", "F", "I", "L", "O", "R", "V", "Y"]
-dlist = ["", "", "", "", "", "", "S", "", "Z"]
+alist = ["", "", "A", "D", "G", "J", "M", "P", "T", "W"]
+blist = ["", "", "B", "E", "H", "K", "N", "Q", "U", "X"]
+clist = ["", "", "C", "F", "I", "L", "O", "R", "V", "Y"]
+dlist = ["", "", "", "", "", "", "", "S", "", "Z"]
 ans = 0
 
 for i in inp:
+    ans += 1
     if i in alist:
         ans += alist.index(i)
+    elif i in blist:
+        ans += blist.index(i)
+    elif i in clist:
+        ans += clist.index(i)
+    else:
+        ans += dlist.index(i)
+
+print(ans)
