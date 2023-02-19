412074d1e7a203c32280e6248d8541e6627431aa
tjrwls0826 <tjrwls0826@naver.com>
2023-02-20 00:28:39 +0900

    Update 9. 크로아티아 알파벳.py

--- BAEKJOON/단계별/6단계 문자열/9. 크로아티아 알파벳.py
+++ BAEKJOON/단계별/6단계 문자열/9. 크로아티아 알파벳.py
@@ -11,3 +11,11 @@ for i in cro_alpha:
         inp = inp.replace(i, ' ')
 
 print(ans + len(inp.replace(' ', '')))
+
+# 개선한 풀이
+
+inp = input()
+cro_alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+for i in cro_alpha:
+    inp = inp.replace(i,' ')
+print(len(inp))
