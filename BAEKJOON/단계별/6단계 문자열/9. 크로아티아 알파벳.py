9897067dcd075df83ef9e4ca21a829fe728f86e2
tjrwls0826 <tjrwls0826@naver.com>
2023-02-19 21:44:10 +0900

    Create 9. 크로아티아 알파벳.py

---
+++ BAEKJOON/단계별/6단계 문자열/9. 크로아티아 알파벳.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/2941
+
+
+inp = input()
+cro_alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+ans = 0
+
+for i in cro_alpha:
+    if i in inp:
+        ans += inp.count(i)
+
+
+print(ans)
