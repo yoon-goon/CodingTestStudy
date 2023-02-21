cb2d7edcc86aee6d17f32a9e4719fd7dad91e66d
tjrwls0826 <tjrwls0826@naver.com>
2023-02-22 01:45:01 +0900

    Update 추가. 공 넣기.py

--- BAEKJOON/단계별/4단계 1차원 배열/추가. 공 넣기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/추가. 공 넣기.py
@@ -1,3 +1,10 @@
 # https://www.acmicpc.net/problem/10810
 
-N,M = input().split()
+N, M = map(int, input().split())
+ans = [0] * N
+for y in range(M):
+    i, j, k = map(int, input().split())
+    for x in range(i - 1, j):
+        ans[x] = k
+
+print(*ans)
