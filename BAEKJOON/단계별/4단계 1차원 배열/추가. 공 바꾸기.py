ae664b7549bde3f4833ce9315557539cb19f17f2
tjrwls0826 <tjrwls0826@naver.com>
2023-02-22 19:42:21 +0900

    Update 추가. 공 바꾸기.py

--- BAEKJOON/단계별/4단계 1차원 배열/추가. 공 바꾸기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/추가. 공 바꾸기.py
@@ -2,8 +2,12 @@
 import sys
 
 N, M = map(int, input().split())
-ans = [0] * N
+ans = []
+for x in range(1, N + 1):
+    ans.append(x)
 
-ans = [0] * N  # N의 개수만큼 리스트에 아무 공도 들어가지 않은 자리인 0 생성해 두기 위함
 for _ in range(M):
-    i, j = sys.stdin
+    i, j = map(int, sys.stdin.readline().split())
+    ans[i], ans[j] = ans[j], ans[i]
+
+print(*ans)
