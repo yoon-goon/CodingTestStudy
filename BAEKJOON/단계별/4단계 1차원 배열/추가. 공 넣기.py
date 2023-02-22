ffe47e47d4b74955fcbdce510a4d813f461113d6
tjrwls0826 <tjrwls0826@naver.com>
2023-02-22 19:27:35 +0900

    Update 추가. 공 넣기.py

--- BAEKJOON/단계별/4단계 1차원 배열/추가. 공 넣기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/추가. 공 넣기.py
@@ -1,10 +1,11 @@
 # https://www.acmicpc.net/problem/10810
+import sys
 
 N, M = map(int, input().split())
-ans = [0] * N # N의 개수만큼 리스트에 아무 공도 들어가지 않은 자리인 0 생성해 두기 위함
+ans = [0] * N  # N의 개수만큼 리스트에 아무 공도 들어가지 않은 자리인 0 생성해 두기 위함
 for y in range(M):
-    i, j, k = map(int, input().split())
+    i, j, k = map(int, sys.stdin.readline().split())
     for x in range(i - 1, j):
         ans[x] = k
 
-print(*ans) # 언패킹
+print(*ans)  # 언패킹
