ef41b2cb69eba3cdbf4d7fc3a1c7d74632257971
tjrwls0826 <tjrwls0826@naver.com>
2023-02-23 22:11:11 +0900

    Update 추가. 공 바꾸기.py

--- BAEKJOON/단계별/4단계 1차원 배열/추가. 공 바꾸기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/추가. 공 바꾸기.py
@@ -9,6 +9,5 @@ for x in range(1, N + 1):
 for _ in range(M):
     i, j = map(int, sys.stdin.readline().split())
     ans[i - 1], ans[j - 1] = ans[j - 1], ans[i - 1] # 인덱스가 0부터 시작함으로 3번째 자리를 바꾸려면 4가 나와야해서 -1
-    print(ans)
 
 print(*ans)
