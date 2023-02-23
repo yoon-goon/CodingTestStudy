e4f60015a039274a8bf4412246c6ccd1fcc169a0
tjrwls0826 <tjrwls0826@naver.com>
2023-02-23 22:46:42 +0900

    Update 추가. 바구니 뒤집기.py

--- BAEKJOON/단계별/4단계 1차원 배열/추가. 바구니 뒤집기.py
+++ BAEKJOON/단계별/4단계 1차원 배열/추가. 바구니 뒤집기.py
@@ -2,12 +2,13 @@
 import sys
 
 N, M = map(int, input().split())
-ans = []
+ans = [0]
 for i in range(1, N + 1):
     ans.append(i)
 
 for _ in range(M):
     i, j = map(int, sys.stdin.readline().split())
-    ans[i - 1:j - 1] = reversed(ans[i - 1:j - 1])
+    ans[i:j] = reversed(ans[i:j])
+    print(ans)
 
 print(ans)
