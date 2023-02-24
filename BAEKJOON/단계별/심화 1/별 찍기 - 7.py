ddde55576ac050c0c7981229367657108a72982f
tjrwls0826 <tjrwls0826@naver.com>
2023-02-25 04:04:18 +0900

    Update 별 찍기 - 7.py

--- BAEKJOON/단계별/심화 1/별 찍기 - 7.py
+++ BAEKJOON/단계별/심화 1/별 찍기 - 7.py
@@ -3,9 +3,6 @@
 stars = int(input())
 
 for i in range(1, stars + 1):
-    if i == 1:
-        print(' ' * (stars - i) + '*')
-    else:
-        print(' ' * (stars - i) + '*' + '**' * (i - 1))
+    print(' ' * (stars - i) + '*' + '**' * (i - 1))
 for x in range(stars - 1, 0, -1):
     print(' ' * (stars - x) + '*' + '**' * (x - 1))
