29857246632e5922d1f50f8d3624dea4d0cba81e
tjrwls0826 <tjrwls0826@naver.com>
2023-02-24 22:52:09 +0900

    Update 추가. 그대로 출력하기.py

--- BAEKJOON/단계별/6단계 문자열/추가. 그대로 출력하기.py
+++ BAEKJOON/단계별/6단계 문자열/추가. 그대로 출력하기.py
@@ -1,9 +1,8 @@
 # https://www.acmicpc.net/problem/11718
-import sys
 
 while True:
     try:
-        inp = sys.stdin.readline()
+        inp = input().strip()
         print(inp)
     except EOFError:
         break
