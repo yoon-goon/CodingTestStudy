3c18006501a59338b4819f534b51d79b1c16caf7
tjrwls0826 <tjrwls0826@naver.com>
2023-02-26 04:06:01 +0900

    Update 바구니 순서 바꾸기.py

--- BAEKJOON/단계별/심화 1/바구니 순서 바꾸기.py
+++ BAEKJOON/단계별/심화 1/바구니 순서 바꾸기.py
@@ -6,6 +6,10 @@ ans = list(range(1, N + 1))
 
 for _ in range(M):
     i, j, k = map(int, sys.stdin.readline().split())
-    ans[i - 1:k], ans[k:j + 1] = reversed(ans[k:j + 1]), reversed(ans[i - 1:k])
+    left = ans[:i]
+    mid1 = ans[i:k]
+    mid2 = ans[k:j+1]
+    right = ans[j+1:]
+    ans = left + mid2 + mid1 + right
 
 print(*ans)
