f7f0d8cea87e48ff795439d7f9edd430b5bee247
tjrwls0826 <tjrwls0826@naver.com>
2023-02-28 02:32:26 +0900

    Update 그룹 단어 체커.py

--- BAEKJOON/단계별/심화 1/그룹 단어 체커.py
+++ BAEKJOON/단계별/심화 1/그룹 단어 체커.py
@@ -7,7 +7,31 @@ word_list = []
 
 for _ in range(nums):
     words = sys.stdin.readline()
+    for i in range(1, len(words)):
+        if words[i] != words[i-1]:
+            if words[i-1] in word_list:
+                pass
+            else:
+                word_list.append(words[i-1])
+
+
+
+print(cnt)
+
+
+
+
+
+
+
+
+'''
+for _ in range(nums):
+    words = sys.stdin.readline()
     for i in range(0, len(words)):
         if words[i] not in word_list:
             word_list.append(words[i])
-            
+        else:
+            if words[i] == words[i-1]:
+'''
+
