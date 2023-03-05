9775230a0571900d84b6bdaf1c3a86f445917389
tjrwls0826 <tjrwls0826@naver.com>
2023-03-05 19:29:47 +0900

    Update 너의 평점은.py

--- BAEKJOON/단계별/심화 1/너의 평점은.py
+++ BAEKJOON/단계별/심화 1/너의 평점은.py
@@ -4,6 +4,7 @@ import sys
 total = 0
 score_dic = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 cnt = 0
+total_score = 0
 
 for _ in range(20):
     name, score, achieve, = sys.stdin.readline().split()
@@ -11,6 +12,10 @@ for _ in range(20):
         cnt += 1
         pass
     else:
-        total += float(score) * float(score_dic[achieve])
+        print(total, score, score_dic[achieve])
+        total += (float(score) * (score_dic[achieve]))
+        total_score += float(score)
 
-print(total / (20 - cnt))
+print(total, cnt)
+
+print(total / total_score)
