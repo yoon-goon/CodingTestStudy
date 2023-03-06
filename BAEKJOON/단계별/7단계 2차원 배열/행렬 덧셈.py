047c3050ab2733c03eb9f7623586e9bc167c270c
tjrwls0826 <tjrwls0826@naver.com>
2023-03-06 13:43:52 +0900

    Update 행렬 덧셈.py

--- BAEKJOON/단계별/7단계 2차원 배열/행렬 덧셈.py
+++ BAEKJOON/단계별/7단계 2차원 배열/행렬 덧셈.py
@@ -1,13 +1,20 @@
 # https://www.acmicpc.net/problem/2738
 import sys
 
-N, M = map(int,input().split())
+N, M = map(int, input().split())
 A = []
 B = []
 
 for _ in range(M):
-    A.append(sys.stdin.readline().rstrip())
+    A.append(sys.stdin.readline().rstrip().replace(' ', '', N))
 
 for _ in range(M):
-    B.append(sys.stdin.readline().rstrip())
-print(A + B)
+    B.append(sys.stdin.readline().rstrip().replace(' ', '', N))
+
+print(A, B)
+
+for i in range(N):
+    for y in range(M):
+        print(int(A[i][y]) + int(B[i][y]))
+
+print(int(A[0][0]) + int(B[0][0]))
