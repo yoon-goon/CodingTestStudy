8633595b4fa41309e8cc5676ca32b5e64839d14e
tjrwls0826 <tjrwls0826@naver.com>
2023-03-07 16:11:17 +0900

    Update 최댓값.py

--- BAEKJOON/단계별/7단계 2차원 배열/최댓값.py
+++ BAEKJOON/단계별/7단계 2차원 배열/최댓값.py
@@ -3,12 +3,11 @@ import sys
 
 A = []
 C = []
+
 for _ in range(9):
     B = sys.stdin.readline().split()
     A.append(B)
     C.append(max(B))
-
-print(C)
 
 for i in range(9):
     for y in range(9):
