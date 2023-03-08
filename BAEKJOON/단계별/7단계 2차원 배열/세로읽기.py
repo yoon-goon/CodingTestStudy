b3afe7b42dc039bab2e202795a7fecd46a091f46
tjrwls0826 <tjrwls0826@naver.com>
2023-03-08 10:10:52 +0900

    Update 세로읽기.py

--- BAEKJOON/단계별/7단계 2차원 배열/세로읽기.py
+++ BAEKJOON/단계별/7단계 2차원 배열/세로읽기.py
@@ -4,13 +4,11 @@ import sys
 A = []
 
 for _ in range(5):
-    A.append(sys.stdin.readline().split())
-
-print(A)
+    A.append(sys.stdin.readline().rstrip())
 
 for i in range(15):
     for y in range(5):
         try:
-            print(A[i][y], end='')
-        except:
+            print(A[y][i], end='')
+        except IndexError:
             print('', end='')
