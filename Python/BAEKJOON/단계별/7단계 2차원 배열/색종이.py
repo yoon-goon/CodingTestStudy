5ac7827bce40b56c7ee79a1dcdb7826a8e96e583
tjrwls0826 <tjrwls0826@naver.com>
2023-03-12 19:22:18 +0900

    Update 색종이.py

--- Python/BAEKJOON/단계별/7단계 2차원 배열/색종이.py
+++ Python/BAEKJOON/단계별/7단계 2차원 배열/색종이.py
@@ -1,16 +1,21 @@
 # https://www.acmicpc.net/problem/2563
 import sys
 
-space = []
-for i in range(100):
-    space.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
+space = [[0] * 100 for _ in range(100)]
 
 papers = int(input())
 for _ in range(papers):
     A, B = map(int, sys.stdin.readline().split())
-    for y in range(A,A+10):
-        space[B][y] = 1
+    for i in range(A, A + 10):
+        for y in range(B, B + 10):
+            space[i][y] = 1
 
+cnt = 0
+for x in range(100):
+    for w in range(100):
+        if space[x][w] == 1:
+            cnt += 1
+
+print(cnt)
 
 print(space)
