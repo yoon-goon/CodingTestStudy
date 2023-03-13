d93683fe8b6c62182a34c6053812c033a0032e86
tjrwls0826 <tjrwls0826@naver.com>
2023-03-13 13:02:22 +0900

    Update 벌집.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/벌집.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/벌집.py
@@ -2,12 +2,13 @@
 
 # 1,7,19,37,... (6씩증가)
 N = int(input())
-cnt = 1
+cnt = 0
 if N == 1:
     print(0)
+
 else:
-    while N > 6:
-        N = N - 6 * cnt
+    while N - 1 > 6 * cnt:
+        N = N - (6 * cnt)
         cnt += 1
 
-    print(cnt)
+    print(cnt+1)
