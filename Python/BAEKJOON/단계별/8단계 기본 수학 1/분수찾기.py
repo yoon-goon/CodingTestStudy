6112b308df7f20bc623c59de470652665b411f29
tjrwls0826 <tjrwls0826@naver.com>
2023-03-17 22:20:20 +0900

    Update 분수찾기.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/분수찾기.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/분수찾기.py
@@ -1,11 +1,18 @@
 # https://www.acmicpc.net/problem/1193
 
-num= int(input())
-A = []
-x_num = 0
-y_num = 0
 
+x = int(input())
 
-for _ in range(num):
-    A.append()
+row = 1
+while x > row:
+    x -= row
+    row += 1
 
+if row % 2 == 0:  # A/B 형식으로 생각
+    A = x
+    B = row - x + 1
+else:
+    A = row - x + 1
+    B = x
+
+print(str(A) + "/" + str(B))
