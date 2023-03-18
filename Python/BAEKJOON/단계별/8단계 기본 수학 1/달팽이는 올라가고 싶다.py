400fc462e983311a832390f77cc0709b170f7f36
tjrwls0826 <tjrwls0826@naver.com>
2023-03-18 18:41:59 +0900

    Update 달팽이는 올라가고 싶다.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/달팽이는 올라가고 싶다.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/달팽이는 올라가고 싶다.py
@@ -1,4 +1,21 @@
 # https://www.acmicpc.net/problem/2869
+''' 시간초과가 걸리는 풀이
+A, B, V = map(int, input().split())
+day = 0
 
-A, B, V = input().split()
-print(A,B,V)
+while V > A:
+    V -= A
+    if V > 0:
+        V += B
+    day += 1
+
+print(day + 1)
+'''
+
+A, B, V = map(int, input().split())
+day = 0
+
+while V > A:
+    day += V // A
+    V = (V // A) * B + V % A
+print(day + 1)
