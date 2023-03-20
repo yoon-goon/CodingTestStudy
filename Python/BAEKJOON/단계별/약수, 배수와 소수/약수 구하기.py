9a401a5929a6df136836fe2085df7a926544a96d
tjrwls0826 <tjrwls0826@naver.com>
2023-03-20 12:05:01 +0900

    Update 약수 구하기.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수/약수 구하기.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수/약수 구하기.py
@@ -1,5 +1,10 @@
 # https://www.acmicpc.net/problem/2501
 
-N, K = int(input())
+N, K = map(int, input().split())
 A = []
 
+for i in range(1, N + 1):
+    if N % i == 0:
+        A.append(i)
+
+print(A[K])
