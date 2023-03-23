938a6f5b2a582ba6414387dff6f9d42cea2fc54a
tjrwls0826 <tjrwls0826@naver.com>
2023-03-23 21:27:02 +0900

    Update 소수 찾기.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수/소수 찾기.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수/소수 찾기.py
@@ -4,3 +4,13 @@ import sys
 Nums = int(input())
 
 A = sys.stdin.readline().split()
+
+print(A)
+for i in A:
+    B = []
+    if i == 1:
+        pass
+    else:
+        for y in range(1,i):
+            if i % y == 0:
+                B.append(y)
