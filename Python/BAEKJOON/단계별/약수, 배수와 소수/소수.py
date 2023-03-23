aee915e4f46667ebbe371d9ef8f77d24820ba578
tjrwls0826 <tjrwls0826@naver.com>
2023-03-24 04:27:56 +0900

    Update 소수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수/소수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수/소수.py
@@ -3,3 +3,15 @@
 allMN = []
 M = int(input())
 N = int(input())
+
+for i in range(M, N + 1):
+    A = []
+    if i == 1:
+        pass
+    else:
+        for y in range(1, i + 1):
+            if i % y == 0:
+                A.append(y)
+    if len(A) == 2:
+        allMN.append(i)
+
