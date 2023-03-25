88ee5f1d906513c47fe0b450e705350a44436d5b
tjrwls0826 <tjrwls0826@naver.com>
2023-03-26 02:59:16 +0900

    Update 소인수분해.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수/소인수분해.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수/소인수분해.py
@@ -1,11 +1,16 @@
 # https://www.acmicpc.net/problem/11653
 
 N = int(input())
+i = 2
 
 if N == 1:
     pass
 else:
-    for i in range(2, N):
+    # for i in range(2, N):
+    while N >= 2:
         if N % i == 0:
             print(i)
-            N = N % i
+            N = N / i
+            i = 2
+        else:
+            i += 1
