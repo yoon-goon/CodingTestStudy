fb7c0c6c23a6a2b69be77f2cbc2e712b626d2fb2
tjrwls0826 <tjrwls0826@naver.com>
2023-03-27 13:32:59 +0900

    Update 실습과제 #02.py

--- Python/수업 실습/실습과제 #02.py
+++ Python/수업 실습/실습과제 #02.py
@@ -1,8 +1,14 @@
 print("사용자의 회원등급을 입력해 주세요.")
-A = input()
+A = str(input())
 print("주차 시간(분)을 입력해 주세요.")
 B = int(input())
 
-if A != "플래티넘" or "골드":
+money = 0
+
+if A == "실버" or A == "프렌즈" or A == "비회원":
     print("구매 금액을 입력해주세요.")
     C = int(input())
+else:
+    money = (B - 120) // 10 * 1000
+
+print(f"주차요금은 {money}원 입니다.")
