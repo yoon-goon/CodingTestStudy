73a3d9fef2c080bd74a0f0990e82f73cae5bd8ac
tjrwls0826 <tjrwls0826@naver.com>
2023-03-27 13:36:16 +0900

    Update 실습과제 #02.py

--- Python/수업 실습/실습과제 #02.py
+++ Python/수업 실습/실습과제 #02.py
@@ -8,6 +8,14 @@ money = 0
 if A == "실버" or A == "프렌즈" or A == "비회원":
     print("구매 금액을 입력해주세요.")
     C = int(input())
+    if A == "실버" or A == "프렌즈":
+        if C >= 30000:
+            money = (B - 120) // 10 * 1000
+        elif C >= 10000:
+            money = (B - 60) // 10 * 1000
+        else:
+            money = B // 10 * 1000
+
 else:
     money = (B - 120) // 10 * 1000
 
