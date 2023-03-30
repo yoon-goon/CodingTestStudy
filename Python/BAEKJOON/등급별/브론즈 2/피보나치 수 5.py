70e43d48c4f8cbe5424243699e737d40e96833b6
tjrwls0826 <tjrwls0826@naver.com>
2023-03-30 20:19:35 +0900

    Update 피보나치 수 5.py

--- Python/BAEKJOON/등급별/브론즈 2/피보나치 수 5.py
+++ Python/BAEKJOON/등급별/브론즈 2/피보나치 수 5.py
@@ -2,15 +2,19 @@
 
 n = int(input())
 
-A = list(range(n))
+A = list(range(n + 2))
+
 
 A[0] = 0
 A[1] = 1
 A[2] = 1
 
-print(A)
+if n == 0:
+    print(A[0])
+if n == 1 or 2:
+    print(1)
+else:
+    for i in range(3, n + 1):
+        A[i] = A[i - 1] + A[i - 2]
 
-for i in range(3, n):
-    A[i] = A[i - 1] + A[i - 2]
-
-print(A[n-1])
+print(A[n])
