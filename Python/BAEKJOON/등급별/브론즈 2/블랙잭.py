8731277024e5bfc1eca6f76119495548a572c215
tjrwls0826 <tjrwls0826@naver.com>
2023-04-01 20:29:04 +0900

    Update 블랙잭.py

--- Python/BAEKJOON/등급별/브론즈 2/블랙잭.py
+++ Python/BAEKJOON/등급별/브론즈 2/블랙잭.py
@@ -2,9 +2,19 @@
 import sys
 
 N, M = map(int, input().split())
+sums = []
 
 print(N, M)
 
-A = (sys.stdin.readline().split())
+A = list(map(int, input().split()))
 
 print(A)
+
+for i in A:
+    for j in (i + 1, A):
+        for k in j + 1, A:
+            print(i, j, k)
+            if (i + j + k) <= M:
+                sums.append(i + j + k)
+
+print(max(sums))
