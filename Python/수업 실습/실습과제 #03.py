bc934c6c606ba636ed9ea5cef8254466b1d5d5dc
tjrwls0826 <tjrwls0826@naver.com>
2023-04-03 17:53:30 +0900

    Update 실습과제 #03.py

--- Python/수업 실습/실습과제 #03.py
+++ Python/수업 실습/실습과제 #03.py
@@ -2,28 +2,32 @@ pnum = input()
 
 length = len(pnum)
 
-
 middle = pnum.find('-')
-
-
 
 year = int(pnum[:2])
 month = int(pnum[2:4])
+day = int(pnum[4:6])
 
 if length != 14:
     print("글자수가 부적합합니다.")
-elif middle != 6: #
+if middle != 6:  #
     print("-의 위치가 부적합합니다.")
-elif month < 1 or month > 12:
+if month < 1 or month > 12:
     print("월 입력이 부적합합니다.")
 
-day = int(pnum[4:6])
+if month in [4, 6, 9, 11]:
+    if day > 30:
+        print("30일까지만 있는 월입니다.")
+else:
+    if day > 31:
+        print("31일이 넘는값을 입력했습니다.")
 
-
-
-
-if year >= 23:
-    year = 20 
+if year >= 23:  # 수정필요
+    if pnum[7] == 3 or pnum[7] == 4:
+        print("2000년 이전 출생자는 1,2로 시작해야합니다.")
+else:
+    if pnum[7] == 1 or pnum[7] == 2:
+        print("2000년 이후 출생자는 3,4로 시작해야합니다.")
 
 print(pnum)
 print(length)
