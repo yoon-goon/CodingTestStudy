6da9b196276f5a051ccb3bc620d1346f200845d1
tjrwls0826 <tjrwls0826@naver.com>
2023-04-03 20:26:17 +0900

    Update 실습과제 #03.py

--- Python/수업 실습/실습과제 #03.py
+++ Python/수업 실습/실습과제 #03.py
@@ -1,7 +1,5 @@
-print("주민번호를 입력하세요:")
+print("주민번호를 입력하세요(******-*******):")
 pnum = input()
-
-length = len(pnum)
 
 middle = pnum.find('-')
 
@@ -11,10 +9,10 @@ day = int(pnum[4:6])
 Lunar = 0
 result = 1
 
-if length != 14:
+if len(pnum) != 14:
     print("글자수가 부적합합니다.")
     result = 0
-if middle != 6:  #
+if middle != 6:  
     print("-의 위치가 부적합합니다.")
     result = 0
 if month < 1 or month > 12:
@@ -25,39 +23,36 @@ if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):  # 윤년 계산
     Lunar = 1
 
 if day > 31:
-    print("31일이 넘는값을 입력했습니다. (일이 부적합합니다.)")
+    print("31일이 넘는값을 입력했습니다.")
     result = 0
 elif month in [4, 6, 9, 11]:
     if day > 30:
-        print("30일까지만 있는 월입니다. (일이 부적합합니다.)")
+        print("30일까지만 있는 월입니다.")
         result = 0
 else:
     if month == 2:
         if Lunar == 1:
             if day > 29:
-                print("윤년에는 29일까지입니다. (일이 부적합합니다.)")
+                print("윤년에는 29일까지입니다.")
                 result = 0
         else:
             if day > 28:
-                print("윤년이 아닌 2월은 28일까지입니다. (일이 부적합합니다.)")
+                print("윤년이 아닌 2월은 28일까지입니다.")
                 result = 0
 
-if year >= 23:  # 수정필요 추가해야함
-    if pnum[7] == 3 or pnum[7] == 4:
-        print("2000년 이전 출생자는 1,2로 시작해야합니다.")
+if int(pnum[middle + 1]) > 4:
+    print("- 이후 첫자리는 1~4로 시작해야합니다.")
+    result = 0
+elif year > 23:  # 수정필요 추가해야함
+    if int(pnum[middle + 1]) == 3 or int(pnum[middle + 1]) == 4:
+        print("2000년 이전 출생자는 - 이후 1,2로 시작해야합니다.")
         result = 0
 else:
-    if pnum[7] == 1 or pnum[7] == 2:
-        print("2000년 이후 출생자는 3,4로 시작해야합니다.")
+    if int(pnum[middle + 1]) == 1 or int(pnum[middle + 1]) == 2:
+        print("2000년 이후 출생자는 - 이후 3,4로 시작해야합니다.")
         result = 0
-
-print(pnum)
-print(length)
-print(middle)
-print(year)
-print(month)
 
 if result == 1:
-    print("주민등록번호가 적합합니다.")
+    print("주민등록번호가 유효합니다.")
 else:
-    print("주민등록번호가 부적합합니다.")
+    print("주민등록번호가 유효하지 않습니다.")
