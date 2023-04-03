fe58790e8fe1d13fb31553ad3ee0c8c7f5fc90c6
tjrwls0826 <tjrwls0826@naver.com>
2023-04-03 15:02:29 +0900

    Update 실습과제 #03.py

--- Python/수업 실습/실습과제 #03.py
+++ Python/수업 실습/실습과제 #03.py
@@ -1,13 +1,21 @@
 pnum = input()
 
 length = len(pnum)
+if length != 14:
+    pass
+
 middle = pnum.find('-')
+
+if middle != 6:
+    pass
 
 year = int(pnum[:2])
 month = int(pnum[2:4])
+
+if month < 1 or month > 12:
+    pass
+
 day = int(pnum[4:6])
-
-
 
 if year >= 23:
     year = 2000
