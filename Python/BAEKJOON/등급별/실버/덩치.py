df3c3f27435eb7301f315235f068ac141aa26032
tjrwls0826 <tjrwls0826@naver.com>
2023-04-06 20:48:06 +0900

    Update 덩치.py

--- Python/BAEKJOON/등급별/실버/덩치.py
+++ Python/BAEKJOON/등급별/실버/덩치.py
@@ -1,4 +1,4 @@
-#
+# https://www.acmicpc.net/problem/7568
 import sys
 
 N = int(input())
@@ -13,4 +13,4 @@ for y in e_list:  # 리스트를 한번 쭉 흝고감
     for j in e_list:  # 그 이후 요소들을 비교에 사용하기 위해 j 사용
         if y[0] < j[0] and y[1] < j[1]:  # 무게 , 키 둘다 작으면 랭크가 뒤로 밀려나기때문에
             rank += 1
-    print(rank)
+    print(rank, end=" ")
