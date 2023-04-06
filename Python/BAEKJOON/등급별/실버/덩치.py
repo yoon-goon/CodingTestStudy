9b334ab779f8ce710deb1c6781c62c0a587a23e0
tjrwls0826 <tjrwls0826@naver.com>
2023-04-06 20:45:51 +0900

    Update 덩치.py

--- Python/BAEKJOON/등급별/실버/덩치.py
+++ Python/BAEKJOON/등급별/실버/덩치.py
@@ -6,15 +6,11 @@ e_list = []
 
 for i in range(N):
     A, B = map(int, sys.stdin.readline().split())
-    e_list.append([A,B])
+    e_list.append([A, B])
 
-
-for y in e_list:
+for y in e_list:  # 리스트를 한번 쭉 흝고감
     rank = 1
-    for j in e_list:
-        if y[0] < j [0] and y[1] < j[1]:
+    for j in e_list:  # 그 이후 요소들을 비교에 사용하기 위해 j 사용
+        if y[0] < j[0] and y[1] < j[1]:  # 무게 , 키 둘다 작으면 랭크가 뒤로 밀려나기때문에
             rank += 1
     print(rank)
-
-print(e_list)
-print(e_list[0][0])
