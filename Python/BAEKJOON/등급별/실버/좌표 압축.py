bdb93a71ccc04a3d779bf22b402fc6299a807649
tjrwls0826 <tjrwls0826@naver.com>
2023-04-08 21:47:06 +0900

    Update 좌표 압축.py

--- Python/BAEKJOON/등급별/실버/좌표 압축.py
+++ Python/BAEKJOON/등급별/실버/좌표 압축.py
@@ -4,8 +4,13 @@ N = int(input())
 
 # 자신보다 작은 수 갯수를 출력하면 됨
 
-
 xlist = list(map(int, input().split()))
 
 
 print(xlist)
+
+for i in N:
+    cnt = 0
+    for y in range(N):
+        if i > xlist[y]:
+            cnt += 1
