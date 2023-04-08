3feb4dc74f9881a23f2d5b688d19a1130d414880
tjrwls0826 <tjrwls0826@naver.com>
2023-04-08 22:13:23 +0900

    Update 좌표 압축.py

--- Python/BAEKJOON/등급별/실버/좌표 압축.py
+++ Python/BAEKJOON/등급별/실버/좌표 압축.py
@@ -3,6 +3,41 @@ import sys
 
 N = int(sys.stdin.readline())
 
+xlist = list(map(int, sys.stdin.readline().split()))
+
+sorted_xlist = sorted(xlist)
+count_dict = {}
+count = 0
+
+for i in range(N):
+    if sorted_xlist[i] not in count_dict:
+        count_dict[sorted_xlist[i]] = count
+        count += 1
+
+for i in range(N):
+    print(count_dict[xlist[i]], end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+N = int(sys.stdin.readline())
+
 # 자신보다 작은 수 갯수를 출력하면 됨
 
 xlist = list(sys.stdin.readline().split())
@@ -13,8 +48,8 @@ print(xset)
 # print(xlist)
 
 for i in xlist:
-    print(xset.index(i),end=' ')
-
+    print(xset.index(i),end=' ') # 인덱스를 이용해서 반복문을 한번 더 줄였지만 그래도 시간초과
+'''
 
 
 
