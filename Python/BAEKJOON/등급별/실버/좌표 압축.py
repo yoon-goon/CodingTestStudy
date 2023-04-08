c3a8899095766007ec0ce15416725476dfd617c9
tjrwls0826 <tjrwls0826@naver.com>
2023-04-08 21:56:56 +0900

    Update 좌표 압축.py

--- Python/BAEKJOON/등급별/실버/좌표 압축.py
+++ Python/BAEKJOON/등급별/실버/좌표 압축.py
@@ -1,17 +1,44 @@
 # https://www.acmicpc.net/problem/18870
+import sys
 
+N = int(sys.stdin.readline())
+
+# 자신보다 작은 수 갯수를 출력하면 됨
+
+xlist = list(map(int, sys.stdin.readline().split()))
+xset = list(set(xlist))
+
+# print(xlist)
+
+for i in xlist:
+    cnt = 0
+    for y in range(len(xset)):
+        if i > xset[y]:
+            cnt += 1
+    print(cnt, end=' ')
+
+
+
+
+
+
+'''
 N = int(input())
 
 # 자신보다 작은 수 갯수를 출력하면 됨
 
-xlist = list(set(map(int, input().split())))
+xlist = list(map(int, input().split()))
+xset = list(set(xlist))
 
-
-#print(xlist)
+# print(xlist)
 
 for i in xlist:
     cnt = 0
-    for y in range(len(xlist)):
-        if i > xlist[y]:
+    for y in range(len(xset)):
+        if i > xset[y]:
             cnt += 1
     print(cnt, end=' ')
+    
+    #시간초과된 풀이
+'''
+
