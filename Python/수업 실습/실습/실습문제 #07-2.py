1eeae83d8886df178e7431b7e924036e21495151
tjrwls0826 <tjrwls0826@naver.com>
2023-04-10 14:05:54 +0900

    Create 실습문제 #07-2.py

---
+++ Python/수업 실습/실습/실습문제 #07-2.py
@@ -0,0 +1,28 @@
+def getSecondword(s):
+    s = s.strip()
+    pos1 = s.find(' ')
+    pos2 = s.find('\t')
+    pos3 = s.find('\n')
+    # -1보다 큰 값을 반환함
+    print(s)
+    print(pos1, pos2, pos3)
+    p1 = max(pos1, pos2, pos3) + 1
+    print(p1)
+    s2 = s[p1:]
+    print(s2)
+    pos1 = s2.find(' ')
+    pos2 = s2.find('\t')
+    pos3 = s2.find('\n')
+    print(pos1,pos2,pos3)
+    p2 = max(pos1, pos2, pos3)
+    print(p2)
+    if p2 == -1:
+        p2 = len(s)
+    else:
+        p2 += p1
+    print(p2)
+    return s[p1:p2]
+
+
+print(getSecondword("What a beautiful day"))
+#print(getSecondword("Beautiful life"))
