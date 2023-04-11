2332aa3f84ccdf74ff26e6eec8b895f7357e4221
tjrwls0826 <tjrwls0826@naver.com>
2023-04-11 19:14:25 +0900

    Update N과 M (1).py

--- Python/BAEKJOON/등급별/실버/N과 M (1).py
+++ Python/BAEKJOON/등급별/실버/N과 M (1).py
@@ -1,11 +1,17 @@
 # https://www.acmicpc.net/problem/15649
 
-N, M = map(int,input().split())
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, N + 1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+
+N, M = map(int, input().split())
 s = []
 
 print(N, M)
-
-for i in range(1, N + 1):
-    s.append(i)
-    print(s)
-    s.pop()
+dfs()
