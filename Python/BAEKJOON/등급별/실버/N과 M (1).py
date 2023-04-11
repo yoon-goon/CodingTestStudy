2e301e4cfd58bb5b8f4a5ae5b48b3fe82ab033aa
tjrwls0826 <tjrwls0826@naver.com>
2023-04-11 19:18:48 +0900

    Update N과 M (1).py

--- Python/BAEKJOON/등급별/실버/N과 M (1).py
+++ Python/BAEKJOON/등급별/실버/N과 M (1).py
@@ -1,10 +1,12 @@
 # https://www.acmicpc.net/problem/15649
 
 def dfs():
-    if len(s) == M:
-        print(' '.join(map(str, s)))
+    if len(s) == M: # s의 길이가 입력값 M과 같아진다면
+        print(' '.join(map(str, s)))  # .join : 매개변수로 들어온 리스트에 있는 요소를 합쳐서 하나의 문자열로 반환하는 함수
         return
     for i in range(1, N + 1):
+        if i in s:  # 중복되는 숫자를 피하기 위해 i가 이미 s에 있는경우 아래를 실행하지않고 for문 continue
+            continue
         s.append(i)
         dfs()
         s.pop()
