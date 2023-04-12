891e29adbace633290ea98b2b402254de109dfaa
tjrwls0826 <tjrwls0826@naver.com>
2023-04-12 21:44:06 +0900

    Update 실습과제 #04-2.py

--- Python/수업 실습/실습과제 #04-2.py
+++ Python/수업 실습/실습과제 #04-2.py
@@ -1,14 +1,15 @@
 t = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis nisi at dapibus semper. Ut quis sapien vulputate, pharetra sapien vel, commodo diam. Nam tincidunt nulla sit amet neque iaculis, at interdum erat ultrices. Donec nec turpis sagittis, malesuada dui ut, pellentesque magna. Integer ultricies pharetra ex ut semper. Pellentesque ex orci, rhoncus vitae dolor in, vulputate volutpat felis. Fusce quis ornare purus."
 
 
-def getNextPeriodPos():
-    return
+def getNextPeriodPos(txt, startPos):
+    txt = txt[startPos:]
+    N = txt.find('.')
+
+    return N
+
 
 
 idx = getNextPeriodPos(t, 0)
 print(idx)
 idx = getNextPeriodPos(t, idx + 1)
 print(idx)
-
-
-
