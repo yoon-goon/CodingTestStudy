e5715641ddb1791ec40e918eda2f7d76f0a736cc
tjrwls0826 <tjrwls0826@naver.com>
2023-04-13 20:28:58 +0900

    Update 실습과제 #04-1.py

--- Python/수업 실습/실습과제 #04-1.py
+++ Python/수업 실습/실습과제 #04-1.py
@@ -1,4 +1,23 @@
 # 함수를 이용해 간략화 시키기
+def over120(time):
+    if time > 120:
+        fee = (time - 120) // 10 * 1000
+        print(f"주차 요금: {fee}원")
+    else:
+        print("주차 요금 0원")
+
+
+def over60(time):
+    if time > 60:
+        fee = (time - 60) // 10 * 1000
+        print(f"주차 요금: {fee}원")
+    else:
+        print("주차 요금 0원")
+
+
+def else10(time):
+    fee = time // 10 * 1000
+    print(f"주차 요금: {fee}원")
 
 
 membership = input("회원 등급 입력: ")
@@ -6,42 +25,22 @@ parkingTime = int(input("주차 시간 입력: "))
 print(f"회원 등급: {membership}")
 print(f"주차 시간: {parkingTime}")
 if membership == "플래티넘" or membership == "골드":
-    if parkingTime > 120:
-        fee = (parkingTime - 120) // 10 * 1000
-        print(f"주차 요금: {fee}원")
-    else: # if parkingTime <= 120:
-        print("주차 요금 0원")
+    over120(parkingTime)
 elif membership == "실버" or membership == "프렌즈":
     purchased = int(input("구매 금액 입력: "))
     if purchased >= 30000:
-        if parkingTime > 120:
-            fee = (parkingTime - 120) // 10 * 1000
-            print(f"주차 요금: {fee}원")
-        else: # if parkingTime <= 120:
-            print("주차 요금 0원")
+        over120(parkingTime)
     elif purchased >= 10000:
-        if parkingTime > 60:
-            fee = (parkingTime - 60) // 10 * 1000
-            print(f"주차 요금: {fee}원")
-        else: # if parkingTime <= 60:
-            print("주차 요금 0원")
+        over60(parkingTime)
     else:
-        fee = parkingTime // 10 * 1000
-        print(f"주차 요금: {fee}원")
+        else10(parkingTime)
+
 elif membership == "비회원":
     purchased = int(input("구매 금액 입력: "))
     if purchased >= 50000:
-        if parkingTime > 120:
-            fee = (parkingTime - 120) // 10 * 1000
-            print(f"주차 요금: {fee}원")
-        else: # if parkingTime <= 120:
-            print("주차 요금 0원")
+        over120(parkingTime)
     elif purchased >= 30000:
         if parkingTime > 60:
-            fee = (parkingTime - 60) // 10 * 1000
-            print(f"주차 요금: {fee}원")
-        else: # if parkingTime <= 60:
-            print("주차 요금 0원")
+            over60(parkingTime)
     else:
-        fee = parkingTime // 10 * 1000
-        print(f"주차 요금: {fee}원")
+        else10(parkingTime)
