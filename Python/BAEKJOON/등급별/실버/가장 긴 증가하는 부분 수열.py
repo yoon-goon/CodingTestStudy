d06ba96dc7b344762ab378d3adc0f47b59111cdb
tjrwls0826 <tjrwls0826@naver.com>
2023-04-16 05:16:28 +0900

    Update 가장 긴 증가하는 부분 수열.py

--- Python/BAEKJOON/등급별/실버/가장 긴 증가하는 부분 수열.py
+++ Python/BAEKJOON/등급별/실버/가장 긴 증가하는 부분 수열.py
@@ -3,8 +3,12 @@ import sys
 
 N = int(input())
 
+A = list(map(int, sys.stdin.readline().split()))
 
-A = list(map(int,sys.stdin.readline().split()))
+cnt = 1
 
+for i in range(1, len(A)):
+    if A[i] > A[i - 1]:
+        cnt += 1
 
-print(A)
+print(cnt)
