464a89dee01300bae0d66c42b014184fc150ec83
tjrwls0826 <tjrwls0826@naver.com>
2023-04-21 01:41:30 +0900

    Update 실습 #02-1.py

--- Python/수업 실습/실습/실습 #02-1.py
+++ Python/수업 실습/실습/실습 #02-1.py
@@ -7,21 +7,29 @@
 
 oilprice1km = 190
 # 키로미터당 60/40 분
-carkmtime = 60/40
-print("키로미터당 분",carkmtime)
+carkmtime = 60 / 40
+print("키로미터당 분", carkmtime)
 workprice = 9160 / 60
-print("분당 임금",workprice)
-ofAdistnce = int(input("A의 거리"))
-ofAprice = int(input("A의 가격"))
-A = ofAprice + (ofAdistnce * oilprice1km + ofAdistnce * carkmtime * workprice)*2
+print("분당 임금", workprice)
+ofAdistnce = float(input("A의 거리"))
+ofAprice = float(input("A의 가격"))
+A = ofAprice + (ofAdistnce * oilprice1km + ofAdistnce * carkmtime * workprice) * 2
 print(f"A의 가격은{A}")
 
-ofBdistnce = int(input("B의 거리"))
-ofBprice = int(input("B의 가격"))
-B = ofBprice + (ofBdistnce * oilprice1km + ofBdistnce * carkmtime * workprice)*2
+ofBdistnce = float(input("B의 거리"))
+ofBprice = float(input("B의 가격"))
+B = ofBprice + (ofBdistnce * oilprice1km + ofBdistnce * carkmtime * workprice) * 2
 print(f"B의 가격은{B}")
 
-onprice = int(input())
-ondelivery = int(input())
+onprice = float(input())
+ondelivery = float(input())
 
+on = onprice + ondelivery
+print(f"온라인의 가격은{on}")
 
+if min(A, B, on) == A:
+    print('best place to buy is A쇼핑몰')
+elif min(A, B, on) == B:
+    print('best place to buy is B쇼핑몰')
+else:
+    print("온라인 쇼핑몰가세요")
