36faa4302a69d3b77a77c2590ebea500ee31bbcf
tjrwls0826 <tjrwls0826@naver.com>
2023-04-21 21:39:21 +0900

    Update 실습 #05-1.py

--- Python/수업 실습/실습/실습 #05-1.py
+++ Python/수업 실습/실습/실습 #05-1.py
@@ -1,9 +1,15 @@
 import random
 
-computer = random.randint(1,6)
+cpu = random.randint(1, 6)
 
-human = int(input("1부터 6까지의 정수값을 입력하세요"))
-if not 1 <= human <= 6:
+print("컴퓨터의 주사위:%d", cpu)
 
-print(f"cpu:{computer}")
-print(f"Human:{human}")
+# import random
+#
+# computer = random.randint(1,6)
+#
+# human = int(input("1부터 6까지의 정수값을 입력하세요"))
+# if not 1 <= human <= 6:
+#
+# print(f"cpu:{computer}")
+# print(f"Human:{human}")
