4fe37ba4b328db29394e6312fca24f071294bb00
tjrwls0826 <tjrwls0826@naver.com>
2023-04-22 01:36:33 +0900

    Update 실습 #05-1.py

--- Python/수업 실습/실습/실습 #05-1.py
+++ Python/수업 실습/실습/실습 #05-1.py
@@ -1,8 +1,25 @@
+# Same as 실습 #03-2
+
 import random
 
 cpu = random.randint(1, 6)
 
-print("컴퓨터의 주사위:%d", cpu)
+
+human = int(input())
+
+if not 1 <= human <= 6:
+    print("오류 1-6사이 값을 입력하세요")
+else:
+    print("컴퓨터의 주사위:", cpu)
+    print("사용자의 주사위:", human)
+    if cpu > human:
+        print("컴퓨터 승리!:", cpu)
+    elif cpu == human:
+        print("비김!", cpu)
+    elif cpu < human:
+        print("사용자승리!", human)
+
+
 
 # import random
 #
