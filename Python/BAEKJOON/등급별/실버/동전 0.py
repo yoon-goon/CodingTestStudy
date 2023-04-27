88d45e3ba0a298d2900e23d967eca62feda07dd2
tjrwls0826 <tjrwls0826@naver.com>
2023-04-28 04:50:59 +0900

    Update 동전 0.py

--- Python/BAEKJOON/등급별/실버/동전 0.py
+++ Python/BAEKJOON/등급별/실버/동전 0.py
@@ -1,6 +1,14 @@
 # https://www.acmicpc.net/problem/11047
+import sys
 
 kind, csum = map(int, input().split())
+coinL = []
 print(kind, csum)
 
 for i in range(kind):
+    coinL.append(sys.stdin.readline().strip())
+
+print(coinL)
+
+for i in range(kind,0,-1):
+    print(i)
