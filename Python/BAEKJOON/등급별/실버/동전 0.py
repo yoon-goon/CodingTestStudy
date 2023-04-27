eaae72202b6b53a2ac7c13ee9d4bfe3260776b54
tjrwls0826 <tjrwls0826@naver.com>
2023-04-28 04:58:27 +0900

    Update 동전 0.py

--- Python/BAEKJOON/등급별/실버/동전 0.py
+++ Python/BAEKJOON/등급별/실버/동전 0.py
@@ -3,6 +3,8 @@ import sys
 
 kind, csum = map(int, input().split())
 coinL = []
+sum = 0
+count = 0
 print(kind, csum)
 
 for i in range(kind):
@@ -10,5 +12,16 @@ for i in range(kind):
 
 print(coinL)
 
-for i in range(kind,0,-1):
+for i in range(kind - 1, -1, -1):
+    if sum == csum:
+        break
+    sum += int(coinL[i])
+    count += 1
+    if sum > csum:
+        sum -= int(coinL[i])
+        count -= 1
+
+    print(sum)
     print(i)
+
+print(count)
