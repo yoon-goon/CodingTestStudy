5de77ae9a33b5d50c868b13587cf228cdb0897df
tjrwls0826 <tjrwls0826@naver.com>
2023-05-01 13:46:43 +0900

    Update 실습과제 #05.py

--- Python/수업 실습/실습과제/실습과제 #05.py
+++ Python/수업 실습/실습과제/실습과제 #05.py
@@ -7,14 +7,11 @@
  반복되는 코드를 반복문을 이용해서 줄일 것
 '''
 
-
 t = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis nisi at dapibus semper. Ut quis sapien vulputate, pharetra sapien vel, commodo diam. Nam tincidunt nulla sit amet neque iaculis, at interdum erat ultrices. Donec nec turpis sagittis, malesuada dui ut, pellentesque magna. Integer ultricies pharetra ex ut semper. Pellentesque ex orci, rhoncus vitae dolor in, vulputate volutpat felis. Fusce quis ornare purus."
 
 def find(s,ss,pos):
     a = s.find(ss,pos)
     return a
-
-#print(find("help me",'e',3))
 
 def getNextPeriodPos(txt, startpos):
     # txt.find('.')
@@ -25,7 +22,6 @@ def getNextPeriodPos(txt, startpos):
         return -1  # find 에서도 자체적으로 -1을 출력하지만 이렇게 구현할 수도 있음
     return txt.find('.', startpos, len(txt))
 
-
 def getNextLine(txt, startpos):
     if startpos >= len(txt):
         return None
@@ -35,53 +31,16 @@ def getNextLine(txt, startpos):
     else:
         return txt[startpos:idx + 1]
 
-
 def main():
     idx = 0
-
-    idx = getNextPeriodPos(t, idx)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)
-    print(idx)
-    idx = getNextPeriodPos(t, idx + 1)  # -1
-    print(idx)
-
+    for i in range(8): # (while not -1)
+        print(find(t, '.',idx))
+        idx = find(t, '.',idx) + 1
     idx = 0
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
-    idx += len(s)
-    s = getNextLine(t, idx)
-    print(s)
+    for i in range(8): # (while not None)
+        s = getNextLine(t, idx)
+        print(s)
+        idx = find(t, '.',idx) + 1
 
 
 main()
