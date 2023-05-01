457d1e80984b4ef4836c38dfe6e8dd4e8941a6bb
tjrwls0826 <tjrwls0826@naver.com>
2023-05-01 13:48:22 +0900

    Update 실습과제 #05.py

--- Python/수업 실습/실습과제/실습과제 #05.py
+++ Python/수업 실습/실습과제/실습과제 #05.py
@@ -9,18 +9,17 @@
 
 t = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis nisi at dapibus semper. Ut quis sapien vulputate, pharetra sapien vel, commodo diam. Nam tincidunt nulla sit amet neque iaculis, at interdum erat ultrices. Donec nec turpis sagittis, malesuada dui ut, pellentesque magna. Integer ultricies pharetra ex ut semper. Pellentesque ex orci, rhoncus vitae dolor in, vulputate volutpat felis. Fusce quis ornare purus."
 
-def find(s,ss,pos):
-    a = s.find(ss,pos)
+
+def find(s, ss, pos):
+    a = s.find(ss, pos)
     return a
 
-def getNextPeriodPos(txt, startpos):
-    # txt.find('.')
-    # txt.find('.', start, end) start부터 end까지 탐색
-    # txt.find('.', start) start부터 끝까지
 
+def getNextPeriodPos(txt, startpos):
     if startpos >= len(txt):
         return -1  # find 에서도 자체적으로 -1을 출력하지만 이렇게 구현할 수도 있음
     return txt.find('.', startpos, len(txt))
+
 
 def getNextLine(txt, startpos):
     if startpos >= len(txt):
@@ -31,16 +30,17 @@ def getNextLine(txt, startpos):
     else:
         return txt[startpos:idx + 1]
 
+
 def main():
     idx = 0
-    for i in range(8): # (while not -1)
-        print(find(t, '.',idx))
-        idx = find(t, '.',idx) + 1
+    for i in range(8):  # (while not -1)
+        print(find(t, '.', idx))
+        idx = find(t, '.', idx) + 1
     idx = 0
-    for i in range(8): # (while not None)
+    for i in range(8):  # (while not None)
         s = getNextLine(t, idx)
         print(s)
-        idx = find(t, '.',idx) + 1
+        idx = find(t, '.', idx) + 1
 
 
 main()
