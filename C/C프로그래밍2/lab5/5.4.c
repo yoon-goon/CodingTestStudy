db866231803dbd8b05f7133bc1ce767a0e34ec7f
tjrwls0826 <tjrwls0826@naver.com>
2023-05-05 23:23:55 +0900

    Update 5.4.c

--- C/C프로그래밍2/lab5/5.4.c
+++ C/C프로그래밍2/lab5/5.4.c
@@ -2,20 +2,21 @@
 
 void b(int *p)
 {
-	printf("%d %d %d\n", p[0],p[-1],p[-2]);
-	return;
+        printf("%d %d %d\n", p[0],p[-1],p[-2]);
+        return;
 }
 
 int main(void)
 {
-	int a[] = {0,1,2,3,4,5,6,7,8,9};
+        int a[] = {0,1,2,3,4,5,6,7,8,9};//배열 a 선언
 
-	int n = (int)(sizeof(a)/sizeof(int));
-	for(int i = 0; i<n;i++)
-		printf("%d ", a[i]);
-	putchar('\n');
+        int n = (int)(sizeof(a)/sizeof(int));//n = 배열 a의 크기
+        for(int i = 0; i<n;i++)
+                printf("%d ", a[i]);// 배열의 끝까지 출력
+        putchar('\n');
 
-	b(a+n-1);
+        b(a+n-1);//a배열의 마지막을 가리키는 포인터
+        // 그러므로 p[0], p[-1], p[-2]는 마지막원소부터 앞쪽으로 2개
 
-	return 0;
+        return 0;
 }
