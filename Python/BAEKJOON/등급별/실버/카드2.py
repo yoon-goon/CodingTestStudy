4dedfd36f3f86c4b5131c8817a03e81e0b64bc4c
tjrwls0826 <tjrwls0826@naver.com>
2023-05-13 01:36:47 +0900

    Update 카드2.py

--- Python/BAEKJOON/등급별/실버/카드2.py
+++ Python/BAEKJOON/등급별/실버/카드2.py
@@ -7,3 +7,10 @@ alist = []
 
 for i in range(1, N + 1):
     alist.append(i)
+
+while len(alist) != 1:
+    alist.remove(alist[0])
+    alist.append(alist[0])
+    alist.remove(alist[0])
+
+print(*alist)
