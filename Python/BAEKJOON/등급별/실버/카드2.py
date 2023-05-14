4e30a355da0a57041e9c6802d15c8d60dc46da3f
tjrwls0826 <tjrwls0826@naver.com>
2023-05-14 19:04:23 +0900

    Update 카드2.py

--- Python/BAEKJOON/등급별/실버/카드2.py
+++ Python/BAEKJOON/등급별/실버/카드2.py
@@ -1,6 +1,26 @@
 # https://www.acmicpc.net/problem/2164
 
 
+
+'''
+N = int(input())
+
+alist = []
+
+for i in range(1, N + 1): // 이 부분을 최적화 할수 있을것 같음
+    alist.append(i)
+
+while len(alist) != 1:
+    alist.remove(alist[0])
+    alist.append(alist[0])
+    alist.remove(alist[0])
+
+print(*alist)
+
+# 시간 초과
+
+'''
+
 N = int(input())
 
 alist = []
@@ -14,5 +34,3 @@ while len(alist) != 1:
     alist.remove(alist[0])
 
 print(*alist)
-
-# 시간 초과
