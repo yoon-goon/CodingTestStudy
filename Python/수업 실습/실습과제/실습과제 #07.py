a537ecd54ae9a121e3dba819493b774505edb7d8
tjrwls0826 <tjrwls0826@naver.com>
2023-05-17 21:25:38 +0900

    Update 실습과제 #07.py

--- Python/수업 실습/실습과제/실습과제 #07.py
+++ Python/수업 실습/실습과제/실습과제 #07.py
@@ -1,11 +1,38 @@
+def findChar(cList, ch):
+    if len(cList) > 0:
+        for lst in cList:
+            if lst[0] == ch:
+                return lst
+    return None
 
 
+def countChars(txt):
+    txt = txt.lower()
+    for ch in txt:
+        lst = findChar(clist, ch)
+        if lst == None:
+            clist.append([ch, 1])
+        else:
+            lst[1] += 1
+    return clist
 
 
-f = open("lorem.txt")
-s = f.readline()
+def printList(cList):
+    for lst in cList:
+        print(f"{lst[0]}: {lst[1]}")
 
-while s:
-    print(s,end="")
+
+def main():
+    f = open("lorem.txt")
     s = f.readline()
-f.close()
+
+    global clist
+    clist = []
+
+    while s:
+        print(s, end="")
+        #print(countChars(s))
+        s = f.readline()
+    f.close()
+
+main()
