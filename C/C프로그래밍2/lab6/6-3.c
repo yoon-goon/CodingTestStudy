fb9cb09b89344c14b52f54ec43cf2194b59ac9b7
tjrwls0826 <tjrwls0826@naver.com>
2023-05-20 04:55:21 +0900

    Update 6-3.c

--- C/C프로그래밍2/lab6/6-3.c
+++ C/C프로그래밍2/lab6/6-3.c
@@ -9,8 +9,17 @@ int binsearch(char *, struct key *, int);
 
 int main()
 {
+    int n;
+    char word[MAXWORD];
+    
+    while (getword(word, MAXWORD) != EOF) 
+        if (isalpha(word[0])) 
+            if ((n = binsearch(word, keytab, NKEYS)) >= 0) 
+                keytab[n].count++;
+    for (n = 0; n < NKEYS; n++) 
+        if (keytab[n].count > 0) 
+            printf("%4d %s\n", keytab[n].count, keytab[n].word);
         
-        
-
-        return 0;
-}  
+    
+    return 0;
+}
