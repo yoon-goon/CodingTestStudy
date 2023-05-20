196204c2504bcb8572912ce09a5bb29eece00c62
tjrwls0826 <tjrwls0826@naver.com>
2023-05-20 16:27:14 +0900

    Update 6-4.c

--- C/C프로그래밍2/lab6/6-4.c
+++ C/C프로그래밍2/lab6/6-4.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAXWORD 100
+#define BUFSIZE 100
 
 struct tnode {
         char *word;
         int count;
         struct tnode *left;
         struct tnode *right;
-}
+};
 
 struct tnode *addtree(struct tnode *, char *);
 struct tnode *talloc(void);
@@ -61,3 +63,49 @@ int getword( char *word, int lim)
 }
 
 struct tnode *addtree(struct tnode *p, char *w) {
+	int cond;
+
+	if(p == NULL) {                       /* a new word has arrived */
+		p = talloc();                        /* make a new node */
+		p->word = strdup(w); // getword에서 나온 문자열
+		p->count = 1;
+		p->left = p->right = NULL;
+ } 	else if((cond = strcmp(w, p->word)) == 0)
+		p->count++;                          /* repeated word */
+	else if(cond < 0)                     /* less than into left subtree */
+		p->left = addtree(p->left, w);
+	else                                  /* greater than into right subtree */
+		p->right = addtree(p->right, w);
+
+	return p;
+}
+
+struct tnode *talloc(void) {
+	return(struct tnode *)malloc(sizeof(struct tnode));
+}
+
+void treeprint(struct tnode *p) {
+	if(p != NULL) {
+	treeprint(p->left);
+	printf("%4d %s\n", p->count, p->word);
+	treeprint(p->right);
+	}
+}
+
+// 이전 함수 재사용
+
+int getch(void)
+{
+        return (bufp > 0) ? buf[--bufp] : getchar();
+        // 0보다 크면 버퍼에 저장되어 있는 것, 저장된 문자 반환. 그렇지 않다면 getchar()로 새로운 문자를 입력, 반환
+}
+
+void ungetch(int c)
+{
+  if (c == EOF)
+    return;
+  if (bufp >= BUFSIZE)
+    printf ("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
