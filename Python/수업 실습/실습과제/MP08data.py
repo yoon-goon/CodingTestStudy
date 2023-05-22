0fe97f4fd5f872b7c754e934ac5f25470ab67fe2
tjrwls0826 <tjrwls0826@naver.com>
2023-05-22 20:58:31 +0900

    MP08

--- Python/수업 실습/실습과제/MP08data.py
+++ Python/수업 실습/실습과제/MP08data.py
@@ -2,10 +2,10 @@ import os
 
 print(os.getcwd())
 
-filename = input("파일이름을 입력하세요: ")  # 주석 1번
+filename = input("input file name: ")  # 주석 1번
 try:
     with open(filename, encoding="utf-8") as f:  # 주석 2번
         lst = f.readlines()
 
 except FileNotFoundError:
-    filename = input("파일을 찾지 못했습니다. 다시한번 입력해 주세요")  # 주석 3번
+    filename = input("Couldn't find the file, input again")  # 주석 3번
--- Python/수업 실습/실습과제/실습과제 #08.py
+++ Python/수업 실습/실습과제/실습과제 #08.py
@@ -2,19 +2,23 @@ import os
 
 print(os.getcwd())
 
-filename = input("파일이름을 입력하세요: ")
+filename = input("input file name: ")
 
 
 def writeNew(s):
     with open(s, "w") as f1:
-        f1.write(lst)
+        idx = 0
+        while idx < len(lst):
+            f1.write(lst[idx])
+            idx += 1
 
 
 try:
     with open(filename, encoding="utf-8") as f:
         lst = f.readlines()
+        print(lst)
         newfile = "cr_" + filename
         writeNew(newfile)
 
 except FileNotFoundError:
-    filename = input("파일을 찾지 못했습니다. 다시한번 입력해 주세요")
+    filename = input("Couldn't find the file, input again: ")
