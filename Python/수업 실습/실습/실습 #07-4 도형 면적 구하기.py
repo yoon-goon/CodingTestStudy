6c25fdf7f2f0966cc3951b4a51fb57e2b4b94ebf
tjrwls0826 <tjrwls0826@naver.com>
2023-05-22 13:17:53 +0900

    Update 실습 #07-4 도형 면적 구하기.py

--- Python/수업 실습/실습/실습 #07-4 도형 면적 구하기.py
+++ Python/수업 실습/실습/실습 #07-4 도형 면적 구하기.py
@@ -3,6 +3,25 @@ def inputRect():
     y1 = int(input("Y1입력: "))
     x2 = int(input("X2입력: "))
     y2 = int(input("Y2입력: "))
-    return (x1,y1,x2,y2)
+    return (x1, y1, x2, y2)
 
-print(inputRect())
+
+def inputRect2():
+    t = tuple()
+    lst = ["x1", "y1", "x2", "y2"]
+    for i in lst:
+        int(input(i + " 입력: "))
+
+
+def inputTriangle():
+    x1 = int(input("X1입력: "))
+    y1 = int(input("Y1입력: "))
+    x2 = int(input("X2입력: "))
+    y2 = int(input("Y2입력: "))
+    x3 = int(input("X3입력: "))
+    y3 = int(input("Y3입력: "))
+    return (x1, y1, x2, y2, x3, y3)
+
+
+# print(inputRect())
+print(inputTriangle())
