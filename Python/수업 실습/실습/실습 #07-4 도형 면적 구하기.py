7b933f449714dc26b331f82dd899dbb1497b55d9
tjrwls0826 <tjrwls0826@naver.com>
2023-05-22 13:13:10 +0900

    Create 실습 #07-4 도형 면적 구하기.py

---
+++ Python/수업 실습/실습/실습 #07-4 도형 면적 구하기.py
@@ -0,0 +1,8 @@
+def inputRect():
+    x1 = int(input("X1입력: "))
+    y1 = int(input("Y1입력: "))
+    x2 = int(input("X2입력: "))
+    y2 = int(input("Y2입력: "))
+    return (x1,y1,x2,y2)
+
+print(inputRect())
