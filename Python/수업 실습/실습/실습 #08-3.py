44dadc8ec2e5baaaee56a5004a787a4bcc74b1e2
tjrwls0826 <tjrwls0826@naver.com>
2023-05-22 14:50:30 +0900

    Update 실습 #08-3.py

--- Python/수업 실습/실습/실습 #08-3.py
+++ Python/수업 실습/실습/실습 #08-3.py
@@ -1,9 +1,33 @@
 def readFile(fileName):
     with open(fileName) as f:
-        return f.readlines()
+        strLst = f.readlines()
+    floatlist = []
+    for s in strLst:
+        floatlist.append(float(s))
+    return floatlist
 
 
 lst1 = readFile("MP09Data1.txt")
 lst2 = readFile("MP09Data2.txt")
 
-print(lst1, lst2)
+idx1, idx2 = 0, 0
+
+while idx1 < len(lst1) and idx2 < len(lst2):
+    if lst1[idx1] >= lst2[idx2]:
+        print(lst1[idx1])
+        idx1 += 1
+    else:
+        print(lst2[idx2])
+        idx2 += 1
+
+# 윗 단계에서 어느부분에서 멈췄는지 알수 없기에
+while idx1 < len(lst1):
+    print(lst1[idx1])
+    idx1 += 1
+while idx2 < len(lst2):
+    print(lst2[idx2])
+    idx2 += 1
+
+# elif lst1[idx1] < lst2[idx2]
+
+# print(lst1, lst2)
