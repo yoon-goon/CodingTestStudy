9d69506a3dd740b447d1ffc63e5462199a5838e6
tjrwls0826 <tjrwls0826@naver.com>
2023-05-23 21:57:04 +0900

    Update 카드2.py

--- Python/BAEKJOON/등급별/실버/카드2.py
+++ Python/BAEKJOON/등급별/실버/카드2.py
@@ -31,7 +31,7 @@ print(*alist)
 # 
 # print(*alist)
 
-from collections import deque # deque를 이용한 풀이
+from collections import deque  # deque를 이용한 풀이
 
 N = int(input())
 
@@ -40,9 +40,14 @@ alist = deque()
 for i in range(1, N + 1):
     alist.append(i)
 
+'''
+이렇게도 가능
+queue = deque(range(1, int(input())+1))
+'''
+
 while len(alist) > 1:
-    alist.popleft() #가장 왼쪽 빼고
-    alist.append(alist.popleft()) # 한번더 가장 왼쪽 빼고 뺀 그 que를 가장 오른쪽에 append
+    alist.popleft()  # 가장 왼쪽 빼고
+    alist.append(alist.popleft())  # 한번더 가장 왼쪽 빼고 뺀 그 que를 가장 오른쪽에 append
 
 print(*alist)  # unpack
 
