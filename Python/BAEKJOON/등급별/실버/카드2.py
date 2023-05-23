5dcbc690fb5c0f53c3196a67caa0120fae0eb235
tjrwls0826 <tjrwls0826@naver.com>
2023-05-23 21:56:04 +0900

    Update 카드2.py

--- Python/BAEKJOON/등급별/실버/카드2.py
+++ Python/BAEKJOON/등급별/실버/카드2.py
@@ -6,7 +6,7 @@ N = int(input())
 
 alist = []
 
-for i in range(1, N + 1): // 이 부분을 최적화 할수 있을것 같음
+for i in range(1, N + 1): # 이 부분을 최적화 할수 있을것 같음
     alist.append(i)
 
 while len(alist) != 1:
@@ -31,16 +31,19 @@ print(*alist)
 # 
 # print(*alist)
 
+from collections import deque # deque를 이용한 풀이
 
 N = int(input())
 
-alist = list(range(1, N + 1))
+alist = deque()
 
-while len(alist) > 1:  # 시간초과발생 구하는 방법을 개선해야할 것으로 보임
-    alist.remove(alist[0])
-    alist.append(alist[0])
-    alist.remove(alist[0])
+for i in range(1, N + 1):
+    alist.append(i)
 
-print(*alist) # unpack
+while len(alist) > 1:
+    alist.popleft() #가장 왼쪽 빼고
+    alist.append(alist.popleft()) # 한번더 가장 왼쪽 빼고 뺀 그 que를 가장 오른쪽에 append
 
-# 참고 https://my-coding-notes.tistory.com/83
+print(*alist)  # unpack
+
+# 참고 https://suri78.tistory.com/50
