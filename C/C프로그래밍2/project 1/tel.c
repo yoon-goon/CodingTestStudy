a9f64ec4b7b99c8dc7947a3b05d51159d0a65d6c
tjrwls0826 <tjrwls0826@naver.com>
2023-05-26 22:34:16 +0900

    Update tel.c

--- C/C프로그래밍2/project 1/tel.c
+++ C/C프로그래밍2/project 1/tel.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#MAX_CONTACTS 100
+#define MAX_CONTACTS 100
 
 
 struct Contact {
         char name[30];
         char phone[20];
         char memo[40];
-}  
+};
+
+
+int main() {
+    char filename[] = "data.txt";
+    FILE *file = fopen(filename, "a");
+
+    if (file == NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
+    }
+}
+    
