68f024c409480f619882bd6345db37386c709fee
tjrwls0826 <tjrwls0826@naver.com>
2023-05-26 22:41:41 +0900

    Update tel.c

--- C/C프로그래밍2/project 1/tel.c
+++ C/C프로그래밍2/project 1/tel.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_CONTACTS 100
-
+#define MAX_Contacts 100
 
 struct Contact {
         char name[30];
@@ -12,7 +11,17 @@ struct Contact {
 };
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        printf("잘못된 명령어입니다. 사용법: ./tel -a 이름 전화번호 메모(옵션)\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-a") != 0) {
+        printf("잘못된 옵션입니다. -a 옵션을 사용하세요.\n");
+        return 1;
+    }
+
     char filename[] = "data.txt";
     FILE *file = fopen(filename, "a");
 
@@ -20,5 +29,17 @@ int main() {
         printf("파일을 열 수 없습니다.\n");
         return 1;
     }
+
+    // 파일에 추가할 내용을 문자열로 생성
+    char entry[MAX_Contacts];
+    snprintf(entry, MAX_Contacts, "%s:%s:%s\n", argv[2], argv[3], argv[4]);
+
+    // 파일에 내용 추가
+    fputs(entry, file);
+
+    printf("data.txt 파일에 추가되었습니다.\n");
+
+    fclose(file);
+
+    return 0;
 }
-    
