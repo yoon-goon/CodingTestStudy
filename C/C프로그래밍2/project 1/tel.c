8b4a65101a9f687b35a4321559c57105765ab054
tjrwls0826 <tjrwls0826@naver.com>
2023-05-28 20:55:32 +0900

    Update tel.c

--- C/C프로그래밍2/project 1/tel.c
+++ C/C프로그래밍2/project 1/tel.c
@@ -12,49 +12,88 @@ struct Contact {
 
 
 int main(int argc, char *argv[]) {
-    if (argc < 4 || argc > 5) {
+    if (argc != 2 && (argc < 4 || argc > 5)) {
         printf("잘못된 명령어입니다. 사용법: ./tel -a 이름 전화번호 메모(옵션)\n");
         return 1;
     }
 
-    if (strcmp(argv[1], "-a") != 0) { // 다른 명령도 수행하도록 수정해야함
-        printf("잘못된 옵션입니다. -a 옵션을 사용하세요.\n");
-        return 1;
-    }
+    if (strcmp(argv[1], "-a") == 0) {
+        if (argc < 4 || argc > 5) {
+            printf("-a 옵션을 사용할 때는 이름, 전화번호, 메모(필수아님)를 입력하세요.\n");
+            return 1;
+        }
 
-    char filename[] = "data.txt";
-    FILE *file = fopen(filename, "a");
-
-    if (file == NULL) {
-        printf("파일을 열 수 없습니다.\n");
-        return 1;
-    }
+		char filename[] = "data.txt";
+		FILE *file = fopen(filename, "a");
 	
-	if (argc != 5) {
-		argv[4] = "";
-	}
+		if (file == NULL) {
+			printf("파일을 열 수 없습니다.\n");
+			return 1;
+		}
+		
+		if (argc != 5) {
+			argv[4] = "";
+		}
+		
+		printf("%s %s %s\n", argv[2], argv[3], argv[4]);
+		
+		printf("add? [Y/N]: ");
+		char answer;
+		scanf(" %c", &answer);	
 	
-	printf("%s %s %s\n", argv[2], argv[3], argv[4]);
+		if (answer == 'Y' || answer == 'y') {
+			// 파일에 추가할 내용을 문자열로 생성
+			char entry[MAX_Contacts];
+			snprintf(entry, MAX_Contacts, "%s:%s:%s\n", argv[2], argv[3], argv[4]);
 	
-	printf("add? [Y/N]: ");
-    char answer;
-    scanf(" %c", &answer);	
-
-	if (answer == 'Y' || answer == 'y') {
-		// 파일에 추가할 내용을 문자열로 생성
-		char entry[MAX_Contacts];
-		snprintf(entry, MAX_Contacts, "%s:%s:%s\n", argv[2], argv[3], argv[4]);
+			// 파일에 내용 추가
+			fputs(entry, file);
 	
-		// 파일에 내용 추가
-		fputs(entry, file);
-
-		printf("data.txt 파일에 추가되었습니다.\n");
-	}
-	else {
-		printf("저장하지 않고 프로그램을 종료합니다.\n");
-	}
+			printf("data.txt 파일에 추가되었습니다.\n");
+		}
+		else {
+			printf("저장하지 않고 프로그램을 종료합니다.\n");
+		}
 
     fclose(file);
+	}
+	else{
+		// search 구현
+		char number[20];
+        strcpy(number, argv[1]);
+
+        char filename[] = "data.txt";
+        FILE *file = fopen(filename, "r");
+
+        if (file == NULL) {
+            printf("파일을 열 수 없습니다.\n");
+            return 1;
+        }
+
+        char line[MAX_Contacts];
+        int found = 0;
+
+        while (fgets(line, sizeof(line), file) != NULL) {
+            char *name = strtok(line, ":");
+            char *phone = strtok(NULL, ":");
+            char *memo = strtok(NULL, ":");
+			int order = 1;
+			
+
+            if (strstr(phone, number) != NULL) {
+                printf("%d %s %s %s\n", order, name, phone, memo);
+                found = 1;
+				order += 1;
+            }
+        }
+
+        if (!found) {
+            printf("일치하는 정보가 없습니다.\n");
+        }
+
+        fclose(file);
+    }
+
 
     return 0;
 }
