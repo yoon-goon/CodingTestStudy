2105358ed0f10a0bbc223e0ee7828d61645bf3d3
tjrwls0826 <tjrwls0826@naver.com>
2023-06-01 23:11:51 +0900

    Update 실습 #08-1.py

--- Python/수업 실습/실습/실습 #08-1.py
+++ Python/수업 실습/실습/실습 #08-1.py
@@ -1,10 +1,13 @@
 import os
 
+def trylist(lst):
+    try:
+        print(os.listdir(lst))
+    except FileNotFoundError:
+        print("올바르지 않은 경로입니다.")
 
 lst = input("경로를 입력하세요")
 trylist(lst)
 
-def trylist(lst):
-    print(os.listdir(lst))
 
 
