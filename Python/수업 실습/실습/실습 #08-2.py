f8d869c0899f30944874fc2cbe663ecc07d041de
tjrwls0826 <tjrwls0826@naver.com>
2023-06-02 22:38:42 +0900

    Update 실습 #08-2.py

--- Python/수업 실습/실습/실습 #08-2.py
+++ Python/수업 실습/실습/실습 #08-2.py
@@ -5,18 +5,18 @@ def trylist(lst):
     try:
         f = open(lst)
         texts = f.readlines()
-        while texts:
-            print(texts, end="")
+        for txt in texts:
+            print(txt, end="")
     except FileNotFoundError:
         print("올바르지 않은 이름입니다.\n")
         try:
             lst = input("파일이름을 입력하세요 ")
             f = open(lst)
             texts = f.readlines()
-            while texts:
-                print(texts, end="")
+            for txt in texts:
+                print(txt, end="")
         except FileNotFoundError:
-            return
+            print("종료됩니다.")
 
 
 lst = input("파일이름을 입력하세요 ")
