162c62998f8c404412b0b844d6f94ad47e472799
tjrwls0826 <tjrwls0826@naver.com>
2023-06-05 14:50:54 +0900

    Update smartphone Class.py

--- Python/수업 실습/실습/클래스/smartphone Class.py
+++ Python/수업 실습/실습/클래스/smartphone Class.py
@@ -7,7 +7,12 @@ class SmartPhone:
     def call(self, phoneNum):
         if self.battery >= 10:
             print(f"{self.number}에서{phoneNum}으로 전화를 겁니다.")
-            hwcall(phoneNum) ##
+            self._hwcall(phoneNum) ##
     def getBatteryStatus(self):
         return self.battery
-    def _hwself ## 
+    def _hwcall(self,phoneNum):
+        print(f"하드웨어를 제어하여 {phoneNum}으로 전화겁니다.")
+
+ph1 = SmartPhone("Yoo","010-2222-4444", 80)
+ph1.call("010-4252-2422")
+print(ph1.getBatteryStatus())
