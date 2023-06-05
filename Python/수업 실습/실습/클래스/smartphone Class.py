ef758cd100a28cb06214e8785bcb8d1a74bf265b
tjrwls0826 <tjrwls0826@naver.com>
2023-06-05 14:40:23 +0900

    Create smartphone Class.py

---
+++ Python/수업 실습/실습/클래스/smartphone Class.py
@@ -0,0 +1,10 @@
+class SmartPhone:
+    def __init__(self, name, phoneNum, battery):
+        self.owner = name
+        self.number = phoneNum
+        self.battery = battery
+
+    def call(self, phoneNum):
+        if self.battery >= 10:
+            print(f"{self.number}에서{phoneNum}으로 전화를 겁니다.")
+            hwcall(phoneNum)
