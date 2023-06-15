77847bf89080bc51cbe495a96bbe1237d77685a9
tjrwls0826 <tjrwls0826@naver.com>
2023-06-15 23:34:35 +0900

    Update 실습 #06-1 풀이.py

--- Python/수업 실습/실습/실습 #06-1 풀이.py
+++ Python/수업 실습/실습/실습 #06-1 풀이.py
@@ -1,15 +1,23 @@
-def maxValueOutOfList(lst):
-   return max(lst)
-
-[1, 8, 3, 0, 10, 5, 4, 15]
-def maxValueOutOfList(lst):
-   m = lst[0]  # 현재 최대값
-   for i in range(1, len(lst)):
-       if m < lst[i]:
-           m = lst[i]
-   return m
-
-[1, 8, 3, 0, 10, 5, 4, 15]
-
+# def maxValueOutOfList(lst):
+#    return max(lst)
+#
+# # [1, 8, 3, 0, 10, 5, 4, 15]
+# def maxValueOutOfList(lst):
+#    m = lst[0]  # 현재 최대값
+#    for i in range(1, len(lst)):
+#        if m < lst[i]:
+#            m = lst[i]
+#    return m
+#
+# # [1, 8, 3, 0, 10, 5, 4, 15]
+#
 l = [1, 8, 3, 0, 10, 5, 4, 15]
+
+def maxValueOutOfList(lst):
+    m = lst[0]  # 현재 최대값
+    for i in lst:
+        if m < i:
+            m = i
+    return m
+
 print(maxValueOutOfList(l))
