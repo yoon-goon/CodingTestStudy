a5af61b1064db419f09275f9805ff6f5c9475864
tjrwls0826 <tjrwls0826@naver.com>
2023-06-16 23:57:04 +0900

    Update 실습 #07-3.py

--- Python/수업 실습/실습/실습 #07-3.py
+++ Python/수업 실습/실습/실습 #07-3.py
@@ -1,4 +1,9 @@
 lst = []
 
 with open("convert.ini") as f:
-    print(f)
+    lst2 = f.readlines()
+    lst.append(lst2[0].strip())
+
+    new_lst = [lst2[1].strip(), lst2[2].strip()]
+    lst.append(new_lst)
+print(lst)
