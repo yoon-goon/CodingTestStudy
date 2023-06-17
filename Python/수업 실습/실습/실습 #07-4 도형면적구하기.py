f19bc2e445f4b045b52053006f9401f8a545bfa8
tjrwls0826 <tjrwls0826@naver.com>
2023-06-17 23:37:00 +0900

    Update 실습 #07-4 도형면적구하기.py

--- Python/수업 실습/실습/실습 #07-4 도형면적구하기.py
+++ Python/수업 실습/실습/실습 #07-4 도형면적구하기.py
@@ -1,12 +1,14 @@
-a1 = input()
-a2 = input()
-a3 = input()
-a4 = input()
-a5 = input()
+shape = input("도형의 종류를 입력하세요: ")
 
-def rectangle():
-    x1 = int(input())
-    y1 = int(input())
-    x2 = int(input())
-    y2 = int(input())
 
+def input_rectangle():
+    x1 = int(input("x1좌표"))
+    y1 = int(input("y1좌표"))
+    x2 = int(input("x2좌표"))
+    y2 = int(input("y2좌표"))
+    return (x1, y1, x2, y2)
+
+
+def tup_rect():
+    t1 = tuple()
+    list = ()
