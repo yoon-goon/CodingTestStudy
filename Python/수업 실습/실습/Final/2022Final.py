e7deb27ea22e41998ecd69d84f2433b9ac4ad840
tjrwls0826 <tjrwls0826@naver.com>
2023-06-18 23:33:32 +0900

    update

---
+++ Python/수업 실습/실습/Final/2022Final.py
@@ -0,0 +1,12 @@
+def readData(filename):
+    dic = {}
+    typemoney = []
+    with open(filename,encoding='utf-8') as f:
+        line = f.readlines()
+        for i in line
+    return line
+
+
+print(readData("items.txt"))
+
+
