b809d49ed5e93e57ce83da9dc81cf27a27eaf33d
tjrwls0826 <tjrwls0826@naver.com>
2023-06-18 23:47:20 +0900

    Update 2022Final.py

--- Python/수업 실습/실습/Final/2022Final.py
+++ Python/수업 실습/실습/Final/2022Final.py
@@ -1,9 +1,36 @@
 def readData(filename):
     dic = {}
     typemoney = []
+
     with open(filename,encoding='utf-8') as f:
         line = f.readlines()
-        for i in line
+        for i in line:
+            print(i)
+            first = i.find(',')
+            print(first)
+            a = i[:first].strip()
+            b = i[first:].find(',')
+            dic.update({a:b})
+            print(dic)
+
+
+
+
+    # with open(filename,encoding='utf-8') as f: #반복문을 이용해 , 찾는방법
+    #     line = f.readlines()
+    #     for i in line:
+    #         tmp = ''
+    #         tmplist = []
+    #         for y in i:
+    #             if y == ',':
+    #                 for y in
+    #                 break
+    #             else:
+    #                 tmp += y
+    #         print(tmp)
+
+
+
     return line
 
 
