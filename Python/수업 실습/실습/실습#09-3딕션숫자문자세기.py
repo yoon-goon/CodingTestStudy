1f3822da4f9cf90090751b0d60140dfa66c5240a
tjrwls0826 <tjrwls0826@naver.com>
2023-06-18 22:14:27 +0900

    prac 09

--- Python/수업 실습/실습/실습#09-3딕션숫자문자세기.py
+++ Python/수업 실습/실습/실습#09-3딕션숫자문자세기.py
@@ -3,3 +3,13 @@ s = "D:\Program Files (x86)\Python 310\python.exe"
 d = {"alpha": 0, "digit": 0}
 
 for ch in s:
+    if ch.isalpha():
+        d["alpha"] += 1
+    elif ch.isdigit():
+        d["digit"] += 1
+print(d)
+
+a = d["alpha"]
+b = d["digit"]
+print(f"alpha: {a}")
+print(f"digit: {b}")
