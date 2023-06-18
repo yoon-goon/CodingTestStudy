08f7d2c87793d6a8435ffcae72c25fa265e9ce50
tjrwls0826 <tjrwls0826@naver.com>
2023-06-19 00:15:18 +0900

    Update 2022Final.py

--- Python/수업 실습/실습/Final/2022Final.py
+++ Python/수업 실습/실습/Final/2022Final.py
@@ -16,30 +16,44 @@ def readData(filename):
     #         dic.update({a: b})
     #         print(dic)
 
-    with open(filename, encoding='utf-8') as f:  # 반복문을 이용해 , 찾는방법
-        line = f.readlines()
-        for i in line:
-            tmp = ''
-            type = ''
-            price = ''
-            tmplist = []
-            for y in i:
-                if y == ',':
-                    for x in i[len(tmp) + 1:]:
-                        if x == ',':
-                            for z in i[len(tmp) + 1 + len(type) + 1:]:
-                                price += z
-                            break
-                        else:
-                            type += x
-                    break
-                else:
-                    tmp += y
-            print(tmp)
-            print(type)
-            print(price)
+    try:
+        with open(filename, encoding='utf-8') as f:  # 반복문을 이용해 , 찾는방법
+            line = f.readlines()
+            for i in line:
+                tmp = ''
+                type = ''
+                price = ''
+                tmplist = []
+                for y in i:
+                    if y == ',':
+                        for x in i[len(tmp) + 1:]:
+                            if x == ',':
+                                for z in i[len(tmp) + 1 + len(type) + 1:]:
+                                    price += z
+                                break
+                            else:
+                                type += x
+                        break
+                    else:
+                        tmp += y
+                try:
+                    dic.update({tmp: [type, int(price.strip())]})
+                except ValueError:
+                    print("가격에 정수가 아닌 문자열이 있습니다.")
+                    return 0
 
-    return line
+    except FileNotFoundError:
+        print("파일을 찾을 수 없습니다.")
+        return 0
+    return dic
 
 
-readData("items.txt")
+def printItem(dic, itemName):
+    pass
+
+
+
+def main(filename):
+    print(readData(filename))
+
+main("items.txt")
