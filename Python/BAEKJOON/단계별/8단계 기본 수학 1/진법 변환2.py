893fdb91a0b66869c56d6748d88f078c8136d6f5
tjrwls0826 <tjrwls0826@naver.com>
2023-06-24 03:28:57 +0900

    Update 진법 변환2.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/진법 변환2.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/진법 변환2.py
@@ -1,6 +1,6 @@
 # https://www.acmicpc.net/problem/11005
 
-N, a = input().split()  # N 진법
+N, a = map(int,input().split())  # N 진법
 
 lst = []
 
@@ -9,4 +9,8 @@ while N != 0:
     if a >= 10:
         lst.append(chr(b+55))
     else:
-        
+        lst.append(str(b))
+    N = N // a
+
+print(*lst)
+
