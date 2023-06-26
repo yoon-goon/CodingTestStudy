5d9b791116644e45ca584669230914448c33b475
tjrwls0826 <tjrwls0826@naver.com>
2023-06-27 05:16:05 +0900

    Update 세탁소 사장 동혁.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/세탁소 사장 동혁.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/세탁소 사장 동혁.py
@@ -5,4 +5,6 @@ times = int(input())
 for - in range(times):
     totalM = int(input())
     while totalM > 5:
-        
+        while totalM >= 25:
+            quarter = totalM // 25
+            totalM = totalM % 25
