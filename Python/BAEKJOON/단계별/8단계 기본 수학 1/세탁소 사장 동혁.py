4facac68927f831a676649142ac3b8214c46c4c5
tjrwls0826 <tjrwls0826@naver.com>
2023-06-27 05:18:27 +0900

    Update 세탁소 사장 동혁.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/세탁소 사장 동혁.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/세탁소 사장 동혁.py
@@ -2,9 +2,15 @@
 
 times = int(input())
 
+
 for - in range(times):
+    output = []
     totalM = int(input())
     while totalM > 5:
-        while totalM >= 25:
-            quarter = totalM // 25
-            totalM = totalM % 25
+        quarter = totalM // 25
+        totalM = totalM % 25
+        dime = totalM // 25
+        totalM = totalM % 10
+        nickel = totalM // 5
+        totalM = totalM % 5
+        
