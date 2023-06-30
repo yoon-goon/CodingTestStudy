d03e633eee0a71b6337eb8317fb9f3e578938a0c
tjrwls0826 <tjrwls0826@naver.com>
2023-06-30 18:47:55 +0900

    Update 중앙 이동 알고리즘.py

--- Python/BAEKJOON/단계별/8단계 기본 수학 1/중앙 이동 알고리즘.py
+++ Python/BAEKJOON/단계별/8단계 기본 수학 1/중앙 이동 알고리즘.py
@@ -1,5 +1,15 @@
 # https://www.acmicpc.net/problem/2903
 
 n = int(input())
+'''
+점의 갯수는 한변의 점의 갯수의^2
+횟수  변의 점갯수 
+1       2       
+2       3
+3       5
+4       9
+
+
+'''
 
 print((2 ** n + 1) ** 2)
