60d27eca15edc2af0f5225066ae7588cd7277f4d
tjrwls0826 <tjrwls0826@naver.com>
2023-07-02 04:00:14 +0900

    Update 직사각형에서 탈출.py

--- Python/BAEKJOON/단계별/기하 직사각형과 삼각형/직사각형에서 탈출.py
+++ Python/BAEKJOON/단계별/기하 직사각형과 삼각형/직사각형에서 탈출.py
@@ -1,13 +1,8 @@
 # https://www.acmicpc.net/problem/1085
-import sys
+    import sys
 
-x, y, w, h = map(int, sys.stdin.readline().split())
+    x, y, w, h = map(int, sys.stdin.readline().split())
+    horizon = w - x
+    vertical = h - y
 
-
-horizon = w - x
-vertical = h - y
-
-if horizon <= vertical:
-    print(horizon)
-else:
-    print(vertical)
+    print(min(x, y, vertical, horizon))
