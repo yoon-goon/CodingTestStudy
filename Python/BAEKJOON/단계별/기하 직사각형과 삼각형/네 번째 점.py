fc26e13116a63e764adeed3981533055b9780f82
tjrwls0826 <tjrwls0826@naver.com>
2023-07-04 21:12:47 +0900

    Update 네 번째 점.py

--- Python/BAEKJOON/단계별/기하 직사각형과 삼각형/네 번째 점.py
+++ Python/BAEKJOON/단계별/기하 직사각형과 삼각형/네 번째 점.py
@@ -5,3 +5,10 @@ import sys
 x1, y1 = map(int, sys.stdin.readline().split())
 x2, y2 = map(int, sys.stdin.readline().split())
 x3, y3 = map(int, sys.stdin.readline().split())
+
+if x1 == x2:
+    x = x3
+elif x1 == x3:
+    x = x2
+elif x2 == x3:
+    x = x1
