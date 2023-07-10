bbfc825ad9a07811fed228688d87402c19809916
tjrwls0826 <tjrwls0826@naver.com>
2023-07-10 14:45:02 +0900

    Update 삼각형과 세 변.py

--- Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
+++ Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
@@ -6,7 +6,9 @@ a, b, c = 1, 1, 1
 while a != 0 and b != 0 and c != 0:
     a, b, c = map(int, sys.stdin.readline().split())
 
-    if a == b and b == a and c == a:
+    if a == 0 and b == 0 and c == 0:
+        break
+    elif a == b and b == a and c == a:
         print("Equilateral")
     elif max(a, b, c) < a + b + c - max(a, b, c):
         aset = set()
