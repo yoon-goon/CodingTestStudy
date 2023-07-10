6b2394beccc83b6bca4e270a8ddf9ae1bf4c0c4d
tjrwls0826 <tjrwls0826@naver.com>
2023-07-10 14:46:18 +0900

    Update 삼각형과 세 변.py

--- Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
+++ Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
@@ -19,3 +19,17 @@ while a != 0 and b != 0 and c != 0:
             print("Scalene")
     else:
         print("Invalid")
+
+'''
+set를 이용한 풀이
+
+A, B, C = input().split()
+A, B, C = int(A), int(B), int(C)
+
+while A:
+    if(sum(L:=[A,B,C]))<=max(L)*2:print("Invalid")
+    else:print([0,"Equilateral","Isosceles","Scalene"][len(set(L))])
+    A, B, C = input().split()
+    A, B, C = int(A), int(B), int(C)
+    
+'''
