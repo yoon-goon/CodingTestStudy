eeffd146c3fcab849ac9f69c29b41fec3681036a
tjrwls0826 <tjrwls0826@naver.com>
2023-07-10 14:40:13 +0900

    Update 삼각형과 세 변.py

--- Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
+++ Python/BAEKJOON/단계별/기하 직사각형과 삼각형/삼각형과 세 변.py
@@ -7,3 +7,14 @@ while a == 0 and b == 0 and c == 0:
     b = int(input())
     c = int(input())
 
+    if a == b and b == a and c == a:
+        print("Equilateral")
+    elif a + b + c == 180:
+        aset = set()
+        aset.update([a, b, c])
+        if len(aset) == 2:
+            print("Isosceles")
+        else:
+            print("Scalene")
+    else:
+        print("Error")
