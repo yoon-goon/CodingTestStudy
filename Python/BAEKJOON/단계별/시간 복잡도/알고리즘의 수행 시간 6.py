35eb85eab9ded6126560d6150425d0360e14f17f
tjrwls0826 <tjrwls0826@naver.com>
2023-07-17 21:46:57 +0900

    Create 알고리즘의 수행 시간 6.py

---
+++ Python/BAEKJOON/단계별/시간 복잡도/알고리즘의 수행 시간 6.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/24267
+
+n = int(input())
+count = 0
+
+for i in range(n - 2):
+    j = i + 1
+    for j in range(n - 1):
+        k = j + 1
+        for k in range(n):
+            count += 1
+
+print(count, "\n3")
+
+print(n * n * n, "\n3")
