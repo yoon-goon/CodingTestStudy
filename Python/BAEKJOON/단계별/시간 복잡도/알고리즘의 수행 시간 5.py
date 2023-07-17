065fba24b549b09c630e903abce5181ac4b40bd3
tjrwls0826 <tjrwls0826@naver.com>
2023-07-17 21:43:09 +0900

    Update 알고리즘의 수행 시간 5.py

--- Python/BAEKJOON/단계별/시간 복잡도/알고리즘의 수행 시간 5.py
+++ Python/BAEKJOON/단계별/시간 복잡도/알고리즘의 수행 시간 5.py
@@ -1,6 +1,13 @@
 # https://www.acmicpc.net/problem/24266
 
 n = int(input())
+count = 0
 
 for i in range(n):
-    
+    for j in range(n):
+        for k in range(n):
+            count += 1
+
+print(count, "\n3")
+
+print(n * n * n, "\n3")
