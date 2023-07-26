63a4e264bb2301cbfb0ff02eca45ff6dc5dbe977
tjrwls0826 <tjrwls0826@naver.com>
2023-07-26 23:27:26 +0900

    Update 수학은 비대면강의입니다.py

--- Python/BAEKJOON/단계별/브루트 포스/수학은 비대면강의입니다.py
+++ Python/BAEKJOON/단계별/브루트 포스/수학은 비대면강의입니다.py
@@ -3,6 +3,7 @@ import sys
 
 a, b, c, d, e, f = map(int, sys.stdin.readline().split())
 
-for i in range(-999,999):
-    for y in range(-999,999):
-        if c =
+for i in range(-999, 999):
+    for y in range(-999, 999):
+        if c == a * i + b * y:
+            print(i, y)
