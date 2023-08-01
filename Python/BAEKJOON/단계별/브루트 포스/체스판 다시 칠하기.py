882046b22493003e3b00f3c80f7c15e3a3246187
tjrwls0826 <tjrwls0826@naver.com>
2023-08-01 22:13:09 +0900

    Update 체스판 다시 칠하기.py

--- Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
+++ Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
@@ -1,3 +1,12 @@
 # https://www.acmicpc.net/problem/1018
+'''
+N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수
+'''
 
-M, N = map(int, input())
+
+N, M = map(int, input())
+board = []
+result_list = []
+
+for _ in range(N):
+    board.append(input())
