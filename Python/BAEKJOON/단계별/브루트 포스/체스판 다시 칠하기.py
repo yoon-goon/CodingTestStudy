a402d82a52a077cab469c7434cc9a3660126c597
tjrwls0826 <tjrwls0826@naver.com>
2023-08-02 22:41:18 +0900

    Update 체스판 다시 칠하기.py

--- Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
+++ Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
@@ -12,6 +12,28 @@ for _ in range(N):
 
 for i in range(N - 7):  # 8*8크기의 체스판이므로 전체 보드의 인덱스를 벗어나지 않기위함
     for j in range(M - 7):
-        pass
+        a = 0  # W로 시작하는 경우 카운트
+        b = 0  # B로 시작하는 경우 카운트
 
-print(board)
+        for x in range(i, i + 8):
+            for y in range(j, j + 8):
+                if (x + y) % 2 == 0:
+                    '''
+                    행의 번호 x, 현재 열의 번호 y의 합이 짝수이면 시작점의 색깔과 같아야 하고,
+                    홀수이면 시작점의 색깔과 다른 색이어야 한다.
+                    '''
+                    if board[x][y] != 'B':
+                        a += 1
+                    if board[x][y] != 'W':
+                        b += 2
+                else:
+                    if board[x][y] != 'W':
+                        a += 1
+                    if board[x][y] != 'B':
+                        b += 2
+
+        result_list.append(a)
+        result_list.append(b)
+
+print(result_list)
+print(min(result_list))
