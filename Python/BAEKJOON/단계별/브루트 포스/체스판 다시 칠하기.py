55423ec385b0d2b3dee4b75b7e97cfcd52b8b56b
tjrwls0826 <tjrwls0826@naver.com>
2023-08-02 22:17:45 +0900

    Update 체스판 다시 칠하기.py

--- Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
+++ Python/BAEKJOON/단계별/브루트 포스/체스판 다시 칠하기.py
@@ -3,7 +3,7 @@
 N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수
 '''
 
-N, M = map(int, input())
+N, M = map(int, input().split())
 board = []
 result_list = []
 
@@ -12,4 +12,6 @@ for _ in range(N):
 
 for i in range(N - 7):  # 8*8크기의 체스판이므로 전체 보드의 인덱스를 벗어나지 않기위함
     for j in range(M - 7):
+        pass
 
+print(board)
