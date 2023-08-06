034fa88c014aa2f971116e8ea21fea5b0e8d3f02
tjrwls0826 <tjrwls0826@naver.com>
2023-08-06 21:56:09 +0900

    Update 영화감독 숌.py

--- Python/BAEKJOON/단계별/브루트 포스/영화감독 숌.py
+++ Python/BAEKJOON/단계별/브루트 포스/영화감독 숌.py
@@ -1,5 +1,6 @@
 # https://www.acmicpc.net/problem/1436
 
+'''
 n = int(input())
 list = []
 
@@ -10,3 +11,17 @@ for i in range(10000000):
         list.append(i)
 
 print(list[n-1])
+# 시간이 매우 오래걸림
+'''
+
+n = int(input())
+i = 666
+cnt = 0
+
+while 1:
+    if '666' in str(i): # '666'이 들어간 경우에만 걸리므로 667,668 다 건너뛰고 1666까지 가게됨
+        cnt += 1
+    if cnt == n:
+        print(i)
+        break
+    i += 1
