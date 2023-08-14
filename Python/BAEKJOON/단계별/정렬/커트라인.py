9d692d4497e323e921ab975c6550a3ba8011a5d4
tjrwls0826 <tjrwls0826@naver.com>
2023-08-15 00:09:27 +0900

    Update 커트라인.py

--- Python/BAEKJOON/단계별/정렬/커트라인.py
+++ Python/BAEKJOON/단계별/정렬/커트라인.py
@@ -4,4 +4,10 @@ import sys
 n, k = map(int, sys.stdin.readline().split())
 x = list(map(int, sys.stdin.readline().strip().split()))
 
-print(sorted(x, reverse=True))
+print(sorted(x, reverse=True)[k - 1])
+
+'''
+c.sort()
+print(c[-b])
+로 간단하게 구현 가능
+'''
