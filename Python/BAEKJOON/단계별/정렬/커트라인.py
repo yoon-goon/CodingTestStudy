c54ba946a74c7ba7744a928d4cbf9b5a65773b69
tjrwls0826 <tjrwls0826@naver.com>
2023-08-14 23:49:04 +0900

    Update 커트라인.py

--- Python/BAEKJOON/단계별/정렬/커트라인.py
+++ Python/BAEKJOON/단계별/정렬/커트라인.py
@@ -1,4 +1,7 @@
 # https://www.acmicpc.net/problem/25305
 import sys
 
-n, k = map(int(sys.stdin.readline().split()))
+n, k = map(int,sys.stdin.readline().split())
+x = list(sys.stdin.readline().strip().split())
+
+print(x)
