66a0834a73e12834a8db85823ddd98d5a4b6e641
tjrwls0826 <tjrwls0826@naver.com>
2023-08-18 00:26:47 +0900

    Update 수 정렬하기 3(카운팅정렬).py

--- Python/BAEKJOON/단계별/정렬/수 정렬하기 3(카운팅정렬).py
+++ Python/BAEKJOON/단계별/정렬/수 정렬하기 3(카운팅정렬).py
@@ -1,11 +1,12 @@
 # https://www.acmicpc.net/problem/10989 카운팅 정렬을 사용하는 문제
+import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 
 lst = [0] * 10001
 
 for _ in range(n):
-    a = int(input())
+    a = int(sys.stdin.readline())
     lst[a] += 1
 
 for i in range(10001):
