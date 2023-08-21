703bc65bbb0d8f99d5097449597bd2a9b6f5a987
tjrwls0826 <tjrwls0826@naver.com>
2023-08-21 23:51:10 +0900

    Update 좌표 정렬하기.py

--- Python/BAEKJOON/단계별/정렬/좌표 정렬하기.py
+++ Python/BAEKJOON/단계별/정렬/좌표 정렬하기.py
@@ -1,7 +1,12 @@
 # https://www.acmicpc.net/problem/11650
+import sys
 
 n = int(input())
 xlst = []
 ylst = []
 
 for _ in range(n):
+    x, y = map(int, sys.stdin.readline().strip())
+    xlst.append(x)
+    ylst.append(y)
+
