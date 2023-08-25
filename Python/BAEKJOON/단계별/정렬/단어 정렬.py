869c755bdd1b8bf425b7b90ffe60938ca2b0cd54
tjrwls0826 <tjrwls0826@naver.com>
2023-08-25 17:02:21 +0900

    Update 단어 정렬.py

--- Python/BAEKJOON/단계별/정렬/단어 정렬.py
+++ Python/BAEKJOON/단계별/정렬/단어 정렬.py
@@ -5,9 +5,14 @@ n = int(input())
 lst = []
 
 for _ in range(n):
-    x = sys.stdin.readline()
+    x = sys.stdin.readline().strip()
     if x in lst:
         pass
     else:
         lst.append(x)
 print(lst)
+
+lst.sort(key=len())
+
+for i in lst:
+    print(i)
