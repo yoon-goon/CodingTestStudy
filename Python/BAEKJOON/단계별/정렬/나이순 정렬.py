377a71871bb14c501be905a726eedc9566520ba0
tjrwls0826 <tjrwls0826@naver.com>
2023-08-28 20:11:13 +0900

    Update 나이순 정렬.py

--- Python/BAEKJOON/단계별/정렬/나이순 정렬.py
+++ Python/BAEKJOON/단계별/정렬/나이순 정렬.py
@@ -6,9 +6,17 @@ lst = []
 
 for i in range(n):
     x, y = sys.stdin.readline().strip().split()
-    lst.append([x, y])
+    if [x,y] in lst:
+        pass
+    else:
+        lst.append([x, y])
 
 lst.sort(key=lambda x: x[0])
 
 for i in lst:
     print(*i)
+
+'''
+동명이인 처리를 추가해야함
+시도 1 if [x,y] in lst
+'''
