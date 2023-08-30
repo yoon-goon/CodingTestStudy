55a0db9fb405183e7388c65b8294a16bb40ad61b
tjrwls0826 <tjrwls0826@naver.com>
2023-08-30 21:03:03 +0900

    Update 문자열 집합.py

--- Python/BAEKJOON/단계별/집합과 맵/문자열 집합.py
+++ Python/BAEKJOON/단계별/집합과 맵/문자열 집합.py
@@ -1,7 +1,10 @@
 # https://www.acmicpc.net/problem/14425
 import sys
 
-n, m = input().split()
-
+n, m = map(int,input().split())
 own = []
 
+for _ in range(n):
+    own.append(sys.stdin.readline().strip())
+
+print(own)
