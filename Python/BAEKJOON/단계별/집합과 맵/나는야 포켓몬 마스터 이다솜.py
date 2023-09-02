6cf773a8e551c64793f20c7e480d78d5cfdc1d21
tjrwls0826 <tjrwls0826@naver.com>
2023-09-02 23:53:37 +0900

    Update 나는야 포켓몬 마스터 이다솜.py

--- Python/BAEKJOON/단계별/집합과 맵/나는야 포켓몬 마스터 이다솜.py
+++ Python/BAEKJOON/단계별/집합과 맵/나는야 포켓몬 마스터 이다솜.py
@@ -2,6 +2,11 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().strip().split())
+pokename = dict()
+pokenum = dict()
+
+''' # 시간초과 발생 index는 O(N)이 걸리는 아주 느린 함수
+n, m = map(int, sys.stdin.readline().strip().split())
 poke = []
 
 for _ in range(n):
@@ -12,4 +17,5 @@ for y in range(m):
     try:
         print(poke[int(x) - 1])
     except:
-        print(poke.index(x) + 1)  # 시간초과 발생 index는 O(N)이 걸리는 아주 느린 함수
+        print(poke.index(x) + 1)  
+'''
