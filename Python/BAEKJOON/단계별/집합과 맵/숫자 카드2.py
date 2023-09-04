b4d1a650deec74fba2376377c584f78ff2925b6b
tjrwls0826 <tjrwls0826@naver.com>
2023-09-04 23:04:54 +0900

    Update 숫자 카드2.py

--- Python/BAEKJOON/단계별/집합과 맵/숫자 카드2.py
+++ Python/BAEKJOON/단계별/집합과 맵/숫자 카드2.py
@@ -3,10 +3,12 @@ import sys
 
 input()
 own = set(map(int, sys.stdin.readline().split()))  # 시간초과로 인해 set로 변경
-m = input()
+m = int(input())
 compare = list(map(int, sys.stdin.readline().split()))
 result = [0] * m
 
 for i in range(m):
     if compare[i] in own:
-        pass
+        result[i] += 1
+
+print(result)
