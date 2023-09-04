c4c207c0d4cbdcfd62c3fad41172519fba25214c
tjrwls0826 <tjrwls0826@naver.com>
2023-09-04 23:10:56 +0900

    Update 숫자 카드2.py

--- Python/BAEKJOON/단계별/집합과 맵/숫자 카드2.py
+++ Python/BAEKJOON/단계별/집합과 맵/숫자 카드2.py
@@ -1,14 +1,14 @@
 # https://www.acmicpc.net/problem/10816
 import sys
 
-input()
-own = set(map(int, sys.stdin.readline().split()))  # 시간초과로 인해 set로 변경
+n = int(input())
+own = list(map(int, sys.stdin.readline().split()))
 m = int(input())
 compare = list(map(int, sys.stdin.readline().split()))
 result = [0] * m
 
-for i in range(m):
-    if compare[i] in own:
-        result[i] += 1
+for i in own:
+    if i in compare:
+        result[compare.index(i)] += 1
 
-print(result)
+print(*result)
