e3ff2f22a67f3b2b70721bd74c8418641084ad24
tjrwls0826 <tjrwls0826@naver.com>
2023-09-06 23:30:06 +0900

    Update 듣보잡.py

--- Python/BAEKJOON/단계별/집합과 맵/듣보잡.py
+++ Python/BAEKJOON/단계별/집합과 맵/듣보잡.py
@@ -11,9 +11,11 @@ for _ in range(int(n)):
 for _ in range(int(m)):
     nosee.append(sys.stdin.readline().strip())
 
+''' # 시간초과 발생
 for i in nohear:
     if i in nosee:
         ans.append(i)
+'''
 
 print(len(ans))
 for i in sorted(ans):
