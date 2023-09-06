0e1c4c8ca01666572dcbd9e1146d5b38680216b5
tjrwls0826 <tjrwls0826@naver.com>
2023-09-06 23:26:12 +0900

    Update 듣보잡.py

--- Python/BAEKJOON/단계별/집합과 맵/듣보잡.py
+++ Python/BAEKJOON/단계별/집합과 맵/듣보잡.py
@@ -4,9 +4,16 @@ import sys
 n, m = input().strip().split()
 nohear = []
 nosee = []
+ans = []
 
-for _ in range(n):
+for _ in range(int(n)):
     nohear.append(sys.stdin.readline().strip())
 
-for _ in range(m):
+for _ in range(int(m)):
+    nosee.append(sys.stdin.readline().strip())
 
+print(nohear,nosee)
+
+for i in nohear:
+    if i in nosee:
+        print()
