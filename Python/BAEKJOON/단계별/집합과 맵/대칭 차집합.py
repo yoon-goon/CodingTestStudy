ab0a4e60c76e6bfd29e6739d8f91c17f54919ace
tjrwls0826 <tjrwls0826@naver.com>
2023-09-08 20:43:31 +0900

    Update 대칭 차집합.py

--- Python/BAEKJOON/단계별/집합과 맵/대칭 차집합.py
+++ Python/BAEKJOON/단계별/집합과 맵/대칭 차집합.py
@@ -1,3 +1,9 @@
 # https://www.acmicpc.net/problem/1269
+import sys
 
-aNum, bNum = map(int,input().strip().split())
+aNum, bNum = map(int, input().strip().split())
+
+a = list(sys.stdin.readline().split())
+print(a)
+b = list(sys.stdin.readline().split())
+print(b)
