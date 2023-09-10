afe90e82a9ca2c90b938fa689c8e5b9aa6748857
tjrwls0826 <tjrwls0826@naver.com>
2023-09-10 22:49:15 +0900

    Update 최소공배수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/최소공배수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/최소공배수.py
@@ -4,9 +4,10 @@ case = int(input())
 
 for _ in range(case):
     a, b = map(int, input().strip().split())
-    for i in range(min(a, b), 0, -1):  # 최소공약수
+    for i in range(min(a, b), 0, -1):  # 최대공약수
         if (a % i == 0) and (b % i == 0):
-            print(i)
+            lcm = a * b // i # 최대공약수를 이용해 최소공배수 구하기
+            print(lcm)
             break
 
 ''' #시간초과
