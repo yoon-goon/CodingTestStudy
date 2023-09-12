dffbbb57ffe2f724e03828fa228d590d375da699
tjrwls0826 <tjrwls0826@naver.com>
2023-09-12 21:34:03 +0900

    Update 분수 합.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/분수 합.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/분수 합.py
@@ -1,6 +1,10 @@
 # https://www.acmicpc.net/problem/1735
 import sys
 
-a1,b1 = map(int,sys.stdin.readline().split())
-a2,b2 = map(int,sys.stdin.readline().split())
+a1, b1 = map(int, sys.stdin.readline().split())
+a2, b2 = map(int, sys.stdin.readline().split())
 
+upper = a1 * b2 + a2 * b1
+lower = b1 * b2
+
+print(upper, lower)
