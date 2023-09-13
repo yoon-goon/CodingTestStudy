a3bb94a5508dbf277fbe4463f857fb33b0a30bd1
tjrwls0826 <tjrwls0826@naver.com>
2023-09-14 00:24:27 +0900

    Update 가로수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
@@ -4,7 +4,7 @@ import math
 
 n = int(sys.stdin.readline())
 a = int(sys.stdin.readline())
-lst = [a]
+lst = [a] # 전체 리스트
 distance = []  # 간격
 for _ in range(n - 1):
     b = int(sys.stdin.readline())
@@ -12,7 +12,9 @@ for _ in range(n - 1):
     distance.append(b - a)
     a = b
 
-gcd = math.gcd(*distance)
+gcd = math.gcd(*distance) # 간격간 최대공약수
 
+print(max(lst),min(lst),gcd,n)
 to_add = ((max(lst) - min(lst)) // gcd + 1 - n)
+# 최대값 - 최소값을 공약수로 나눈 후 n(초기에 입력받은 가로수의 수) 만큼 빼줘야함
 print(to_add)
