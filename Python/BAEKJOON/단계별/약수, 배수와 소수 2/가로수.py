89ad35715b559e81fdaeb181cb9076b55a408e2c
tjrwls0826 <tjrwls0826@naver.com>
2023-09-14 00:16:41 +0900

    Update 가로수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
@@ -3,7 +3,6 @@ import sys
 import math
 
 n = int(sys.stdin.readline())
-
 a = int(sys.stdin.readline())
 lst = [a]
 distance = []  # 간격
@@ -13,11 +12,7 @@ for _ in range(n - 1):
     distance.append(b - a)
     a = b
 
-print(lst)
-print(distance)
-
 gcd = math.gcd(*distance)
-print(gcd)
 
 to_add = ((max(lst) - min(lst)) // gcd + 1 - n)
 print(to_add)
