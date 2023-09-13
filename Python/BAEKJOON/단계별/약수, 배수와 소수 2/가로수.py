ab49bc1dbd03ca8e62832ddc3e36002332e29732
tjrwls0826 <tjrwls0826@naver.com>
2023-09-13 23:47:20 +0900

    Update 가로수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
@@ -4,9 +4,14 @@ import sys
 n = int(sys.stdin.readline())
 lst = []
 a = int(sys.stdin.readline())
+st = set()
+st.add(a)
 for _ in range(n-1):
     b = int(sys.stdin.readline())
+    st.add(b)
     lst.append(b-a)
     a = b
 
-print(lst)
+
+print(st)
+print(min(lst))
