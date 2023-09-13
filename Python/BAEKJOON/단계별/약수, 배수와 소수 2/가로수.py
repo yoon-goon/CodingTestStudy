841d8ad42082f363e09f98d9547c18339d960cb1
tjrwls0826 <tjrwls0826@naver.com>
2023-09-14 00:05:13 +0900

    Update 가로수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/가로수.py
@@ -1,27 +1,23 @@
 # https://www.acmicpc.net/problem/2485
 import sys
+import math
 
 n = int(sys.stdin.readline())
-lst = []
-a = int(sys.stdin.readline())
-st = set()
-final = set()
-st.add(a)
+
+a =int(sys.stdin.readline())
+lst = [a]
+distance = []
 for _ in range(n-1):
     b = int(sys.stdin.readline())
-    st.add(b)
-    lst.append(b-a)
+    lst.append(b)
+    distance.append(b-a)
     a = b
 
+print(lst)
+print(distance)
 
-print(st)
-print(min(lst))
+gcd = math.gcd(*distance)
+print(gcd)
 
-i = min(st)
-final.add(i)
-while i < max(st):
-    i = i+min(lst)
-    final.add(i)
-
-print(final)
-print(len(final)-len(st))
+to_add = (max(distance)-min(distance)//gcd -1)
+print(to_add)
