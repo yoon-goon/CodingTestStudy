985a49b692f592755034ce96fddd4bc736d4342d
tjrwls0826 <tjrwls0826@naver.com>
2023-09-16 23:40:59 +0900

    Update 다음 소수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/다음 소수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/다음 소수.py
@@ -1,20 +1,17 @@
 # https://www.acmicpc.net/problem/4134
 import sys
 
+
+def prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
 n = int(sys.stdin.readline())
 
 for _ in range(n):
     a = int(sys.stdin.readline())
-    b = 0
-    if a == 1 or a == 0:
-        pass
-    else:
-        while b == 0:
-            for i in range(2, int(a ** 0.5) + 1):
-                if a % i == 0:
-                    a += 1
-                    pass
-                else:
-                    print(a)
-                    b = 1
-                    break
