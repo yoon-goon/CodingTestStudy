5d1fdb0b97eb0e86789bd66612b194cb5e4642e3
tjrwls0826 <tjrwls0826@naver.com>
2023-09-16 21:44:07 +0900

    Update 다음 소수.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/다음 소수.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/다음 소수.py
@@ -5,5 +5,8 @@ n = int(sys.stdin.readline())
 
 for _ in range(n):
     a = int(sys.stdin.readline())
-    while True:
-        
+    while a != 0 and a != 1:
+        for i in range(2, a ** 0.5 + 1): #루트
+            if a%i == 0:
+                print(a)
+                break
