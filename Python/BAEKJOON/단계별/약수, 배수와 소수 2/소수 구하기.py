66642d55a9d7adb451c91bbab88c627011f5e574
tjrwls0826 <tjrwls0826@naver.com>
2023-09-17 23:28:29 +0900

    Update 소수 구하기.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/소수 구하기.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/소수 구하기.py
@@ -1,4 +1,18 @@
 # https://www.acmicpc.net/problem/1929
 import sys
 
-m, n = map(int, sys.stdin.readline().split())
+
+def prime(x):
+    if x < 2:  # 만약 2보다 작으면 소수가 아니므로 False 반환
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+n, m = map(int, sys.stdin.readline().split())
+
+for i in range(n, m + 1):
+    if prime(i):
+        print(i)
