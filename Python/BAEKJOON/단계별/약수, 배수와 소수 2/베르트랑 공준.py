70d2779679ce6d51a10378fdc6e8065dc39feab3
tjrwls0826 <tjrwls0826@naver.com>
2023-09-18 21:06:21 +0900

    Update 베르트랑 공준.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/베르트랑 공준.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/베르트랑 공준.py
@@ -1,6 +1,19 @@
 # https://www.acmicpc.net/problem/4948
 import sys
 
-n = int(sys.stdin.readline())
+def prime(x):
+    if x < 2:  # 만약 2보다 작으면 소수가 아니므로 False 반환
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+n = int(sys.stdin.readline())  # n<x<=2
 while n != 0:
+    num = 0
+    for i in range(n + 1, 2*n+1):
+        if prime(i):
+            num += 1
+    print(num)
     n = int(sys.stdin.readline())
