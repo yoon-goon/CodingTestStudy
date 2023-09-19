05429d108addf3e83f3d024074fba52140e46356
tjrwls0826 <tjrwls0826@naver.com>
2023-09-19 23:50:49 +0900

    Update 골드바흐 파티션.py

--- Python/BAEKJOON/단계별/약수, 배수와 소수 2/골드바흐 파티션.py
+++ Python/BAEKJOON/단계별/약수, 배수와 소수 2/골드바흐 파티션.py
@@ -3,6 +3,10 @@ import sys
 
 input = sys.stdin.readline()
 
+lst = [False, False] + [True] * 999999
+# 리스트에 0,1번에는 False, 2번부터는 True 대입.
+# Index번호는 숫자를 의미하고, Index의 값은 소수인지 아닌지를 판별함.
+
 T = int(input())
 
 for i in range(T):
