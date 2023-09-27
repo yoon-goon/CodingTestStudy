9230d9496ef8667b247740cf7636abff4a6ebe73
tjrwls0826 <tjrwls0826@naver.com>
2023-09-27 14:26:30 +0900

    Update 팩토리얼 0의 개수.py

--- Python/BAEKJOON/등급별/실버/팩토리얼 0의 개수.py
+++ Python/BAEKJOON/등급별/실버/팩토리얼 0의 개수.py
@@ -3,5 +3,16 @@
 def fac(n):
     if n == 0 or n == 1:
         return 1
-    else:
-        
+    return n * fac(n - 1)
+
+
+n = int(input())
+num = fac(n)
+print(num)
+ans = 0
+
+for i in range(len(num)):
+    if num[i] == 0:
+        ans += 1
+
+print(ans)
