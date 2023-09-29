f00c2c531a7ee546b9d91e5f2f0faa3812f312a2
tjrwls0826 <tjrwls0826@naver.com>
2023-09-29 23:45:02 +0900

    Update 제로.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/제로.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/제로.py
@@ -1,6 +1,12 @@
 # https://www.acmicpc.net/problem/10773
+import sys
 
 k = int(input())
 
 stack = []
-for i in range(k):
+for _ in range(k):
+    a = sys.stdin.readline()
+    if a != 0:
+        stack.append()
+    else:
+        stack.pop()
