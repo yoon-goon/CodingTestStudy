107599caef971313605d755626de6df7a01a1f7b
tjrwls0826 <tjrwls0826@naver.com>
2023-09-30 22:08:12 +0900

    Update 스택 2.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/스택 2.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/스택 2.py
@@ -2,11 +2,10 @@
 import sys
 
 n = int(input())
-
 stack = []
 
 for i in range(n):
-    a = list(map(int, sys.stdin.readline()))
+    a = list(map(int, sys.stdin.readline().strip().split()))
     if a[0] == 1:
         stack.append(a[1])
     elif a[0] == 2:
@@ -17,7 +16,7 @@ for i in range(n):
     elif a[0] == 3:
         print(len(stack))
     elif a[0] == 4:
-        if len(a) == 0:
+        if len(stack) == 0:
             print(1)
         else:
             print(0)
