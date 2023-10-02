ab54fb57961b40c2a9c90cddfea4c17688b972c5
tjrwls0826 <tjrwls0826@naver.com>
2023-10-02 21:58:33 +0900

    Update 요세푸스 문제 0.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/요세푸스 문제 0.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/요세푸스 문제 0.py
@@ -2,9 +2,15 @@
 
 n, k = map(int, input().split())
 
-stack = []
+idx = 0
+stack = [i for i in range(1, n + 1)]
+ans = []
 
-for i in range(1,n+1):
-    stack.append(i)
+while stack:
+    idx += k - 1  # k-1번째 인덱스까지 건너뛰기
+    if idx >= len(stack):
+        idx %= len(stack)
+    ans.append(str(stack.pop(idx)))
 
 print(stack)
+print(ans)
