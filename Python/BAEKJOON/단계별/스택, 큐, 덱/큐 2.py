adf40f1b0645d86bdd0df78ac9777f1cadd17cca
tjrwls0826 <tjrwls0826@naver.com>
2023-10-06 23:31:47 +0900

    Update 큐 2.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/큐 2.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/큐 2.py
@@ -1,9 +1,19 @@
 # https://www.acmicpc.net/problem/18258
 import sys
+from collections import deque
 
-input = sys.stdin.readline
 
 n = int(input())
+que = deque()
 
 for _ in range(n):
-    
+    x = list(sys.stdin.readline().split())
+    if x[0] == 'push':
+        que.append(x[1])
+        print(que[-1])
+    elif x[0] == 'pop':
+        if que == False:
+            print(-1)
+        else:
+            print(que.pop())
+    elif x[0] == 'size':
