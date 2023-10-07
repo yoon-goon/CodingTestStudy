cc3fbcbee57b99a476f8960c279135592b272463
tjrwls0826 <tjrwls0826@naver.com>
2023-10-08 00:02:07 +0900

    Update 도키도키 간식드리미.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/도키도키 간식드리미.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/도키도키 간식드리미.py
@@ -1,9 +1,20 @@
 # https://www.acmicpc.net/problem/12789
 import sys
+from collections import deque
 
 n = int(input())
-
-
 x = list(map(int,sys.stdin.readline().split()))
-
+a = 1
+que = deque()
+line = []
 print(x)
+
+for i in x:
+    if i == a:
+        line.append(i)
+        a += 1
+    else:
+        que.append(i)
+
+print(line)
+print(que)
