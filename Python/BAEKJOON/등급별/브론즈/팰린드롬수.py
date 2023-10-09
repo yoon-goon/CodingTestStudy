a7e0a70aaa54bc224c5e21fb158ef36c9e8c9a98
tjrwls0826 <tjrwls0826@naver.com>
2023-10-09 23:07:36 +0900

    Update 팰린드롬수.py

--- Python/BAEKJOON/등급별/브론즈/팰린드롬수.py
+++ Python/BAEKJOON/등급별/브론즈/팰린드롬수.py
@@ -1,6 +1,10 @@
 # https://www.acmicpc.net/problem/1259
 
 a = input()
-while a != 0:
-    b = len(a)/2
-    print(b)
+
+while a != '0':
+    if a == a[::-1]:
+        print("yes")
+    else:
+        print("no")
+    a = input()
