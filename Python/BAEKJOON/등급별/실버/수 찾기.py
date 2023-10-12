90f21988d39a3361357c83baee0047ea5e28cd1b
tjrwls0826 <tjrwls0826@naver.com>
2023-10-12 22:22:40 +0900

    Update 수 찾기.py

--- Python/BAEKJOON/등급별/실버/수 찾기.py
+++ Python/BAEKJOON/등급별/실버/수 찾기.py
@@ -1,8 +1,12 @@
 # https://www.acmicpc.net/problem/1920
 
 n = int(input())
-
-lst = list(input().split())
-print(lst)
-
+lst = set(input().split()) # 시간 절약을 위해 set로
 m = int(input())
+lst2 = list(input().split())
+
+for i in lst2:
+    if i in lst:
+        print(1)
+    else:
+        print(0)
