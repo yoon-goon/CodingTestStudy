35f3ef087cb67d3e1fc83153f0e7e044acf150ea
tjrwls0826 <tjrwls0826@naver.com>
2023-10-15 22:53:43 +0900

    Update 최대공약수와 최소공배수.py

--- Python/BAEKJOON/등급별/브론즈/최대공약수와 최소공배수.py
+++ Python/BAEKJOON/등급별/브론즈/최대공약수와 최소공배수.py
@@ -1,18 +1,20 @@
 # https://www.acmicpc.net/problem/2609
 
-a,b = list(map(int,input().split()))
+a, b = list(map(int, input().split()))
 
-def GCD(x,y): # 최소공배수(Greatest Common Divisor)
 
-    while(y):
-        x,y = y,x%y
+def GCD(x, y):  # 최소공배수(Greatest Common Divisor)
+    while (y):
+        z = y # 값 저장
+        y = x % y
+        x = z
     return x
 
 
-def LCM(x,y): # 최대공약수(Least Common Multiple)
-    result = (x*y)//GCD(x,y)
+def LCM(x, y):  # 최대공약수(Least Common Multiple)
+    result = (x * y) // GCD(x, y)
     return result
 
 
-print(GCD(a,b))
-print(LCM(a,b))
+print(GCD(a, b))
+print(LCM(a, b))
