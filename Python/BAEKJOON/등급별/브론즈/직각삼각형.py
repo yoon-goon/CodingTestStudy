c1188e0f829d62ced2f4d43189cd0650407f0c79
tjrwls0826 <tjrwls0826@naver.com>
2023-10-15 23:01:44 +0900

    Update 직각삼각형.py

--- Python/BAEKJOON/등급별/브론즈/직각삼각형.py
+++ Python/BAEKJOON/등급별/브론즈/직각삼각형.py
@@ -4,5 +4,8 @@ import sys
 a, b, c = map(int, sys.stdin.readline().split())
 
 while a != 0 or b != 0 or c != 0:
+    if a * a + b * b == c * c or a * a + c * c == b * b or b * b + c * c == a * a:
+        print('right')
+    else:
+        print('wrong')
     a, b, c = map(int, sys.stdin.readline().split())
-    
