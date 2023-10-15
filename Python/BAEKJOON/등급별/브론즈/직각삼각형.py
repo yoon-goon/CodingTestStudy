c9e1562e839c7d365057dd416220aa827d9aa428
tjrwls0826 <tjrwls0826@naver.com>
2023-10-15 22:59:23 +0900

    Create 직각삼각형.py

---
+++ Python/BAEKJOON/등급별/브론즈/직각삼각형.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/4153
+import sys
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+while a != 0 or b != 0 or c != 0:
+    a, b, c = map(int, sys.stdin.readline().split())
+    
