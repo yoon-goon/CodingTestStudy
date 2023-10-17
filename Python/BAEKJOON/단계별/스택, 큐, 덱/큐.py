a6d89c88a163175f4e7484594f19ffa5028e97ca
tjrwls0826 <tjrwls0826@naver.com>
2023-10-17 23:10:30 +0900

    Create 큐.py

---
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/큐.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/10845
+
+# 스택과 큐의 주요 차이점은 요소에 액세스하는 순서
+# 스택은 LIFO (LAST in first out) 순서를 따르므로 스택에 마지막으로 추가된 요소가 가장 먼저 제거
+# 반면에 큐는 (First In First Out) 순서
+
+import sys
+
+n = int(input())
+
+que = []
+
+for _ in range(n):
+    a = list(sys.stdin.readline().split())
+
