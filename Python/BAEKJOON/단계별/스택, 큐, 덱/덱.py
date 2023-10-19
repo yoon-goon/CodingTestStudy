42396a6dc66342229141241d7401744bf59882bd
tjrwls0826 <tjrwls0826@naver.com>
2023-10-19 23:19:38 +0900

    Update 덱.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/덱.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/덱.py
@@ -9,6 +9,16 @@ deck = []
 for _ in range(n):
     a = list(sys.stdin.readline().split())
     if a[0] == 'push_front':
-        deck.insert(0,a[1])
+        deck.insert(0, a[1])
     elif a[0] == 'push_back':
         deck.append(a[1])
+    elif a[0] == 'pop_front':
+        if deck:
+            print(deck.pop(0))
+        else:
+            print(-1)
+    elif a[0] == 'pop_back':
+        if deck:
+            print(deck.pop(-1))
+        else:
+            print(-1)
