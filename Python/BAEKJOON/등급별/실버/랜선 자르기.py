3e1c474f402c3b589c5df8539c59f72ce425db3f
tjrwls0826 <tjrwls0826@naver.com>
2023-10-21 22:26:11 +0900

    Update 랜선 자르기.py

--- Python/BAEKJOON/등급별/실버/랜선 자르기.py
+++ Python/BAEKJOON/등급별/실버/랜선 자르기.py
@@ -1,30 +1,30 @@
 # https://www.acmicpc.net/problem/1654
 import sys
 
-n, k = map(int, sys.stdin.readline().split())
+k, n = map(int, sys.stdin.readline().split())
 
 lst = []
 
-for i in range(n):
+for i in range(k):
     lst.append(int(sys.stdin.readline()))
 
-#이진탐색을 위한 세팅
+# 이진탐색을 위한 세팅
 start = 1
 end = max(lst)
 
 while start <= end:
-    mid = (start+end) // 2
-    print("mid",mid)
-    lines = 0
+    lines = 0  # 만들어진 줄의 갯수
+    mid = (start + end) // 2
+    print("mid", mid)
     for y in lst:
-        lines += (y//mid)
+        lines += (y // mid)  # 각각줄의 길이를 중간값으로 절단
 
-    if lines >= k: #
-        start = mid + 1
+    if lines >= n:  # 만들어진 줄의 갯수가 필요한 갯수보다 많은 경우
+        start = mid + 1  # 중간값을 늘려서 최대길이를 늘림
         print(start)
         print(mid)
-    else:
-        end = mid - 1
+    else:  # 만들어진 줄의 갯수가 필요한 갯수보다 적은 경우
+        end = mid - 1  # 중간값을 줄여서 길이를 줄임
         print(end)
         print(mid)
 
