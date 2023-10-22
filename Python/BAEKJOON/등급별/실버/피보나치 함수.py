312ff376999100ad37a80822bfa5a136ddc58bbf
tjrwls0826 <tjrwls0826@naver.com>
2023-10-23 02:45:44 +0900

    Update 피보나치 함수.py

--- Python/BAEKJOON/등급별/실버/피보나치 함수.py
+++ Python/BAEKJOON/등급별/실버/피보나치 함수.py
@@ -25,4 +25,12 @@ for _ in range(t):
     n = int(sys.stdin.readline())
     zero = [1,0]
     one = [0,1]
+    for i in range(2,n+1):
+        zero.append(zero[i-2]+zero[i-1])
+        one.append(one[i-2]+one[i-1])
+
+    # print(zero)
+    # print(one)
+
+    print(zero[n], one[n])
 
