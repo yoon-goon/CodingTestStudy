dcf960e45b4c4677eb57787791d2d9842f88ae72
tjrwls0826 <tjrwls0826@naver.com>
2023-10-23 22:30:49 +0900

    Update ATM.py

--- Python/BAEKJOON/단계별/정렬/ATM.py
+++ Python/BAEKJOON/단계별/정렬/ATM.py
@@ -4,11 +4,11 @@ import sys
 n = int(input())
 p = list(map(int,sys.stdin.readline().split()))
 p.sort()
-print(p)
+# print(p)
 ans = []
-for i in range(len(p)+1):
-    for y in p[:i]:
-        print(y)
+for i in range(len(p)):
+    for y in p[:i+1]:
+        # print(y)
         ans.append(y)
 
 print(sum(ans))
