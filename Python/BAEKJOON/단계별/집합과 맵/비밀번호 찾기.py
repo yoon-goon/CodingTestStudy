9cc483d51ed5bdecc3f7978e4aa38f69008dec53
tjrwls0826 <tjrwls0826@naver.com>
2023-10-24 19:57:46 +0900

    Update 비밀번호 찾기.py

--- Python/BAEKJOON/단계별/집합과 맵/비밀번호 찾기.py
+++ Python/BAEKJOON/단계별/집합과 맵/비밀번호 찾기.py
@@ -2,8 +2,14 @@
 import sys
 
 n, m = map(int, input().split())
-password = []
+password = dict()
 
 for i in range(n):
     a1, a2 = sys.stdin.readline().split()
-    
+    password[a1] = a2
+
+
+for _ in range(m):
+    b1 = sys.stdin.readline().strip()
+    print(password[b1])
+
