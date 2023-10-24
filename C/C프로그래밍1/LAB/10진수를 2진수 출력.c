80e64ededa59f554a13765404f5eddceb8fc01fd
tjrwls0826 <tjrwls0826@naver.com>
2023-10-25 06:08:01 +0900

    Create 10진수를 2진수 출력.c

---
+++ C/C프로그래밍1/LAB/10진수를 2진수 출력.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int num;
+    printf("십진수:");
+    scanf("%u",&num);
+
+    unsigned int mask = 1 << 7; // mask : 10000000
+    printf("이진수:");
+
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0)? printf("0"):printf("1");
+    mask = mask >> 1;
+    
+    return 0;
+
+}
