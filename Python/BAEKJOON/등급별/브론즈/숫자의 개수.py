a782e8e5ad7a5a42384714cf54c530408f25965c
tjrwls0826 <tjrwls0826@naver.com>
2023-10-26 01:36:20 +0900

    Update 숫자의 개수.py

--- Python/BAEKJOON/등급별/브론즈/숫자의 개수.py
+++ Python/BAEKJOON/등급별/브론즈/숫자의 개수.py
@@ -8,3 +8,26 @@ num = a*b*c
 
 lst = [0,0,0,0,0,0,0,0,0,0]
 
+for i in str(num):
+    if i == '0':
+        lst[0] += 1
+    elif i == '1':
+        lst[1] += 1
+    elif i == '2':
+        lst[2] += 1
+    elif i == '3':
+        lst[3] += 1
+    elif i == '4':
+        lst[4] += 1
+    elif i == '5':
+        lst[5] += 1
+    elif i == '6':
+        lst[6] += 1
+    elif i == '7':
+        lst[7] += 1
+    elif i == '8':
+        lst[8] += 1
+    else:
+        lst[9] += 1
+
+print(*lst,sep="\n")
