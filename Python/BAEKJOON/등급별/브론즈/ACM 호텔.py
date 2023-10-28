7cc7b3da397ec1ccb5b8ae80e45028410975c13c
tjrwls0826 <tjrwls0826@naver.com>
2023-10-29 05:17:53 +0900

    Update ACM 호텔.py

--- Python/BAEKJOON/등급별/브론즈/ACM 호텔.py
+++ Python/BAEKJOON/등급별/브론즈/ACM 호텔.py
@@ -7,6 +7,10 @@ for _ in range(t):
     h, w, n = map(int, sys.stdin.readline().split())
 
     f = n % h
-    room = (n // h) + 1
+    if f != 0:
+        room = (n // h) + 1
+    else:  # 딱 나눠떨어지면 최대층수를 사용하면 됨
+        f = h
+        room = (n // h)
 
-    print(f*100+room)
+    print(f * 100 + room)
