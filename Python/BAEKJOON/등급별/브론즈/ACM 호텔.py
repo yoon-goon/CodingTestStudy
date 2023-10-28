aee56c28fdcf68d79af2a34dd8c2026a5eaccd84
tjrwls0826 <tjrwls0826@naver.com>
2023-10-29 05:13:00 +0900

    Update ACM 호텔.py

--- Python/BAEKJOON/등급별/브론즈/ACM 호텔.py
+++ Python/BAEKJOON/등급별/브론즈/ACM 호텔.py
@@ -4,5 +4,9 @@ import sys
 t = int(input())
 
 for _ in range(t):
-    h,w,n = map(int,sys.stdin.readline().split())
-    print(h,w,n)
+    h, w, n = map(int, sys.stdin.readline().split())
+
+    f = n % h
+    room = (n // h) + 1
+
+    print(f*100+room)
