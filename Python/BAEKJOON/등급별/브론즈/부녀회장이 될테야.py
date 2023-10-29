f4552300c5ccb03125af20e9a169750735bd504e
tjrwls0826 <tjrwls0826@naver.com>
2023-10-30 00:45:40 +0900

    Update 부녀회장이 될테야.py

--- Python/BAEKJOON/등급별/브론즈/부녀회장이 될테야.py
+++ Python/BAEKJOON/등급별/브론즈/부녀회장이 될테야.py
@@ -3,7 +3,15 @@ import sys
 
 t = int(input())
 
-
 for _ in range(t):
     k = int(sys.stdin.readline())
     n = int(sys.stdin.readline())
+
+    #  0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다
+    lst = [i for i in range(1, n + 1)] # 1,2,3,4 ...
+    # print(lst)
+    for y in range(k):
+        for j in range(1,n): # 1층 1호면 1명, 1층 2호면 1+2, 1층 3호면 1+2+3
+            lst[j] += lst[j-1]
+
+    print(lst)
