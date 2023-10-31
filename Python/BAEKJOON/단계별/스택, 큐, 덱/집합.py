19717239264e86ddf2b67d3b40c7b2f8af6c7844
tjrwls0826 <tjrwls0826@naver.com>
2023-10-31 23:43:49 +0900

    Update 집합.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/집합.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/집합.py
@@ -6,6 +6,10 @@ s = set()
 
 for _ in range(m):
     a = list(sys.stdin.readline().split())
+
+    if len(a) == 2:  # all 명령 시 생성되는 range값들이 정수인 것을 고려, 입력 값을 int로 변환시켜 준다.
+        a[1] = int(a[1])  # 안바꾸면 값이 틀리게나옴
+
     if a[0] == 'add':
         s.add(a[1])
     elif a[0] == 'remove':
@@ -22,4 +26,6 @@ for _ in range(m):
         else:
             s.add(a[1])
     elif a[0] == 'all':
-        s = set(range(1,21))
+        s = set(range(1, 21))
+    elif a[0] == 'empty':
+        s = set()
