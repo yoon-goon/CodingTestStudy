265406a98b45ca15a90865f6b79af80a588c6215
tjrwls0826 <tjrwls0826@naver.com>
2023-10-31 23:35:37 +0900

    Update 집합.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱/집합.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱/집합.py
@@ -7,7 +7,19 @@ s = set()
 for _ in range(m):
     a = list(sys.stdin.readline().split())
     if a[0] == 'add':
-        s.append(a[1])
+        s.add(a[1])
     elif a[0] == 'remove':
         if a[1] in s:
-            s.remove(s[1])
+            s.remove(a[1])
+    elif a[0] == 'check':
+        if a[1] in s:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == 'toggle':
+        if a[1] in s:
+            s.remove(a[1])
+        else:
+            s.add(a[1])
+    elif a[0] == 'all':
+        s = set(range(1,21))
