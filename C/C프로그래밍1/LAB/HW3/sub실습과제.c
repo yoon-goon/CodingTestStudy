1c3a4c395a8a80ac30d5668b763b3f9be5214379
tjrwls0826 <tjrwls0826@naver.com>
2023-11-01 14:03:03 +0900

    Create sub실습과제.c

---
+++ C/C프로그래밍1/LAB/HW3/sub실습과제.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int c1=1,c2=2,c3=3;
+    srand(time(NULL));
+    for(int i = 0;i < 5;i++){
+        do {
+            c1 = rand()%10;
+            c2 = rand()%10;
+            c3 = rand()%10;
+        }   while (c1 == c2 || c2 == c3 || c1== c3);
+
+        printf("%d %d %d\n",c1,c2,c3);
+
+
+    }
+
+
+
+    return 0;
+}
