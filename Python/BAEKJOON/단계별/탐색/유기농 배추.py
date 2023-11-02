b18e955d98df74fcaaaedffd7a17fabc3ff78140
tjrwls0826 <tjrwls0826@naver.com>
2023-11-03 05:25:29 +0900

    Update 유기농 배추.py

--- Python/BAEKJOON/단계별/탐색/유기농 배추.py
+++ Python/BAEKJOON/단계별/탐색/유기농 배추.py
@@ -7,12 +7,17 @@ t = int(input())
 
 for _ in range(t):
     m, n, k = map(int, input().split())
-    graph = [[0] * 51 for _ in range(51)] # 최대가 50까지라했으나 +1하고 사용할거기 때문
+    graph = [[0] * 51 for _ in range(51)]  # 최대가 50까지라했으나 +1하고 사용할거기 때문
     visit = [[0] * 51 for _ in range(51)]
     print(graph)
 
     for _ in range(k):
         x, y = map(int, input().split())
-        graph[y+1][x+1] = 0
+        graph[y + 1][x + 1] = 1
 
     answer = 0
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if graph[i][j] == 1 and visit[i][j] == 0:
+                dfs(i,j)
+                answer += 1
