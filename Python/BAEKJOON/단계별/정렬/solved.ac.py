7d91c8466548379ea267f242cfc0ce65afc9b43a
tjrwls0826 <tjrwls0826@naver.com>
2023-11-04 04:24:13 +0900

    Update solved.ac.py

--- Python/BAEKJOON/단계별/정렬/solved.ac.py
+++ Python/BAEKJOON/단계별/정렬/solved.ac.py
@@ -12,14 +12,19 @@ def myRound(x):
 
 
 n = int(input())
-lst = []
+if n == 0:
+    print(0)
+else:
+    lst = []
 
-for _ in range(n):
-    lst.append(int(input()))
-
-print(lst)
-
-cutline = myRound(len(lst) * 0.15)
-
-print(cutline)
-
+    for _ in range(n):
+        lst.append(int(input()))
+    lst.sort()
+    # print(lst)
+    cutline = myRound(len(lst) * 0.15)
+    # print(cutline)
+    if cutline != 0:
+        croped = lst[cutline:-cutline]
+        print(myRound(sum(croped) / len(croped)))
+    else:
+        print(myRound(sum(lst) / len(lst)))
