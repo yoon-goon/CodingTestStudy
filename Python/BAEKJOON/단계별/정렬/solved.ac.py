81a69b011aff5dfd4b6de477cf00d978107c66ca
tjrwls0826 <tjrwls0826@naver.com>
2023-11-04 04:03:05 +0900

    Update solved.ac.py

--- Python/BAEKJOON/단계별/정렬/solved.ac.py
+++ Python/BAEKJOON/단계별/정렬/solved.ac.py
@@ -3,6 +3,14 @@ import sys
 
 input = sys.stdin.readline
 
+
+def myRound(x):
+    if x - int(x) >= 0.5:
+        return int(x) + 1
+    else:
+        return int(x)
+
+
 n = int(input())
 lst = []
 
@@ -10,3 +18,8 @@ for _ in range(n):
     lst.append(int(input()))
 
 print(lst)
+
+cutline = myRound(len(lst) * 0.15)
+
+print(cutline)
+
