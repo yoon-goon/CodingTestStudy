56bd2de87fbd4853ad1689828c3f6ba45281159e
tjrwls0826 <tjrwls0826@naver.com>
2023-11-05 05:39:34 +0900

    Update 계단 오르기.py

--- Python/BAEKJOON/단계별/다이나믹 프로그래밍/계단 오르기.py
+++ Python/BAEKJOON/단계별/다이나믹 프로그래밍/계단 오르기.py
@@ -12,5 +12,10 @@ for i in range(n):
 
 dp = [0] * n
 dp[0] = stair[0]
-dp[1] = stair[0]+stair[1]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])
 
+for y in range(3, n):
+    dp[y] = max(dp[y - 3] + stair[y - 1] + stair[y], dp[y - 2] + stair[y])
+
+print(dp[n-1])
