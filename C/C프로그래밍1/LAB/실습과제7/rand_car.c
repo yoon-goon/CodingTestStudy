87a2e2d449864091eda3cdcf90e963386fff76be
tjrwls0826 <tjrwls0826@naver.com>
2023-11-14 18:11:39 +0900

    Update rand_car.c

--- C/C프로그래밍1/LAB/실습과제7/rand_car.c
+++ C/C프로그래밍1/LAB/실습과제7/rand_car.c
@@ -14,3 +14,23 @@ void disp_car(int car_num,int distance)
     }
     printf("\n");
 }
+
+int main(void)
+{
+    int i;
+    int car1_dist=0, car2_dist=0, car3_dist=0;
+
+    srand((unsigned)time(NULL));
+
+    for(i=0;i<6;i++){
+        car1_dist += rand() % 100;
+        car2_dist += rand() % 100;
+        car3_dist += rand() % 100;
+        disp_car(1,car1_dist);
+        disp_car(2,car2_dist);
+        disp_car(3,car3_dist);
+        printf("--------------------\n");
+        _getch();
+    }
+    return 0;
+}
