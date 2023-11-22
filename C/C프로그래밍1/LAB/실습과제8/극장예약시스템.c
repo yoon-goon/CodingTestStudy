e3741dfa79d2fb90b5c9ab42a90ea91ad727ea59
tjrwls0826 <tjrwls0826@naver.com>
2023-11-22 19:25:55 +0900

    Update 극장예약시스템.c

--- C/C프로그래밍1/LAB/실습과제8/극장예약시스템.c
+++ C/C프로그래밍1/LAB/실습과제8/극장예약시스템.c
@@ -7,22 +7,29 @@ int main()
     int num;
     char inp;
     int arr[SEAT] = {0};
+
     while(1){
-        inp=getchar();
-        if (inp=='y'){
+        printf("좌석을 예약하시겠습니까?(y또는n): ");
+        scanf(" %c",&inp);
+        if(inp=='y'){
             printf("-----------------------\n1 2 3 4 5 6 7 8 9 10\n-----------------------\n");
             for(int i = 0;i<SEAT;i++){
                 printf("%d ",arr[i]);
             }
+
+            printf("\n몇번째 좌석을 예약하시겠습니까? ");
+            scanf("%d",&num);
+            
+            if (arr[num-1] == 0){
+                arr[num-1] = 1;
+                printf("예약되었습니다.\n");
+            }
+            else
+                printf("이미 예약된 자리입니다.\n");
         }
-        else{
+        else if(inp=='n'){
             break;
         }
-
-
-
     }
-
-
     return 0;
 }
