d4937c36eea084fedf867608fb949c60ab9d4f06
tjrwls0826 <tjrwls0826@naver.com>
2023-11-22 13:24:05 +0900

    Create is_multiple.c

---
+++ C/C프로그래밍1/LAB/HW4/is_multiple.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+is_multiple(int n, int m){
+    if(n%m==0){
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int main(){
+    int a,b;
+    printf("첫번째 정수를 입력하시오: ");
+    scanf("%d",&a);
+    printf("두번째 정수를 입력하시오: ");
+    scanf("%d",&b);
+
+    if(is_multiple(a,b)==1)
+        printf("%d는 %d의 배수입니다.",a,b);
+    else
+        printf("%d는 %d의 배수가 아닙니다.",a,b);
+
+    return 0;
+}
