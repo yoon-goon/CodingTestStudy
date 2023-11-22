843347a7dba0572bcb1a8bb06878df79268e3a84
tjrwls0826 <tjrwls0826@naver.com>
2023-11-22 13:35:11 +0900

    Update 정수구분.c

--- C/C프로그래밍1/LAB/HW4/정수구분.c
+++ C/C프로그래밍1/LAB/HW4/정수구분.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+int even(int n);
+int absolute(int n);
+int sign(int n);
+
+int main()
+{
+    int a;
+    printf("정수를 입력하시오: ");
+    scanf("%d",&a);
+
+    printf("even()의 결과: %d\n", even(a));
+    absolute(a);
+    printf("sign()의 결과: %d\n", sign(a));
+
+    return 0;
+}
+
+
+
 int even(int n){
-    if(n%2==1)
+    if(n%2==0)
         return 1;
     else
         return 0;
@@ -9,16 +28,16 @@ int even(int n){
 
 int absolute(int n){
     if(n<0)
-        printf("absolute()의 결과: %d",n*-1);
+        printf("absolute()의 결과: %d\n",n*-1);
     else
-        printf("absolute()의 결과: %d",n);
+        printf("absolute()의 결과: %d\n",n);
 }
 
-int main()
-{
-    int a = 14;
-    printf("%d\n", even(a));
-    absolute(a);
-
-    return 0;
+int sign(int n){
+    if(n<0)
+        return -1;
+    else if(n==0)
+        return 0;
+    else
+        return 1;
 }
