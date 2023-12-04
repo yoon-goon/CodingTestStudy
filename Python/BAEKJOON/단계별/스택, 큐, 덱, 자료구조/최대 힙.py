9d204ce4fb83353b34669938b506bf1051742fc1
tjrwls0826 <tjrwls0826@naver.com>
2023-12-04 23:44:42 +0900

    Update 최대 힙.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱, 자료구조/최대 힙.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱, 자료구조/최대 힙.py
@@ -1,6 +1,15 @@
 import sys
+import heapq
 
 n = int(input())
+hp = []
 
 for i in range(n):
     x = int(sys.stdin.readline())
+    if x == True:
+        heapq.heappush(hp, (-x, x))
+    else:
+        if len(hp) >= 1:
+            print(heapq.heappop(hp)[1])
+        else:
+            print(0)
