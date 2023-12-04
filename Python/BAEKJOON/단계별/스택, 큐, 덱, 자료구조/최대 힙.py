4233aef4d8aba427b0a25c4b69c9c13857f19dae
tjrwls0826 <tjrwls0826@naver.com>
2023-12-04 23:57:57 +0900

    Update 최대 힙.py

--- Python/BAEKJOON/단계별/스택, 큐, 덱, 자료구조/최대 힙.py
+++ Python/BAEKJOON/단계별/스택, 큐, 덱, 자료구조/최대 힙.py
@@ -7,8 +7,9 @@ hp = []
 
 for i in range(n):
     x = int(sys.stdin.readline())
-    if x:
-        heapq.heappush(hp, (-x, x))
+    if x:  # 0이 아닐때
+        heapq.heappush(hp, (-x, x)) # 튜플(tuple)를 원소로 추가하거나 삭제하면,
+                                    # 튜플 내에서 맨 앞에 있는 값을 기준으로 최소 힙이 구성되는 원리
     else:
         if len(hp) >= 1:
             print(heapq.heappop(hp)[1])
