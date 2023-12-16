6a9e675de74178bc97556e217ef61483856e619b
tjrwls0826 <tjrwls0826@naver.com>
2023-12-16 23:58:33 +0900

    Update 1,2,3 더하기.py

--- Python/BAEKJOON/단계별/다이나믹 프로그래밍/1,2,3 더하기.py
+++ Python/BAEKJOON/단계별/다이나믹 프로그래밍/1,2,3 더하기.py
@@ -5,3 +5,16 @@ t = int(input())
 
 for _ in range(t):
     n = sys.stdin.readline()
+    dp = [0] * (n + 1)
+
+    if n >= 1:
+        dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
+    if n >= 3:
+        dp[3] = 4
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[n])
